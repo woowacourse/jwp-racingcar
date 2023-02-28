e9faa372cdf9eb77c6bdf32099a5a168b1bdcec1
boorownie <probitanima11@gmail.com>
2023-02-28 11:30:14 +0900

    initial project

---
+++ src/main/java/racingcar/RacingcarApplication.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingcarApplication.class, args);
+	}
+
+}
