b10de5a68873723525f6019b9d62013072f9ee6c
dev_kong <ahdjd5@gmail.com>
2023-04-11 17:25:04 +0900

    feat: db 연결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,24 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+CREATE TABLE `game` (
+                        `id` long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        `trialCount` int NOT NULL,
+                        `date` date NOT NULL
 );
+
+CREATE TABLE `car` (
+                       `id` long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                       `g_id` long NOT NULL,
+                       `name` Varchar(40) NOT NULL,
+                       `position` int NOT NULL
+);
+
+CREATE TABLE `winner` (
+                          `id` long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                          `g_id` long NOT NULL,
+                          `c_id` long NOT NULL
+);
+
+ALTER TABLE `car` ADD FOREIGN KEY (`g_id`) REFERENCES `game` (`id`);
+
+ALTER TABLE `winner` ADD FOREIGN KEY (`g_id`) REFERENCES `game` (`id`);
+
+ALTER TABLE `winner` ADD FOREIGN KEY (`c_id`) REFERENCES `car` (`id`);
