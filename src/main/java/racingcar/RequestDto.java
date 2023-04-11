e1655b8d683276b61d23bfb513a70f86658d2e62
rawfishthelgh <rg970604@naver.com>
2023-04-11 18:00:58 +0900

    feat: 응답/요청 dto 생성

---
+++ src/main/java/racingcar/RequestDto.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class RequestDto {
+    String names;
+    int count;
+
+    public RequestDto(String names, int count) {
+        this.names = names;
+        this.count = count;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ src/main/java/racingcar/ResponseDto.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResponseDto {
+    private String winners;
+    private List<Car> racingCars;
+
+    public ResponseDto(List<Car> winners, List<Car> racingCars) {
+        this.winners = winners.stream().map(car->car.getName()).collect(Collectors.joining(","));
+        this.racingCars = racingCars;
+    }
+
+    public String getWinners() {
+        return winners;
+    }
+
+    public List<Car> getRacingCars() {
+        return racingCars;
+    }
+}
