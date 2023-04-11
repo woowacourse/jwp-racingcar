7b383343c70b944fc91b2070238683003dd5d79f
waterricecake <fruturum@nate.com>
2023-04-11 16:32:53 +0900

    feat: 기존 자동차 경주 코드 가져오기

---
+++ src/main/java/racingcar/Application.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import racingcar.controller.GameController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.play();
+        gameController.showResult();
+    }
+}
