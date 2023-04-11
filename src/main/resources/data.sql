fe5216e5d08fbd14c6932387ca4596649c7bcdbb
seokjin8678 <seokjin8678@gmail.com>
2023-04-11 19:48:05 +0900

    feat: 데이터베이스 의존성 설정 및 추가
    Co-authored-by: mcodnjs <codnjs8989@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,28 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
+CREATE TABLE GAME_RESULT
+(
     id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
+    trial_count INT         NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE PLAYER_RESULT
+(
+    id       INT         NOT NULL AUTO_INCREMENT,
+    game_id  INT         NOT NULL,
+    name     VARCHAR(10) NOT NULL,
+    position INT         NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (game_id) REFERENCES GAME_RESULT (id)
+);
+
+CREATE TABLE WINNER_RESULT
+(
+    id      INT         NOT NULL AUTO_INCREMENT,
+    game_id INT         NOT NULL,
+    winner  VARCHAR(10) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (game_id) REFERENCES GAME_RESULT (id)
+)
+
