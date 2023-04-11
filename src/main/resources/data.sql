094884b49abdbb9184f9287b33c63dcacdbac672
apptie <appti@naver.com>
2023-04-11 16:37:32 +0900

    feat: DB 테이블 스키마 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,31 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
+CREATE TABLE GAME (
+    game_id     INT         NOT NULL AUTO_INCREMENT,
     created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+    trial_count INT         NOT NULL,
+    PRIMARY KEY (game_id)
+);
+
+CREATE TABLE CAR (
+    car_id      INT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(10) NOT NULL,
+    game_id     INT         NOT NULL,
+    PRIMARY KEY (car_id),
+    FOREIGN KEY (game_id) REFERENCES GAME (game_id)
+);
+
+CREATE TABLE RESULT (
+    result_id   INT         NOT NULL AUTO_INCREMENT,
+    car_id      INT         NOT NULL,
+    position    INT         NOT NULL,
+    PRIMARY KEY (result_id),
+    FOREIGN KEY (car_id) REFERENCES CAR (car_id)
+);
+
+CREATE TABLE WINNER (
+    winner_id   INT         NOT NULL AUTO_INCREMENT,
+    game_id     INT         NOT NULL,
+    car_id      INT         NOT NULL,
+    PRIMARY KEY (winner_id),
+    FOREIGN KEY (car_id) REFERENCES CAR (car_id),
+    FOREIGN KEY (game_id) REFERENCES GAME (game_id)
 );
