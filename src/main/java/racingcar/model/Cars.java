97970a2ea0eb0ac757b7a512c834cfbf4da64878
donghae <donghae1999@gmail.com>
2023-04-11 20:05:07 +0900

    feat: 기존 자동차 경주 코드 가져오기

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,39 @@
+package racingcar.model;
+
+import racingcar.util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars = new ArrayList<>();
+
+    public Cars(List<String> carsName) {
+        for (String name : carsName) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public List<Car> getCars() {
+        return List.copyOf(cars);
+    }
+
+    public void moveResult(NumberGenerator numberGenerator) {
+        for (Car car : cars) {
+            car.moveByNumber(numberGenerator.generateNumber());
+        }
+    }
+
+    public List<String> getWinners() {
+        return cars.stream().filter(car -> car.checkLocationEqual(getMaxLocation()))
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private Car getMaxLocation() {
+        return cars.stream()
+                .max(Car::compareTo)
+                .get();
+    }
+}
---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+import java.util.Random;
+
+public class RandomNumberGenerator implements NumberGenerator {
+    private static final int RANDOM_NUMBER_STANDARD = 10;
+    private static Random random = new Random();
+
+    @Override
+    public int generateNumber() {
+        return random.nextInt(RANDOM_NUMBER_STANDARD);
+    }
+}
