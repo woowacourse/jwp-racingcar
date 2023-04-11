ec70950d499cdc44cae4689dacb382ec3c271ceb
kiarakim <kiarakim413@gmail.com>
2023-04-11 17:05:15 +0900

    feat: racing-car 코드 가져오기
    Co-authored-by: CFalws <akdlzmf1123@gmail.com>

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import racingcar.domain.strategy.MovingStrategy;
+
+public class Car {
+	private final CarName carName;
+	private int position;
+
+	public Car(String name) {
+		this.carName = new CarName(name);
+	}
+
+	public void move(MovingStrategy movingStrategy) {
+		if (movingStrategy.isMove()) {
+			position++;
+		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public String getName() {
+		return carName.getName();
+	}
+}
