bfd71c45c2b1812629ce31a9c8d0c10565221f7f
hanueleee <ezzeezz@naver.com>
2023-04-11 19:42:14 +0900

    feat: db에 테이블 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,18 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
+CREATE TABLE GAME
+(
+    id          INT      NOT NULL AUTO_INCREMENT,
+    trial_count INT      NOT NULL,
+    created_at  DATETIME NOT NULL default current_timestamp,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE PLAYER
+(
+    id          INT      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50)      NOT NULL,
+    position  DATETIME NOT NULL default current_timestamp,
+    is_win BOOLEAN NOT NULL,
+    game_id INT NOT NULL,
     PRIMARY KEY (id)
 );
