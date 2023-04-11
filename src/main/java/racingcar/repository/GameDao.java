cf908a57ce9645521c94dcf2de73ef0062d6de9f
thdwoqor <celebarte01@gmail.com>
2023-04-11 20:34:30 +0900

    feat: GameDao 구현

---
+++ src/main/java/racingcar/repository/GameDao.java
@@ -0,0 +1,26 @@
+package racingcar.repository;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class GameDao {
+    private final SimpleJdbcInsert insertActor;
+
+    public GameDao(DataSource dataSource) {
+        this.insertActor = new SimpleJdbcInsert(dataSource)
+                .withTableName("game")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public long insert(final int trial_count) {
+        SqlParameterSource params = new MapSqlParameterSource()
+                .addValue("trial_count", trial_count);
+
+        return insertActor.executeAndReturnKey(params).longValue();
+    }
+}
---
+++ src/test/java/racingcar/repository/GameDaoTest.java
@@ -0,0 +1,40 @@
+package racingcar.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class GameDaoTest {
+    @Autowired
+    private GameDao gameDao;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.execute("DROP TABLE IF EXISTS game");
+        jdbcTemplate.execute("CREATE TABLE game (\n" +
+                "    id int PRIMARY KEY AUTO_INCREMENT,\n" +
+                "    trial_count int NOT NULL,\n" +
+                "    game_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP\n" +
+                ");");
+
+        List<Object[]> trial_count = Arrays.asList(new String[]{"10"}, new String[]{"20"});
+        jdbcTemplate.batchUpdate("INSERT INTO game(trial_count) VALUES (?)", trial_count);
+    }
+
+    @Test
+    @DisplayName("값을 넣었을때 자동으로 증가되는 id 값을 반환하는 테스트")
+    void insert() {
+        long id = gameDao.insert(10);
+        Assertions.assertThat(id).isEqualTo(3);
+    }
+}
