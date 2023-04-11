975d13a49ed5d7d7df519decc633165e1bc353e3
kokodak <kokodakadokok@gmail.com>
2023-04-11 19:22:48 +0900

    test: 자동차 경주 테스트 코드 세팅
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>

---
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PositionTest {
+
+    @DisplayName("increase 메서드는 position 의 값을 1 증가시킨다.")
+    @ParameterizedTest
+    @CsvSource({"0,1", "1,2", "2,3"})
+    void should_IncreaseByOne_When_UsingIncreaseMethod(int beforeValue, int afterValue) {
+        //given
+        Position position = new Position(beforeValue);
+
+        //when
+        position.increase();
+
+        //then
+        assertThat(position.getPosition()).isEqualTo(afterValue);
+    }
+}
