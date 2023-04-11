5f59bfb78d12928ab17045529358069ffb95f8de
odo27 <odo27@naver.com>
2023-04-11 14:27:58 +0900

    feat: 기존 자동차 경주 코드 가져오기

---
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import racinggame.view.Validator;
+
+public class CarGenerator {
+    private static final Validator validator = new Validator();
+
+    public List<Car> generateCars(String[] carNames) {
+        validator.checkValidCarNames(carNames);
+        return Arrays.stream(carNames)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class Input {
+    private static final String DELIMITER = ",";
+    private static final Validator validator = new Validator();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getInput() {
+        return scanner.nextLine();
+    }
+
+    public static String[] getCarNames(String input) {
+        String[] carNames = input.split(DELIMITER);
+        validator.checkValidCarNames(carNames);
+        return carNames;
+    }
+
+    public static int getTryCount(String input) {
+        validator.checkDigits(input);
+        int tryCount = Integer.parseInt(input);
+        validator.checkRange(tryCount);
+        return tryCount;
+    }
+}
