9df5ab020c9bbb579179d0a26ab4cb779b9a05d3
waterricecake <fruturum@nate.com>
2023-04-11 16:48:27 +0900

    feat(HttpMethodController): web 요청-post 기능 구현

---
+++ src/main/java/racingcar/mapping/HttpMethodController.java
@@ -0,0 +1,29 @@
+package racingcar.mapping;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import racingcar.domain.Car;
+import racingcar.dto.GameInputDto;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/http-method")
+public class HttpMethodController {
+    @PostMapping("/car")
+    public ResponseEntity createCar(@RequestBody Car car){
+        Long id = 1L;
+        return ResponseEntity.created(URI.create("/car/"+id)).build();
+    }
+
+    @PostMapping("/racingGame")
+    public ResponseEntity postInput(@RequestBody GameInputDto gameInputDto){
+        Long id = 1L;
+        System.out.println(gameInputDto);
+
+        return ResponseEntity.created(URI.create("/racingGame/"+id)).build();
+    }
+}
