3b24c51551e4dd6100c906da842884cc2fcb4271
0chil <0@chll.it>
2023-04-11 14:21:54 +0900

    chore: 기존 미션 복사

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,68 @@
+package racingcar.controller;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import racingcar.domain.Car;
+import racingcar.domain.Game;
+import racingcar.domain.MoveChance;
+import racingcar.domain.RandomMoveChance;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+
+    private static final String EXCEPTION_PREFIX = "[ERROR]";
+
+    private static final InputView INPUT_VIEW = new InputView();
+    private static final OutputView OUTPUT_VIEW = new OutputView();
+
+    private final Game game;
+    private final MoveChance moveChance = new RandomMoveChance();
+
+    public GameController() {
+        game = createGame();
+    }
+
+    private Game createGame() {
+        return handleExceptionByRepeating(() -> {
+            List<String> carNames = INPUT_VIEW.inputCarNames();
+            int trialCount = INPUT_VIEW.inputTrialCount();
+            return new Game(makeCarsWith(carNames), trialCount);
+        });
+    }
+
+    public void play() {
+        OUTPUT_VIEW.noticeResult();
+        playMultipleTimes();
+        showResult();
+    }
+
+    private List<Car> makeCarsWith(List<String> carNames) {
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private void playMultipleTimes() {
+        while (game.isNotDone()) {
+            game.playOnceWith(moveChance);
+            OUTPUT_VIEW.printStatusOf(game.getCars());
+        }
+    }
+
+    private void showResult() {
+        OUTPUT_VIEW.printStatusOf(game.getCars());
+        OUTPUT_VIEW.printWinners(game.findWinners());
+    }
+
+    private <T> T handleExceptionByRepeating(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (Exception exception) {
+            System.out.println(EXCEPTION_PREFIX + exception.getMessage());
+            return handleExceptionByRepeating(supplier);
+        }
+    }
+}
