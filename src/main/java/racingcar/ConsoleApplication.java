cc2eca56d6b531b1664c24384f37d37d5ca510e9
MoonJeWoong <munjin0201@naver.com>
2023-04-11 14:12:04 +0900

    chore: 기존 콘솔 자동차 경주 프로젝트 코드 마이그레이션

---
+++ src/main/java/racingcar/ConsoleApplication.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import racingcar.controller.Controller;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.Scanner;
+
+public class ConsoleApplication {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller(new InputView(new Scanner(System.in)), new OutputView(),
+                new RandomNumberGenerator());
+        controller.run();
+    }
+
+}
