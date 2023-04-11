0a0383725d9b7efac0ea20aa241e19593995bbd3
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-04-11 14:33:21 +0900

    feat(application): 웹 어플리케이션 구현

---
+++ src/main/java/racing/RacingCarApplication.java
@@ -0,0 +1,13 @@
+package racing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacingCarApplication.class, args);
+    }
+
+}
