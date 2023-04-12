46aed2d50b28f592bbbef1fbeb7b7934dcd50d18
rawfishthelgh <rg970604@naver.com>
2023-04-12 13:49:43 +0900

    feat: 데이터베이스 스키마 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,18 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
 CREATE TABLE PLAY_RESULT (
     id          INT         NOT NULL AUTO_INCREMENT,
+    count       INT         NOT NULL ,
     winners     VARCHAR(50) NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE PLAYERS_INFO(
+    id          INT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(6)  NOT NULL ,
+    position    INT         NOT NULL ,
+    parent_id   INT         NOT NULL ,
+    PRIMARY KEY (id),
+    FOREIGN KEY (parent_id)
+    REFERENCES PLAY_RESULT(id)
 );
