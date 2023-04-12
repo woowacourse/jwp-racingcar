92d9450886261435a5bc0e9f31bcea44d77aa886
yeonkkk <tjddus0302@gmail.com>
2023-04-12 14:31:26 +0900

    refactor: 메서드 분리

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import racingcar.domain.Car;
 import racingcar.dto.CarDto;
 import racingcar.dto.GameRequest;
 import racingcar.dto.GameResponse;
@@ -13,6 +14,8 @@ import racingcar.service.RacingCarService;
 
 @RestController
 public class RacingCarController {
+
+    private static final String DELIMITER = ",";
 
     private final RacingCarService racingCarService;
 
@@ -23,18 +26,26 @@ public class RacingCarController {
     @PostMapping("/plays")
     @ResponseBody
     public GameResponse createGame(@RequestBody GameRequest gameRequest) {
-        String names = gameRequest.getNames();
-        List<String> carNames = List.of(names.split(","));
-
+        List<String> carNames = splitNames(gameRequest);
         racingCarService.createGame(carNames);
+
         racingCarService.race(gameRequest.getCount());
         List<String> winners = racingCarService.findWinners();
-        String winnerNames = String.join(",", winners);
+        String winnerNames = String.join(DELIMITER, winners);
+        List<CarDto> cars = convertDto(racingCarService.findCars());
 
-        List<CarDto> carDtos = racingCarService.findCars().stream()
-                .map(car -> CarDto.from(car))
+        return GameResponse.of(winnerNames, cars);
+    }
+
+    private List<CarDto> convertDto(List<Car> cars) {
+        return cars.stream()
+                .map(CarDto::from)
                 .collect(Collectors.toList());
+    }
 
-        return GameResponse.of(winnerNames, carDtos);
+    private List<String> splitNames(final GameRequest gameRequest) {
+        String names = gameRequest.getNames();
+
+        return List.of(names.split(DELIMITER));
     }
 }
