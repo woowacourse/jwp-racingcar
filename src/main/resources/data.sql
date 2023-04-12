9dc39d51c902eaf19e3384ac06ba421fcdcd61ca
be-student <presentsong@naver.com>
2023-04-12 16:02:50 +0900

    fix: db 관련 설정 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,26 @@
-CREATE TABLE GAME (
-    game_id     INT         NOT NULL AUTO_INCREMENT,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    trial_count INT         NOT NULL,
+CREATE TABLE IF NOT EXISTS GAME
+(
+    game_id     INT       NOT NULL AUTO_INCREMENT,
+    created_at  TIMESTAMP NOT NULL,
+    trial_count INT       NOT NULL,
     PRIMARY KEY (game_id)
 );
 
-CREATE TABLE CAR (
-    car_id      INT         NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(10) NOT NULL,
-    game_id     INT         NOT NULL,
+CREATE TABLE IF NOT EXISTS CAR
+(
+    car_id   INT         NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(10) NOT NULL,
+    position INT         NOT NULL,
+    game_id  INT         NOT NULL,
     PRIMARY KEY (car_id),
     FOREIGN KEY (game_id) REFERENCES GAME (game_id)
 );
 
-CREATE TABLE RESULT (
-    result_id   INT         NOT NULL AUTO_INCREMENT,
-    car_id      INT         NOT NULL,
-    position    INT         NOT NULL,
-    PRIMARY KEY (result_id),
-    FOREIGN KEY (car_id) REFERENCES CAR (car_id)
-);
-
-CREATE TABLE WINNER (
-    winner_id   INT         NOT NULL AUTO_INCREMENT,
-    game_id     INT         NOT NULL,
-    car_id      INT         NOT NULL,
+CREATE TABLE IF NOT EXISTS WINNER
+(
+    winner_id INT NOT NULL AUTO_INCREMENT,
+    game_id   INT NOT NULL,
+    car_id    INT NOT NULL,
     PRIMARY KEY (winner_id),
     FOREIGN KEY (car_id) REFERENCES CAR (car_id),
     FOREIGN KEY (game_id) REFERENCES GAME (game_id)
