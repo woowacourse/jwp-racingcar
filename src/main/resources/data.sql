cd28618efb65054f2c74946d4cb871874a8fdb69
greeng00se <thegreengoose7@gmail.com>
2023-04-12 13:41:00 +0900

    feat: h2 데이터베이스 연동 및 sql 구문 추가
    Co-authored-by: ingpyo <juninpyo726@naver.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,19 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
+CREATE TABLE GAME
+(
+    id         INT         NOT NULL AUTO_INCREMENT,
+    winners    VARCHAR(50) NOT NULL,
+    trial      INT         NOT NULL,
+    created_at DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE CAR
+(
+    id       INT         NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(50) NOT NULL,
+    position INT         NOT NULL,
+    game_id  INT         NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES GAME(id),
+    PRIMARY KEY (id)
+)
