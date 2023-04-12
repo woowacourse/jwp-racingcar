60889328bb661781f0d09ef57d068c566f822bf4
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-12 20:18:14 +0900

    refactor: 메서드 분리
    Co-authored-by: the9kim <the9rla@gmail.com>

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,23 +1,18 @@
 package racingcar.controller;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
-import racingcar.dao.RacingCarDao;
-import racingcar.dao.ResultDao;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import racingcar.domain.*;
+import racingcar.service.RacingCarService;
 import racingcar.util.RandomNumberGenerator;
 import racingcar.validation.Validation;
 
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@Configuration
 public class RacingCarController {
 
     private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
@@ -32,27 +27,39 @@ public class RacingCarController {
 
     @PostMapping("plays")
     public ResponseEntity<GameResultDto> createGame(@RequestBody GameInforamtionDto gameInforamtionDto) {
+        Cars cars = getCars(gameInforamtionDto);
+        int trialCount = getTrialCount(gameInforamtionDto);
+
+        List<RacingCarDto> racingCars = playGame(cars, trialCount);
+
+        GameResultDto gameResultDto = new GameResultDto(cars.getWinnerCars(), racingCars);
+        return ResponseEntity.ok()
+                             .body(gameResultDto);
+    }
+
+    private static Cars getCars(GameInforamtionDto gameInforamtionDto) {
         String names = gameInforamtionDto.getNames();
         Validation.validateCarNames(names);
         Cars cars = new Cars(names);
+        return cars;
+    }
 
+    private static int getTrialCount(GameInforamtionDto gameInforamtionDto) {
         int trialCount = gameInforamtionDto.getCount();
         Validation.validateTryCount(trialCount);
+        return trialCount;
+    }
 
+    private List<RacingCarDto> playGame(Cars cars, int trialCount) {
         for (int count = 0; count < trialCount; count++) {
             cars.moveForRound(randomNumberGenerator);
         }
-
-        long resultId = resultDao.insert(trialCount, cars.getWinnerCars());
 
         List<RacingCarDto> racingCars = new ArrayList<>();
         for (Car car : cars.getCars()) {
             RacingCarDto racingCarDto = new RacingCarDto(car.getName(), car.getLocation());
             racingCars.add(racingCarDto);
-            racingCarDao.insert(car, resultId);
         }
-
-        GameResultDto gameResultDto = new GameResultDto(cars.getWinnerCars(), racingCars);
-        return ResponseEntity.ok().body(gameResultDto);
+        return racingCars;
     }
 }
