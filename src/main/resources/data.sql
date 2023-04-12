5db7aeb91c376e75969d3823da0b80c15ce62bec
ezzanzzan <cjsdmswjd821@naver.com>
2023-04-12 15:43:30 +0900

    chore: application.properties 업데이트
    Co-authored-by: hong-sile <gurwns9325@naver.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,20 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
 CREATE TABLE PLAY_RESULT
 (
     id         INT         NOT NULL AUTO_INCREMENT,
+    trialCount INT         NOT NULL,
     winners    VARCHAR(50) NOT NULL,
     created_at DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE RACING_CARS
+(
+    id              INT          NOT NULL AUTO_INCREMENT,
+    player_name     VARCHAR(255) NOT NULL,
+    player_position INT          NOT NULL,
+    game_id         INT          NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (game_id) REFERENCES PLAY_RESULT (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
