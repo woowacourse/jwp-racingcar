d6089eb0f6ee7374d9c07d611a76441051305199
iamjooon2 <iamjooon2@gmail.com>
2023-04-12 21:17:01 +0900

    refactor: 사용하지 않는 메서드 삭제 및 로직 수정
    Co-authored-by: Combi153 <jcm04@naver.com>

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -1,15 +1,9 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import racingcar.domain.carfactory.CarFactory;
-import racingcar.domain.cars.Cars;
-import racingcar.domain.record.GameRecorder;
-import racingcar.domain.system.GameSystem;
 import racingcar.dto.RequestDTO;
 import racingcar.dto.ResponseDTO;
 import racingcar.service.RacingCarService;
@@ -25,18 +19,7 @@ public class WebController {
 
     @PostMapping("/plays")
     public ResponseEntity createGame(@RequestBody RequestDTO requestDTO) {
-        final String names = requestDTO.getNames();
-        final int count = requestDTO.getCount();
-        ResponseDTO responseDTO = racingCarService.play(names, count);
+        ResponseDTO responseDTO = racingCarService.play(requestDTO.getNames(), requestDTO.getCount());
         return ResponseEntity.ok().body(responseDTO);
-    }
-
-    private Cars makeCars(final List<String> carNames) {
-        CarFactory carFactory = new CarFactory();
-        return carFactory.createCars(carNames);
-    }
-
-    private GameSystem createGameSystem(final int gameRound) {
-        return new GameSystem(gameRound, new GameRecorder(new ArrayList<>()));
     }
 }
