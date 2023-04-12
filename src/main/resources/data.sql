dd2c53493fcee5f39ebe7b5c134fa604a98827ee
jiwon <ljwon77@daum.net>
2023-04-12 19:08:34 +0900

    docs: 테이블 스크립트를 변경한다
    Co-authored-by: younghoondoodoom <dudgns0330@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,18 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT
+CREATE TABLE IF NOT EXISTS RACE_RESULT
 (
-    id         INT         NOT NULL AUTO_INCREMENT,
-    winners    VARCHAR(50) NOT NULL,
-    created_at DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
-);
+    `race_result_id` BIGINT(20) UNIQUE NOT NULL AUTO_INCREMENT,
+    `trial_count`    int         NOT NULL,
+    `winners`        VARCHAR(50) NOT NULL,
+    `created_at`     DATETIME    NOT NULL default current_timestamp,
+    PRIMARY KEY (`race_result_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS CAR
+(
+    `car_id`         BIGINT(20) UNIQUE NOT NULL AUTO_INCREMENT,
+    `name`           VARCHAR(10) NOT NULL UNIQUE,
+    `position`       int         NOT NULL,
+    `race_result_id` BIGINT(20),
+    PRIMARY KEY (`car_id`),
+    FOREIGN KEY (`race_result_id`) REFERENCES RACE_RESULT (`race_result_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
