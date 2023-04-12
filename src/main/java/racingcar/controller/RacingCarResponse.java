1f94945aeea56f3ba83a4b725dad957b3f654241
songusika <sws981202@gmail.com>
2023-04-12 14:32:06 +0900

    feat: 응답 객체 구현

---
+++ src/main/java/racingcar/controller/RacingCarResponse.java
@@ -0,0 +1,22 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.RacingCar;
+
+public class RacingCarResponse {
+    private List<String> winners;
+    private List<RacingCar> racingCars;
+
+    public RacingCarResponse(final List<String> winners, final List<RacingCar> racingCars) {
+        this.winners = winners;
+        this.racingCars = racingCars;
+    }
+
+    public List<String> getWinners() {
+        return winners;
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return racingCars;
+    }
+}
