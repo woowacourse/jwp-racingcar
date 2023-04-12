b7f55588a9ad8977921aa33d8e2352163ead15a9
donghae <donghae1999@gmail.com>
2023-04-12 14:52:48 +0900

    test: playGame Post 메소드 추가

---
+++ src/test/java/racingcar/controller/WebControllerTest.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import racingcar.dto.RequestDto;
+import static org.hamcrest.core.Is.is;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WebControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("playGame - POST")
+    @Test
+    void playGame() {
+        RequestDto requestDto = new RequestDto("ocean,mint", 5);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(requestDto)
+                .when().post("/plays")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", is(2));
+    }
+}
+
