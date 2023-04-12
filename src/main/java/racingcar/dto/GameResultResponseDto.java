c691e94b5620c0148a846f4fb1bbe205435ffda0
sosow0212 <sosow0212@naver.com>
2023-04-12 13:20:43 +0900

    refactor: 우승자 조회를 할 때 동점인 경우에도 우승자로 판단되게 DTO 리팩토링 진행

--- src/main/java/racingcar/dto/GameResultResponseDto.java
+++ src/main/java/racingcar/dto/GameResultResponseDto.java
@@ -6,15 +6,15 @@ import racingcar.domain.Cars;
 
 public class GameResultResponseDto {
 
-    private String winners;
+    private List<String> winners;
     private List<CarStatusResponseDto> racingCars;
 
-    private GameResultResponseDto(final String winners, final List<CarStatusResponseDto> racingCars) {
+    private GameResultResponseDto(final List<String> winners, final List<CarStatusResponseDto> racingCars) {
         this.winners = winners;
         this.racingCars = racingCars;
     }
 
-    public static GameResultResponseDto toDto(final String winners, final Cars cars) {
+    public static GameResultResponseDto toDto(final List<String> winners, final Cars cars) {
         List<CarStatusResponseDto> carStatuses = cars.getCars().stream()
                 .map(CarStatusResponseDto::toDto)
                 .collect(Collectors.toList());
@@ -22,7 +22,11 @@ public class GameResultResponseDto {
         return new GameResultResponseDto(winners, carStatuses);
     }
 
-    public String getWinners() {
+    public boolean isWinner(final String name) {
+        return this.winners.contains(name);
+    }
+
+    public List<String> getWinners() {
         return winners;
     }
 
