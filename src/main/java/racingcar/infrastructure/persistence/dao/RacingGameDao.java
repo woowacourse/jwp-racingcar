aedc7fd509012ac07ebe9b39f86f0096406d66b6
shin-mallang <huipulci1@naver.com>
2023-04-12 13:03:15 +0900

    refactor: simpleJdbcInsert 사용

--- src/main/java/racingcar/infrastructure/persistence/dao/RacingGameDao.java
+++ src/main/java/racingcar/infrastructure/persistence/dao/RacingGameDao.java
@@ -1,46 +1,41 @@
 package racingcar.infrastructure.persistence.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import racingcar.infrastructure.persistence.entity.RacingGameEntity;
 
-import java.sql.PreparedStatement;
-import java.time.LocalDateTime;
 import java.util.Optional;
-
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 @Component
 public class RacingGameDao {
 
     private final JdbcTemplate template;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public RacingGameDao(final JdbcTemplate template) {
         this.template = template;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(template)
+                .withTableName("PLAY_RESULT")
+                .usingGeneratedKeyColumns("id", "created_at");
     }
 
     public Long save(final RacingGameEntity racingGameEntity) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        final String sql = "INSERT INTO PLAY_RESULT (created_at, trial_count) VALUES (?, ?)";
-        template.update(conn -> {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql, RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, LocalDateTime.now().toString());
-            preparedStatement.setInt(2, racingGameEntity.getTrialCount());
-            return preparedStatement;
-        }, keyHolder);
-
-        return ((long) keyHolder.getKeys().get("id"));
+        SqlParameterSource parameters = new BeanPropertySqlParameterSource(racingGameEntity);
+        return (Long) simpleJdbcInsert.executeAndReturnKeyHolder(parameters)
+                .getKeys()
+                .get("id");
     }
 
     public Optional<RacingGameEntity> findById(final Long id) {
         return Optional.ofNullable(
                 template.queryForObject("SELECT * FROM PLAY_RESULT WHERE id = ?",
                         (rs, rowNum) -> new RacingGameEntity(
-                                rs.getLong(1),
-                                rs.getTimestamp(2).toLocalDateTime(),
-                                rs.getInt(3)
+                                rs.getLong("id"),
+                                rs.getTimestamp("created_at").toLocalDateTime(),
+                                rs.getInt("trial_count")
                         ), id)
         );
     }
