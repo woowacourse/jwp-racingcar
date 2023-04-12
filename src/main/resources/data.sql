39051d13fe4976d2bf0b7ae652ce31ba73c7ee77
iamjooon2 <iamjooon2@gmail.com>
2023-04-12 17:21:13 +0900

    refactor: Table query 수정
    Co-authored-by: Combi153 <jcm04@naver.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT
+CREATE TABLE GAME
 (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    trial_count INT         NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
+    id          INT      NOT NULL AUTO_INCREMENT,
+    trial_count INT      NOT NULL,
+    created_at  DATETIME NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE CAR
 (
-    id             INT         NOT NULL AUTO_INCREMENT,
-    name           VARCHAR(50) NOT NULL,
-    position       INT         NOT NULL,
-    play_result_id INT         NOT NULL ,
+    id       INT         NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(50) NOT NULL,
+    position INT         NOT NULL,
+    game_id  INT         NOT NULL,
+    is_win   BOOLEAN     NOT NULl,
     PRIMARY KEY (id),
-    FOREIGN KEY (play_result_id) REFERENCES PLAY_RESULT(id)
+    FOREIGN KEY (game_id) REFERENCES GAME (id)
 );
