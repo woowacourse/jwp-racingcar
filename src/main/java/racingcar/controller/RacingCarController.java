d77e176d6b3a321a3f7bd4e699269004805743d6
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-12 19:21:01 +0900

    feat: db 연동 및 플레이 이력 저장 기능 추가
    Co-authored-by: the9kim <the9rla@gmail.com>

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,18 +1,34 @@
 package racingcar.controller;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+import racingcar.dao.RacingCarDao;
+import racingcar.dao.ResultDao;
 import racingcar.domain.*;
 import racingcar.util.RandomNumberGenerator;
 import racingcar.validation.Validation;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Configuration
 public class RacingCarController {
 
     private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    private final DataSource dataSource = DataSourceBuilder.create()
+                                                           .url("jdbc:h2:mem:testdb")
+                                                           .username("sa")
+                                                           .password("")
+                                                           .type(HikariDataSource.class)
+                                                           .build();
+    private final ResultDao resultDao = new ResultDao(new JdbcTemplate(dataSource));
+    private final RacingCarDao racingCarDao  = new RacingCarDao(new JdbcTemplate(dataSource));
 
     @PostMapping("plays")
     public ResponseEntity<GameResultDto> createGame(@RequestBody GameInforamtionDto gameInforamtionDto) {
@@ -20,17 +36,20 @@ public class RacingCarController {
         Validation.validateCarNames(names);
         Cars cars = new Cars(names);
 
-        int count = gameInforamtionDto.getCount();
-        Validation.validateTryCount(count);
+        int trialCount = gameInforamtionDto.getCount();
+        Validation.validateTryCount(trialCount);
 
-        for (int number = 0; number < count; number++) {
+        for (int count = 0; count < trialCount; count++) {
             cars.moveForRound(randomNumberGenerator);
         }
+
+        long resultId = resultDao.insert(trialCount, cars.getWinnerCars());
 
         List<RacingCarDto> racingCars = new ArrayList<>();
         for (Car car : cars.getCars()) {
             RacingCarDto racingCarDto = new RacingCarDto(car.getName(), car.getLocation());
             racingCars.add(racingCarDto);
+            racingCarDao.insert(car, resultId);
         }
 
         GameResultDto gameResultDto = new GameResultDto(cars.getWinnerCars(), racingCars);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,18 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
+CREATE TABLE RESULTS
+(
     id          INT         NOT NULL AUTO_INCREMENT,
+    trial_count INT         NOT NULL,
     winners     VARCHAR(50) NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE RACING_CARS
+(
+    id        INT         NOT NULL AUTO_INCREMENT,
+    name      VARCHAR(50) NOT NULL,
+    position  INT         NOT NULL,
+    result_id INT         NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (`result_id`) REFERENCES RESULTS (`id`)
 );
