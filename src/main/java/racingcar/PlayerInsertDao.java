c2c847b01c4c57a76024dec4d084ce7a1e794b24
Cyma-s <ssol0319@naver.com>
2023-04-12 17:23:27 +0900

    refactor(PlayerInsertDao): 구현 세부 로직 변경

--- src/main/java/racingcar/PlayerInsertDao.java
+++ src/main/java/racingcar/PlayerInsertDao.java
@@ -1,11 +1,12 @@
 package racingcar;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import racingcar.dto.RacingCarStatusResponse;
 
@@ -13,24 +14,19 @@ import racingcar.dto.RacingCarStatusResponse;
 public class PlayerInsertDao {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
     @Autowired
     private WinnerInsertDao winnerInsertDao;
 
     public void insertPlayer(List<RacingCarStatusResponse> responses, List<String> winnerNames, int gameId) {
-        String sql = "INSERT INTO player(name, position) VALUES(?, ?)";
-        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
-        for (RacingCarStatusResponse response : responses) {
-            String name = response.getName();
-            jdbcTemplate.update(connection -> {
-                PreparedStatement preparedStatement = connection.prepareStatement(sql,
-                        Statement.RETURN_GENERATED_KEYS);
-                preparedStatement.setString(1, name);
-                preparedStatement.setInt(2, response.getPosition());
-                return preparedStatement;
-            }, generatedKeyHolder);
+        KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+        String sql = "INSERT INTO player(name, position) VALUES(:name, :position)";
 
-            if (winnerNames.contains(name)) {
+        for (RacingCarStatusResponse response : responses) {
+            SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(response);
+            jdbcTemplate.update(sql, parameterSource, generatedKeyHolder);
+
+            if (winnerNames.contains(response.getName())) {
                 int playerId = generatedKeyHolder.getKey().intValue();
                 winnerInsertDao.insertWinner(gameId, playerId);
             }
