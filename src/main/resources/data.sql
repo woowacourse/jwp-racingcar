9cfb4a34c130f20a8936100775b774f53bec56c4
sosow0212 <sosow0212@naver.com>
2023-04-12 11:26:04 +0900

    feat: 데이터베이스 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,26 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+-- CREATE TABLE PLAY_RESULT (
+--     id          INT         NOT NULL AUTO_INCREMENT,
+--     winners     VARCHAR(50) NOT NULL,
+--     created_at  DATETIME    NOT NULL default current_timestamp,
+--     PRIMARY KEY (id)
+-- );
+
+CREATE TABLE game (
+                      id INT AUTO_INCREMENT,
+                      trialCount INT NOT NULL,
+                      dateTime DATETIME NOT NULL,
+                      PRIMARY KEY (id)
+);
+
+CREATE TABLE player (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        game_id INT NOT NULL,
+                        name VARCHAR(45) NOT NULL,
+                        position INT NOT NULL,
+                        isWinner TINYINT NOT NULL,
+                        PRIMARY KEY (id),
+                        CONSTRAINT fk_player_game_id
+                            FOREIGN KEY (game_id)
+                                REFERENCES game(id)
 );
