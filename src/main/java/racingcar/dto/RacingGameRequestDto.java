b65f758784994f8c701323f09abe2b13eec9ce89
zillionme <beatus96@naver.com>
2023-04-12 15:51:24 +0900

    feat: 클라이언트 요청 dto 추가

---
+++ src/main/java/racingcar/dto/RacingGameRequestDto.java
@@ -0,0 +1,16 @@
+package racingcar.dto;
+
+public class RacingGameRequestDto {
+
+    private String names;
+    private int count;
+
+    public String getNames() {
+        return names;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
