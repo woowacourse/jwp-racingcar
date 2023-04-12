7bc2751ba1aaacdbb2c714d18463ba95be2e7d6b
dooboocookie <bkh3030@gmail.com>
2023-04-12 15:12:50 +0900

    feat(DB): 데이터베이스 연동 및 테이블 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,22 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
+CREATE TABLE GAME (
     id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
+    trial_count INT         NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE PLAYER (
+    id          INT         NOT NULL AUTO_INCREMENT,
+    name        varchar(10) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE PARTICIPATES (
+    game_id     INT     NOT NULL,
+    player_id   INT     NOT NULL,
+    position    INT     NOT NULL,
+    is_winner   BOOLEAN NOT NULL default false,
+    PRIMARY KEY (game_id, player_id),
+    FOREIGN KEY (game_id)   references GAME(id),
+    FOREIGN KEY (player_id)   references PLAYER(id)
 );
