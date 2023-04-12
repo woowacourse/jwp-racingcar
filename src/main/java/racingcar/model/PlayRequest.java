6c558a41ae81c56596f9f388a3cb9c00d300c343
LJW25 <dbjwlee@gmail.com>
2023-04-12 13:38:29 +0900

    feat: PlayRequest 생성
    Co-authored-by: echo <echo.backend@gmail.com>

---
+++ src/main/java/racingcar/model/PlayRequest.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class PlayRequest {
+    private final String names;
+    private final int count;
+
+    public PlayRequest(final String names, final int count) {
+        this.names = names;
+        this.count = count;
+    }
+}
