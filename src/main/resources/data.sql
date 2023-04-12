7fe469950015c2b74f58a6e5cdd13d50d04601f1
jeomxon <jeomxon@gmail.com>
2023-04-12 14:43:15 +0900

    feat: 데이터 저장을 위한 테이블 sql문 작성
    Co-authored-by: HyunseoPark <goatsfish@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,15 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+CREATE TABLE racing_game
+(
+    id         INT primary key NOT NULL AUTO_INCREMENT,
+    winners    VARCHAR(50)     NOT NULL,
+    created_at DATETIME        NOT NULL default current_timestamp,
+    trial      int             not null
+);
+create table player
+(
+    id             int primary key not null auto_increment,
+    name           varchar(10)     not null,
+    position       int             not null,
+    racing_game_id int             not null,
+    foreign key (racing_game_id) references racing_game (id)
 );
