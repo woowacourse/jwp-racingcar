67a1251d3fd9fbd37a8bd89abd63d9098ca7db74
donghae <donghae1999@gmail.com>
2023-04-12 16:55:55 +0900

    docs: sql data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,15 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
 CREATE TABLE PLAY_RESULT (
     id          INT         NOT NULL AUTO_INCREMENT,
+    trial_count INT         NOT NULL,
     winners     VARCHAR(50) NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE CAR_RESULT (
+    play_result_id  INT         NOT NULL,
+    car_name        VARCHAR(5)  NOT NULL,
+    car_position    INT         NOT NULL,
+    PRIMARY KEY (play_result_id, car_name),
+    FOREIGN KEY (play_result_id) REFERENCES PLAY_RESULT(id)
 );
