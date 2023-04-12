66100fceacb33838d650c0a0c5d6283fc1cdf48d
woosung1223 <dntjd991223@naver.com>
2023-04-12 18:32:44 +0900

    feat: H2 의존성 추가 및 테이블 스키마 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,13 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
+CREATE TABLE GAME_RESULT (
+    id          INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    trial_count INT         NOT NULL,
     winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+    created_at  DATETIME    NOT NULL default current_timestamp
+);
+
+CREATE TABLE PLAYER_RESULT (
+    id          INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(50) NOT NULL,
+    position    INT         NOT NULL,
+    FOREIGN KEY (game_result_id) REFERENCES GAME_RESULT (`id`)
 );
