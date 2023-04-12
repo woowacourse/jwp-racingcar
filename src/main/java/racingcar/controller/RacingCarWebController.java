720f4889201f383f15988dbc9806ef500a1d8b7a
java-saeng <gusdn85554@naver.com>
2023-04-12 14:51:17 +0900

    feat : Validator 분리

--- src/main/java/racingcar/controller/RacingCarWebController.java
+++ src/main/java/racingcar/controller/RacingCarWebController.java
@@ -5,31 +5,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.controller.dto.GameInfoRequest;
 import racingcar.controller.dto.RaceResultResponse;
+import racingcar.controller.validator.GameOptionValidator;
 import racingcar.service.RaceResultService;
 
 @RestController
 public class RacingCarWebController {
 
-    private static final String CAR_NAMES_BLANK_ERROR = "[ERROR] 경주할 자동차 이름이 입력되지 않았습니다.";
-    private static final String TRY_NUM_NOT_POSITIVE_ERROR = "[ERROR] 시도할 횟수는 1 이상이어야 합니다.";
-
+    private final GameOptionValidator gameOptionValidator;
     private final RaceResultService raceResultService;
 
-    public RacingCarWebController(final RaceResultService raceResultService) {
+    public RacingCarWebController(final GameOptionValidator gameOptionValidator,
+                                  final RaceResultService raceResultService) {
+        this.gameOptionValidator = gameOptionValidator;
         this.raceResultService = raceResultService;
     }
 
     @PostMapping("/plays")
-    public RaceResultResponse registerRaceResult(@RequestBody GameInfoRequest gameInfoRequest) {
-        final String names = gameInfoRequest.getNames();
-        if (names.length() < 1) {
-            throw new IllegalArgumentException(CAR_NAMES_BLANK_ERROR);
-        }
+    public RaceResultResponse registerRaceResult(@RequestBody final GameInfoRequest gameInfoRequest) {
 
-        final int trialCount = gameInfoRequest.getCount();
-        if (trialCount <= 0) {
-            throw new IllegalArgumentException(TRY_NUM_NOT_POSITIVE_ERROR);
-        }
+        gameOptionValidator.validateGameOption(gameInfoRequest);
 
         return raceResultService.createRaceResult(gameInfoRequest);
     }
