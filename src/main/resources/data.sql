5be7c2e560b902326e926ab29f641b81d83fe98d
kiarakim <kiarakim413@gmail.com>
2023-04-12 13:27:44 +0900

    feat: 쿼리 수정
    Co-authored-by: CFalws <akdlzmf1123@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,16 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+CREATE TABLE games
+(
+`gameId`        INT          NOT NULL AUTO_INCREMENT,
+`count`         INT          NOT NULL,
+`winner`       VARCHAR(100)  NOT NULL,
+`timeStamp`    TIMESTAMP     NOT NULL,
+PRIMARY KEY (`gameId`)
+);
+CREATE TABLE cars
+(
+`name`       VARCHAR(20) NOT NULL,
+`position`     INT       NOT NULL,
+`gameId`       INT       NOT NULL,
+PRIMARY KEY (`name`, `gameId`),
+FOREIGN KEY ( `gameId` ) REFERENCES `games` ( `gameId` )
 );
