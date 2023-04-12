d14af930c9e958268b0532369a50446d142e4f71
dooboocookie <bkh3030@gmail.com>
2023-04-12 17:29:56 +0900

    feat(PlayerDao): 이름으로 조회하는 기능 추가

--- src/main/java/racingcar/dao/PlayerDao.java
+++ src/main/java/racingcar/dao/PlayerDao.java
@@ -1,13 +1,17 @@
 package racingcar.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import racingcar.dto.PlayerDto;
 
 import java.sql.PreparedStatement;
 import java.util.Objects;
+import java.util.Optional;
 
 @Repository
 public class PlayerDao {
@@ -19,6 +23,11 @@ public class PlayerDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    private final RowMapper<PlayerDto> actorRowMapper = (resultSet, rowNum) -> new PlayerDto(
+            resultSet.getLong("id"),
+            resultSet.getString("name")
+    );
+
     public Long save(final String name) {
         final String sql = "INSERT INTO PLAYER(name) VALUES(?) ";
         KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
@@ -28,5 +37,15 @@ public class PlayerDao {
             return preparedStatement;
         }, generatedKeyHolder);
         return Objects.requireNonNull(generatedKeyHolder.getKey()).longValue();
+    }
+
+    public Optional<PlayerDto> findByName(final String name) {
+        final String sql = "SELECT * FROM PLAYER WHERE name = ? ";
+        try {
+            PlayerDto playerDto = jdbcTemplate.queryForObject(sql, actorRowMapper, name);
+            return Optional.ofNullable(playerDto);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 }
