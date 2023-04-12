f8719a0ffef8a9fe4c78610beb0d0fc7a50548c0
kokodak <kokodakadokok@gmail.com>
2023-04-12 14:57:05 +0900

    docs: JDBC, H2 의존성 추가 및 DDL 작성
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,24 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
+CREATE TABLE users
+(
+    id   BIGINT      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE play_result
+(
+    id          BIGINT      NOT NULL AUTO_INCREMENT,
+    trial_count INT         NOT NULL,
     winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
+    created_at  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users_play_result
+(
+    id               BIGINT NOT NULL AUTO_INCREMENT,
+    users_id         BIGINT NOT NULL,
+    player_result_id BIGINT NOT NULL,
+    position         INT    NOT NULL,
     PRIMARY KEY (id)
 );
