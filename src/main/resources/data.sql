958f8d1f114b5e85b387b835e9a96610bb42ff68
thdwoqor <celebarte01@gmail.com>
2023-04-12 17:02:16 +0900

    chore : sql 위치 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,22 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+CREATE TABLE player
+(
+    name varchar(30) PRIMARY KEY
+);
+
+CREATE TABLE game
+(
+    id          int PRIMARY KEY AUTO_INCREMENT,
+    trial_count int                                 NOT NULL,
+    game_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE record
+(
+    game_id     int,
+    position    int     NOT NULL,
+    is_winner   boolean NOT NULL,
+    player_name varchar(30),
+    PRIMARY KEY (game_id, player_name),
+    FOREIGN KEY (game_id) REFERENCES game (id),
+    FOREIGN KEY (player_name) REFERENCES player (name)
 );
