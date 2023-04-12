ce0f362a7a178cfdcaf6406f5856645bf4193e61
Cyma-s <ssol0319@naver.com>
2023-04-12 17:28:53 +0900

    refactor(WinnerInsertDao): 구현 세부 로직 변경

--- src/main/java/racingcar/WinnerInsertDao.java
+++ src/main/java/racingcar/WinnerInsertDao.java
@@ -1,16 +1,25 @@
 package racingcar;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class WinnerInsertDao {
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     public void insertWinner(int gameId, int playerId) {
-        String insertWinnerSql = "INSERT INTO winners(game_id, player_id) VALUES(?, ?)";
-        jdbcTemplate.update(insertWinnerSql, gameId, playerId);
+        String insertWinnerSql = "INSERT INTO winners(game_id, player_id) VALUES(:game_id, :player_id)";
+
+        SqlParameterSource namedParameters = new MapSqlParameterSource(Map.of(
+                "game_id", gameId,
+                "player_id", playerId
+        ));
+
+        jdbcTemplate.update(insertWinnerSql, namedParameters);
     }
 }
