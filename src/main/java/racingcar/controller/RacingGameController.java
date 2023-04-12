09f672f6f57987b4236c46a64aacb7f28cdcf63a
jeomxon <jeomxon@gmail.com>
2023-04-12 13:56:17 +0900

    feat: 웹 응답기능 구현
    Co-authored-by: HyunseoPark <goatsfish@gmail.com>

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import racingcar.controller.request.RacingInfoRequest;
+import racingcar.controller.dto.RacingInfoRequest;
+import racingcar.controller.dto.RacingInfoResponse;
 import racingcar.domain.CarGroup;
 import racingcar.domain.RacingGame;
 import racingcar.domain.RacingResult;
@@ -15,7 +16,7 @@ import racingcar.domain.RandomNumberGenerator;
 public class RacingGameController {
 
     @PostMapping("/plays")
-    public ResponseEntity<RacingResult> getPlayerNames(@RequestBody RacingInfoRequest request) {
+    public ResponseEntity<RacingInfoResponse> getPlayerNames(@RequestBody RacingInfoRequest request) {
         final String[] carNames = request.getNames().split(",");
         final CarGroup carGroup = new CarGroup(carNames);
 
@@ -25,6 +26,8 @@ public class RacingGameController {
         }
 
         final RacingResult racingResult = racingGame.produceRacingResult();
-        return ResponseEntity.ok(racingResult);
+
+        final RacingInfoResponse racingInfoResponse = new RacingInfoResponse(racingResult.pickWinner(), carGroup.getCars());
+        return ResponseEntity.ok(racingInfoResponse);
     }
 }
