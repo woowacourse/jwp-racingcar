3ec007c3cf16e568ab8dacdf2e2be4c5b6784ed3
kiarakim <kiarakim413@gmail.com>
2023-04-12 14:45:16 +0900

    feat: 이력조회 기능 구현
    Co-authored-by: CFalws <akdlzmf1123@gmail.com>

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -2,6 +2,9 @@ package racingcar.controller;
 
 import static racingcar.dto.DtoMapper.*;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,16 @@ import racingcar.domain.Cars;
 import racingcar.domain.RacingGame;
 import racingcar.dto.NamesAndCountDto;
 import racingcar.dto.ResultDto;
+import racingcar.jdbc.RacingCarDao;
 
 @RestController
 public class RacingCarController {
+	private final RacingCarDao racingCarDao = new RacingCarDao();
+
+	@GetMapping("/plays")
+	public List<ResultDto> findData(){
+		return racingCarDao.find();
+	}
 
 	@PostMapping("/plays")
 	public ResultDto createData(@RequestBody NamesAndCountDto namesAndCountDto) {
@@ -23,7 +33,7 @@ public class RacingCarController {
 		startRacing(count, racingGame);
 
 		Cars cars = racingGame.getCars();
-
+		racingCarDao.insertCar(cars, count);
 		return toResultDto(cars);
 	}
 
