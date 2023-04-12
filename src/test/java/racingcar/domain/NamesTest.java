02eef3d398a1a70191816e1d37b4d08986d52188
songusika <sws981202@gmail.com>
2023-04-12 15:50:41 +0900

    refactor: names 리택터링으로 인한 테스트 코드 수정

--- src/test/java/racingcar/domain/NamesTest.java
+++ src/test/java/racingcar/domain/NamesTest.java
@@ -14,15 +14,13 @@ class NamesTest {
     @Test
     @DisplayName("0명이면 익셉션이 발생한다.")
     void throwExceptionWhenNoNameInput() {
-        assertThatThrownBy(() -> new Names(new ArrayList<>()))
+        assertThatThrownBy(() -> new Names(" "))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("1명이상이 참여하면 익셉션이 발생하지 않는다.")
     void notThrowExceptionWhenOneMoreName() {
-        assertDoesNotThrow(() -> new Names(new ArrayList<>(List.of(
-                "pobi", "honux", "bkcat"
-        ))));
+        assertDoesNotThrow(() -> new Names("pobi, honux, bkcat"));
     }
 }
