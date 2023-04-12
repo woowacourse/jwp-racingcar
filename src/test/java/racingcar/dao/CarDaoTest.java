978831ee917af1644d8041d71b80f66550688369
hanueleee <ezzeezz@naver.com>
2023-04-12 18:04:46 +0900

    test(CarDao): 자동차 정보 저장, 우승자 탐색 테스트 추가

--- src/test/java/racingcar/dao/CarDaoTest.java
+++ src/test/java/racingcar/dao/CarDaoTest.java
@@ -18,6 +18,8 @@ class CarDaoTest {
     private CarDao carDao;
     private GameDao gameDao;
 
+    private List<RacingCarResultDto> cars;
+
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -25,26 +27,41 @@ class CarDaoTest {
     void setUp() {
         carDao = new CarDao(jdbcTemplate);
         gameDao = new GameDao(jdbcTemplate.getJdbcTemplate().getDataSource());
-        gameDao.save(1);
-        gameDao.save(2);
+
     }
 
     @Test
     void 자동차_저장_테스트() {
         //given
-        RacingCar racingCar1 = new RacingCar("오잉");
-        RacingCar racingCar2 = new RacingCar("포이");
-        RacingCar racingCar3 = new RacingCar("말랑");
-        List<RacingCarResultDto> cars = List.of(
-                RacingCarResultDto.of(racingCar1, GameResult.WIN.getValue(), 1),
-                RacingCarResultDto.of(racingCar2, GameResult.WIN.getValue(), 1),
-                RacingCarResultDto.of(racingCar3, GameResult.WIN.getValue(), 2));
-
+        long id1 = gameDao.save(1);
+        long id2 = gameDao.save(2);
+        cars = List.of(
+                RacingCarResultDto.of(new RacingCar("오잉"), GameResult.LOSE.getValue(), id1),
+                RacingCarResultDto.of(new RacingCar("포이"), GameResult.WIN.getValue(), id1),
+                RacingCarResultDto.of(new RacingCar("말랑"), GameResult.WIN.getValue(), id2));
         //when
         carDao.saveAll(cars);
-        List<RacingCarDto> queriedCars = carDao.findCarsById(1);
+        List<RacingCarDto> queriedCars = carDao.findCarsById(id1);
 
         //then
         assertThat(queriedCars).hasSize(2);
+    }
+
+    @Test
+    void 우승자_탐색_테스트() {
+        //given
+        long id1 = gameDao.save(1);
+        long id2 = gameDao.save(2);
+        cars = List.of(
+                RacingCarResultDto.of(new RacingCar("오잉"), GameResult.LOSE.getValue(), id1),
+                RacingCarResultDto.of(new RacingCar("포이"), GameResult.WIN.getValue(), id1),
+                RacingCarResultDto.of(new RacingCar("말랑"), GameResult.WIN.getValue(), id2));
+        carDao.saveAll(cars);
+
+        //when
+        List<String> winners = carDao.findWinnersById(id1);
+
+        //then
+        assertThat(winners).containsExactly("포이");
     }
 }
