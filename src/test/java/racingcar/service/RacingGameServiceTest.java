bec6d3ca065d10bc3ae366a3053ba27257f962c0
drunkenhw <drunkenhw@gmail.com>
2023-04-12 15:45:37 +0900

    test: service test 리팩터링

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -7,29 +7,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.AlwaysMoveNumberGenerator;
-import racingcar.dao.CarDao;
-import racingcar.dao.RacingGameDao;
 import racingcar.dto.RacingGameRequest;
 import racingcar.dto.RacingGameResponse;
 
 @Import(value = AlwaysMoveNumberGenerator.class)
+@Transactional
 @SpringBootTest
 class RacingGameServiceTest {
 
     @Autowired
-    CarDao carDao;
-
-    @Autowired
-    RacingGameDao racingGameDao;
+    RacingGameService racingGameService;
 
     @Test
     void playTest() {
         RacingGameRequest racingGameRequest = new RacingGameRequest("박스터,엔초", 10);
         List<String> winners = List.of("박스터", "엔초");
 
-        RacingGameService racingGameService = new RacingGameService(carDao, racingGameDao,
-                new AlwaysMoveNumberGenerator());
         RacingGameResponse play = racingGameService.play(racingGameRequest);
 
         assertThat(play.getWinners()).isEqualTo(winners);
