78aa1f923d0e7b7d3799de505a124541b8bf77e7
jiwon <ljwon77@daum.net>
2023-04-12 19:09:24 +0900

    feat: 자동차 정보를 저장 및 조회한다
    Co-authored-by: younghoondoodoom <dudgns0330@gmail.com>

---
+++ src/main/java/racingcar/domain/dao/entity/CarEntity.java
@@ -0,0 +1,17 @@
+package racingcar.domain.dao.entity;
+
+public class CarEntity {
+    private final Long carId;
+    private final String name;
+    private final int position;
+
+    public CarEntity(final Long carId, final String name, final int position) {
+        this.carId = carId;
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/racingcar/domain/dao/CarDaoTest.java
@@ -0,0 +1,69 @@
+package racingcar.domain.dao;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import racingcar.domain.Car;
+import racingcar.domain.dao.entity.CarEntity;
+
+@SpringBootTest
+@Transactional
+class CarDaoTest {
+
+    @Autowired
+    private CarDao carDao;
+    @Autowired
+    private RaceResultDao raceResultDao;
+
+    @Test
+    @DisplayName("차들의 정보를 저장한다")
+    public void testSaveAll() {
+        //given
+        final int trialCount = 10;
+        final String winners = "test1,test2";
+        final Long raceResultId = raceResultDao.save(trialCount, winners);
+        final Car car1 = Car.create("test1");
+        final Car car2 = Car.create("test2");
+        final List<Car> cars = List.of(car1, car2);
+
+        //when
+        carDao.saveAll(raceResultId, cars);
+
+        // then
+        final List<CarEntity> result = carDao.findAll(raceResultId);
+        final List<String> resultNames = result.stream().map(CarEntity::getName)
+            .collect(Collectors.toUnmodifiableList());
+
+        // then
+        assertThat(resultNames)
+            .isEqualTo(List.of("test1", "test2"));
+    }
+
+    @Test
+    @DisplayName("모든 차 리스트를 반환한다")
+    public void findAll() {
+        // given
+        final int trialCount = 10;
+        final String winners = "test1,test2";
+        final Long raceResultId = raceResultDao.save(trialCount, winners);
+        final Car car1 = Car.create("test1");
+        final Car car2 = Car.create("test2");
+        final List<Car> cars = List.of(car1, car2);
+        carDao.saveAll(raceResultId, cars);
+
+        // when
+        final List<CarEntity> result = carDao.findAll(raceResultId);
+        final List<String> resultNames = result.stream().map(CarEntity::getName)
+            .collect(Collectors.toUnmodifiableList());
+
+        // then
+        assertThat(resultNames)
+            .isEqualTo(List.of("test1", "test2"));
+    }
+}
