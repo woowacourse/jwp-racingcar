5f1a8f7700b3cc011ac729df7d7fb557b891ecc3
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-04-12 16:05:31 +0900

    refactor(db): 테이블 초기화 방식 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,16 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS cars (
+    car_id bigint NOT NULL AUTO_INCREMENT,
+    car_name VARCHAR(8) NOT NULL,
+    step int(3) NOT NULL,
+    winner boolean default false,
+    game_id bigint not null,
+    PRIMARY KEY (car_id)
+);
+
+CREATE TABLE IF NOT EXISTS games (
+    game_id bigint NOT NULL AUTO_INCREMENT,
+    count int(3) NOT NULL,
+    create_time timestamp NOT NULL,
+    PRIMARY KEY (game_id)
 );
