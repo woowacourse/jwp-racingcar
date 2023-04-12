a4c28d4b4805a5035c7befefd45e501bf53740dc
donghae <donghae1999@gmail.com>
2023-04-12 17:14:07 +0900

    refactor: Service 생성자 의존성 주입 추가

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -7,6 +7,17 @@ import racingcar.util.RandomNumberGenerator;
 @Service
 public class GameService {
 
+    private final NumberGenerator numberGenerator;
+    private final GameDao gameDao;
+    private final CarDao carDao;
+
+    @Autowired
+    public GameService(NumberGenerator numberGenerator, CarDao carDao, GameDao gameDao) {
+        this.numberGenerator = numberGenerator;
+        this.gameDao = gameDao;
+        this.carDao = carDao;
+    }
+
     public void moveCars(Cars cars, int tryCount) {
         for (int count = 0; count < tryCount; count++) {
             cars.moveResult(numberGenerator);
