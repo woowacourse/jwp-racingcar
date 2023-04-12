826520ecb02277ff3186490bb1476f63059d5ec3
iamjooon2 <iamjooon2@gmail.com>
2023-04-12 20:57:39 +0900

    refactor: 메서드 시그니처 변경
    Co-authored-by: Combi153 <jcm04@naver.com>

--- src/main/java/racingcar/dao/CarDao.java
+++ src/main/java/racingcar/dao/CarDao.java
@@ -2,5 +2,5 @@ package racingcar.dao;
 
 public interface CarDao {
 
-    void insert(final String name, final int position, final int gameId, final boolean isWin);
+    void insert(final String name, final int position, final Long gameId, final boolean isWin);
 }
