616d3c9426c3339b426173439b6cada6a44528cf
LJW25 <dbjwlee@gmail.com>
2023-04-12 16:07:26 +0900

    chore: H2 DB 연동
    Co-authored-by: echo <echo.backend@gmail.com>

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,30 +1,13 @@
 package racingcar;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class RacingCarApplication implements CommandLineRunner {
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+public class RacingCarApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(RacingCarApplication.class, args);
     }
 
-    @Override
-    public void run(final String... args) throws Exception {
-        jdbcTemplate.execute("DROP TABLE racing_game IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE racing_game (" +
-                "id INT AUTO_INCREMENT PRIMARY KEY, trial_count INT, winners VARCHAR(255), play_time DATETIME DEFAULT CURRENT_TIMESTAMP)");
-
-
-        jdbcTemplate.execute("DROP TABLE racing_car IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE racing_car (" +
-                "id INT AUTO_INCREMENT PRIMARY_KEY, name VARCHAR(5), position INT, racing_game_id INT, FOREIGN KEY (racing_game_id) REFERENCES racing_game(id) ON DELETE CASCADE)");
-    }
 }
