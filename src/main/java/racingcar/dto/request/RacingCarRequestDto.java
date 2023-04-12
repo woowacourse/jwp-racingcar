13deceb3ecc04c5dfd4388aa81330d558ac9452b
woo-chang <clllickme@naver.com>
2023-04-12 14:44:28 +0900

    feat: 자동차 경주 게임 요청 형식 구현
    Co-authored-by: aiaiaiai1 <cvc100326@naver.com>

---
+++ src/main/java/racingcar/dto/request/RacingCarRequestDto.java
@@ -0,0 +1,15 @@
+package racingcar.dto.request;
+
+public class RacingCarRequestDto {
+
+    private String names;
+    private Integer count;
+
+    public String getNames() {
+        return names;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+}
