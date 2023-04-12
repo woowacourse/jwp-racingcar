3ed52e1304e8f7257991c545b6103ddfc1f01201
waterricecake <fruturum@nate.com>
2023-04-12 16:28:37 +0900

    test: winners insert test

---
+++ src/test/java/racingcar/dao/WinnersDAOTest.java
@@ -0,0 +1,34 @@
+package racingcar.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.NONE)
+public class WinnersDAOTest {
+    @Autowired
+    private WinnersDAO winnersDAO;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        winnersDAO = new WinnersDAO(jdbcTemplate);
+
+        jdbcTemplate.execute("DROP TABLE winners IF EXISTS");
+        jdbcTemplate.execute("create table winners\n" +
+                "(\n" +
+                "    game_number Integer,\n" +
+                "    winner      varchar(20)" +
+                ")");
+    }
+    @Test
+    void insert() {
+        assertThatNoException().isThrownBy(()-> winnersDAO.insert(5, "달리"));
+    }
+}
