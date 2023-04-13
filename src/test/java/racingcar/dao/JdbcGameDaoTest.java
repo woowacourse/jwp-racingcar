2f5d03c26df986a6673c3a2b0dacf2bef84bb83e
dev_kong <ahdjd5@gmail.com>
2023-04-13 16:49:55 +0900

    test: GameDao 테스트코드 SpringBootTest -> JdbcTest

--- src/test/java/racingcar/dao/JdbcGameDaoTest.java
+++ src/test/java/racingcar/dao/JdbcGameDaoTest.java
@@ -2,22 +2,26 @@ package racingcar.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@JdbcTest
 class JdbcGameDaoTest {
-    JdbcGameDao jdbcGameDao;
-    JdbcTemplate jdbcTemplate;
-
     @Autowired
-    public JdbcGameDaoTest(JdbcGameDao jdbcGameDao, JdbcTemplate jdbcTemplate) {
-        this.jdbcGameDao = jdbcGameDao;
-        this.jdbcTemplate = jdbcTemplate;
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    DataSource dataSource;
+    JdbcGameDao jdbcGameDao;
+
+    @BeforeEach
+    void setUp() {
+        jdbcGameDao = new JdbcGameDao(dataSource);
     }
 
     @Test
