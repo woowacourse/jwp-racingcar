fbce168ec8cf507fcb5653b2cc363cd5d0db00c6
iamjooon2 <iamjooon2@gmail.com>
2023-04-13 11:52:06 +0900

    test: JdbcCarDao 테스트 추가
    Co-authored-by: Combi153 <jcm04@naver.com>

---
+++ src/test/java/racingcar/dao/JdbcCarDaoTest.java
@@ -0,0 +1,43 @@
+package racingcar.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+//@JdbcTest
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class JdbcCarDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private CarDao carDao;
+    private GameDao gameDao;
+
+    @BeforeEach
+    void setUp() {
+        carDao = new JdbcCarDao(jdbcTemplate);
+        gameDao = new JdbcGameDao(jdbcTemplate);
+    }
+
+    @Test
+    void 게임_id와_함께_차량_정보를_저장한다() {
+        // given
+        final long gameId = gameDao.insert(5);
+        String sql = "insert into car (name, position, game_id, is_win) values (?,?,?,?)";
+
+        // when
+        final int result = jdbcTemplate.update(sql, "gavi", 3, gameId, true);
+
+        // then
+        assertThat(result).isNotNull();
+    }
+}
