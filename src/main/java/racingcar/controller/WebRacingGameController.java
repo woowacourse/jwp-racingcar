51fdc9843b1b1f7d741965a2a411726ddf0a6c8d
cpot5620 <=cpot5620@naver.com>
2023-04-13 11:22:34 +0900

    refactor(Controller): 예외 처리 로직 추가

--- src/main/java/racingcar/controller/WebRacingGameController.java
+++ src/main/java/racingcar/controller/WebRacingGameController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,6 @@ import racingcar.dto.GameInfoDto;
 import racingcar.dto.GameResultResponseDto;
 import racingcar.service.RacingGameService;
 
-// TODO: 2023-04-12 Service 계층으로 비즈니스 로직 분리
 @RestController
 public class WebRacingGameController {
     private final RacingGameService racingGameService;
@@ -22,5 +22,11 @@ public class WebRacingGameController {
         GameResultResponseDto response = racingGameService.play(request.getNames(), request.getCount());
 
         return ResponseEntity.ok(response);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleException(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
