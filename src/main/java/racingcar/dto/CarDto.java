5c9e81a0572602ba4d0cbc0bace18d87111d0db3
splitube <voicesplit@daum.net>
2023-04-13 11:25:07 +0900

    feat: CarDto 생성

---
+++ src/main/java/racingcar/dto/CarDto.java
@@ -0,0 +1,27 @@
+package racingcar.dto;
+
+import racingcar.domain.Car;
+
+public class CarDto {
+    private final String name;
+    private final int identifier;
+    private final int position;
+    
+    public CarDto(Car car) {
+        this.name = car.getName();
+        this.position = car.getPosition();
+        this.identifier = car.getIdentifier();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getIdentifier() {
+        return identifier;
+    }
+    
+    public int getPosition() {
+        return position;
+    }
+}
