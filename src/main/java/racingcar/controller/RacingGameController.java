b16ccca6203b7a0d79daee0e139d3c965eae38a4
splitube <voicesplit@daum.net>
2023-04-13 12:27:26 +0900

    refactor: 컨트롤러에 서비스멤버 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -7,16 +7,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.dto.GameInputDto;
 import racingcar.dto.RacingResultDto;
-import racingcar.game.Game;
-import racingcar.util.RandomNumberGenerator;
+import racingcar.service.RacingGameService;
 
 @RestController
 public class RacingGameController {
 
+    private final RacingGameService racingGameService;
+
+    public RacingGameController(RacingGameService racingGameService) {
+        this.racingGameService = racingGameService;
+    }
+
     @PostMapping(path = "/plays", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<RacingResultDto> play(@RequestBody GameInputDto gameInput) {
-        Game game = new Game(gameInput.getNames(), gameInput.getCount());
-        game.playGameWithoutPrint(new RandomNumberGenerator());
-        return ResponseEntity.ok(new RacingResultDto(game.getWinners(), game.getCars()));
+    public ResponseEntity<RacingResultDto> play(@RequestBody GameInputDto gameInputDto) {
+        RacingResultDto racingResultDto = racingGameService.playGameWithoutPrint(gameInputDto);
+        return ResponseEntity.ok(racingResultDto);
     }
 }
