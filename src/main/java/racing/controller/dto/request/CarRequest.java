8853d559e556412a770d0a682c619a631bb3c937
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-04-13 20:57:34 +0900

    Refactor: CarRequest 이름 변경 및 알맞은 위치의 directory로 이동

--- src/main/java/racing/controller/dto/request/CarRequest.java
+++ src/main/java/racing/controller/dto/request/CarRequest.java
@@ -1,22 +1,22 @@
-package racing;
+package racing.controller.dto.request;
 
 import racing.domain.Car;
 
-public class CarEntity {
+public class CarRequest {
     private final Long gameId;
     private final String carName;
     private final int step;
     private final boolean isWinner;
 
-    private CarEntity(Long gameId, String carName, int step, boolean isWinner) {
+    private CarRequest(Long gameId, String carName, int step, boolean isWinner) {
         this.gameId = gameId;
         this.carName = carName;
         this.step = step;
         this.isWinner = isWinner;
     }
 
-    public static CarEntity of(Long gameId, Car car, boolean isWinner) {
-        return new CarEntity(gameId, car.getName(), car.getStep(), isWinner);
+    public static CarRequest of(Long gameId, Car car, boolean isWinner) {
+        return new CarRequest(gameId, car.getName(), car.getStep(), isWinner);
     }
 
     public Long getGameId() {
