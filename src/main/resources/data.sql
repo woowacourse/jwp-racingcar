287a83b1b18776b16c2cb483811940ae0d63db3e
hgo641 <hgo641@gmail.com>
2023-04-13 14:50:39 +0900

    refactor: 테이블 생성 SQL문 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
 CREATE TABLE PLAY_RESULT
 (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
+    id          INT         NOT NULL UNSIGNED AUTO_INCREMENT,
+    winners     VARCHAR(120) NOT NULL,
     trial_count INT         NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
@@ -12,7 +12,8 @@ CREATE TABLE CAR_RESULT
 (
     id             INT         NOT NULL AUTO_INCREMENT,
     play_result_id INT         NOT NULL,
-    name           VARCHAR(50) NOT NULL,
+    name           VARCHAR(10) NOT NULL,
     position       INT         NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (play_result_id) REFERENCES PLAY_RESULT (id)
 );
