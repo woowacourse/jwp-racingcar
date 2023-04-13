ae3fa8f329cac3ad7a23bf3557579ff5ea242d87
jeomxon <jeomxon@gmail.com>
2023-04-13 14:38:42 +0900

    feat: 전역 예외 처리 컨트롤러 추가
    Co-authored-by: HyunseoPark <goatsfish@gmail.com>

---
+++ src/main/java/racingcar/controller/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package racingcar.controller.dto;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private final String message;
+    private final LocalDateTime timestamp;
+    private final int statusCode;
+
+    public ErrorResponse(final String message, final LocalDateTime timestamp, final int statusCode) {
+        this.message = message;
+        this.timestamp = timestamp;
+        this.statusCode = statusCode;
+    }
+}
