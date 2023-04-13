5bddfe9f84053d231897a3591aefd9cdd117e09e
Choi-JJunho <junho5336@gmail.com>
2023-04-13 14:31:31 +0900

    refactor: CarGameRequest 검증 추가

--- src/main/java/racingcar/dto/request/CarGameRequest.java
+++ src/main/java/racingcar/dto/request/CarGameRequest.java
@@ -1,12 +1,28 @@
 package racingcar.dto.request;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class CarGameRequest {
     private final String names;
     private final int count;
 
     public CarGameRequest(String names, int count) {
-        this.names = names;
+        validateCount(count);
+        this.names = trimName(names);
         this.count = count;
+    }
+
+    private String trimName(String names) {
+        return Arrays.stream(names.split(","))
+                .map(String::strip)
+                .collect(Collectors.joining(","));
+    }
+
+    private void validateCount(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("시도횟수는 0 이상여야합니다." + System.lineSeparator() + " count : " + count);
+        }
     }
 
     public int getCount() {
