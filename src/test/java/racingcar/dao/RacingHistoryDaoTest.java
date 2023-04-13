6050501fcf396de5b7bff8e2a337561a4448af87
somsom13 <somin1113@naver.com>
2023-04-13 14:06:03 +0900

    fix: 시간 비교 테스트에 시간 포맷 추가
    - 조회되는 밀리초의 길이가 다른 문제로 DateTimeFormatter 를 사용해 플레이 시간 포맷을 설정하여 비교하도록 수정

--- src/test/java/racingcar/dao/RacingHistoryDaoTest.java
+++ src/test/java/racingcar/dao/RacingHistoryDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @JdbcTest
 class RacingHistoryDaoTest {
+
     private RacingHistoryDao racingHistoryDao;
 
     @Autowired
@@ -30,19 +32,20 @@ class RacingHistoryDaoTest {
         //given
         int trialCount = 10;
         LocalDateTime now = LocalDateTime.now();
-
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss");
         //when
         Long savedId = racingHistoryDao.save(trialCount, now);
         //then
         RacingHistory racingHistory = jdbcTemplate.queryForObject(
-                "SELECT trial_count, play_time FROM racing_game WHERE id = :id",
-                new MapSqlParameterSource("id", savedId),
-                (rs, rowNum) -> new RacingHistory(rs.getInt("trial_count"),
-                        rs.getTimestamp("play_time").toLocalDateTime()));
+            "SELECT trial_count, play_time FROM racing_game WHERE id = :id",
+            new MapSqlParameterSource("id", savedId),
+            (rs, rowNum) -> new RacingHistory(rs.getInt("trial_count"),
+                rs.getTimestamp("play_time").toLocalDateTime()));
 
         assertAll(
-                () -> assertThat(racingHistory.getPlayTime()).isEqualTo(now),
-                () -> assertThat(racingHistory.getTrialCount()).isEqualTo(trialCount)
+            () -> assertThat(racingHistory.getPlayTime().format(timeFormatter)).isEqualTo(
+                now.format(timeFormatter)),
+            () -> assertThat(racingHistory.getTrialCount()).isEqualTo(trialCount)
         );
 
     }
