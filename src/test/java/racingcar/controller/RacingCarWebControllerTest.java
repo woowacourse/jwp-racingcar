8b42d25fb7d15ffa36d28756b9e927a66346eb40
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-04-13 11:44:36 +0900

    test : post 테스트 추가

---
+++ src/test/java/racingcar/controller/RacingCarWebControllerTest.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import racingcar.dto.RacingGameRequestDto;
+
+import static org.hamcrest.core.Is.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RacingCarWebControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("이름과 시도 횟수를 보내면 결과로 우승자와 참가자(이름,포지션)을 반환한다")
+    @Test
+    void return_result_given_names_and_trial_count() {
+        RacingGameRequestDto racingGameRequestDto = new RacingGameRequestDto("마코,아코", 10);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(racingGameRequestDto)
+                .when().post("/plays")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", is(2));
+    }
+
+}
