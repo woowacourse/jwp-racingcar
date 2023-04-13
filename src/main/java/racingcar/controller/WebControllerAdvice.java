d1bbe0b1abcf3af8a14cdaf6c8f5f3389ddc1da6
BackFoxx <backfoxxx@gmail.com>
2023-04-13 14:10:04 +0900

    refactor: 서버에서 작업 결과에 알맞는 status code를 반환하고, 클라이언트에서 status에 따라 다르게 작업하도록 변경

--- src/main/java/racingcar/controller/WebControllerAdvice.java
+++ src/main/java/racingcar/controller/WebControllerAdvice.java
@@ -1,12 +1,15 @@
 package racingcar.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import racingcar.dto.ExceptionMessageDTO;
 
 @RestControllerAdvice
 public class WebControllerAdvice {
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionMessageDTO sendExceptionMessage(IllegalArgumentException ex) {
         return new ExceptionMessageDTO(ex.getMessage());
     }
