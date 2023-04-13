f8c1c8dd9bd0567555169f1f6a9a8b130d041607
jeonghoon <shb03323@naver.com>
2023-04-13 11:19:00 +0900

    chore: sql ddl수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,20 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE PLAY_RESULT (
-    id          INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+create table game
+(
+    id          bigint auto_increment
+        primary key,
+    trial_count int                      not null,
+    winners     varchar(100)             not null,
+    created_at  datetime default (now()) not null
+);
+
+create table player_result
+(
+    id             bigint auto_increment
+        primary key,
+    name           varchar(10) not null,
+    final_position int         not null,
+    game_id        bigint      not null,
+    constraint player_result_game_id_fk
+        foreign key (game_id) references game (id)
 );
