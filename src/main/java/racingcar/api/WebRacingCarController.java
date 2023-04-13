023a98a7fe7a62c3941539b8add7c15bf64df31e
Choi-JJunho <junho5336@gmail.com>
2023-04-13 14:36:56 +0900

    refactor: controller 축약

--- src/main/java/racingcar/api/WebRacingCarController.java
+++ src/main/java/racingcar/api/WebRacingCarController.java
@@ -4,16 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import racingcar.domain.CarRandomNumberGenerator;
-import racingcar.domain.Cars;
-import racingcar.domain.RacingGame;
 import racingcar.dto.request.CarGameRequest;
 import racingcar.dto.response.CarGameResponse;
 import racingcar.service.RacingGameService;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class WebRacingCarController {
@@ -26,9 +19,7 @@ public class WebRacingCarController {
 
     @PostMapping("/plays")
     public ResponseEntity<CarGameResponse> plays(@RequestBody CarGameRequest request) {
-        List<String> names = Arrays.stream(request.getNames().split(",")).collect(Collectors.toList());
-        RacingGame game = new RacingGame(new CarRandomNumberGenerator(), new Cars(names), request.getCount());
-        CarGameResponse result = racingGameService.play(game);
+        CarGameResponse result = racingGameService.play(request);
         return ResponseEntity.ok(result);
     }
 }
