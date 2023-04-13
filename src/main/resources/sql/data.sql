2f1278b89af9c38e5fe5acfe813e70d7a8d06665
java-saeng <gusdn85554@naver.com>
2023-04-13 16:49:24 +0900

    feat : mock data 추가 및 data 초기화 path 변경

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,17 @@
+insert into RACE_RESULT
+values (1, 10, '빙봉', current_timestamp);
+insert into CAR
+values (1, '우르', 9, 1);
+insert into CAR
+values (2, '빙봉', 10, 1);
+
+insert into RACE_RESULT
+values (2, 5, 'a,b,c,d', current_timestamp);
+insert into CAR
+values (3, 'a', 5, 2);
+insert into CAR
+values (4, 'b', 5, 2);
+insert into CAR
+values (5, 'c', 5, 2);
+insert into CAR
+values (6, 'd', 5, 2);
