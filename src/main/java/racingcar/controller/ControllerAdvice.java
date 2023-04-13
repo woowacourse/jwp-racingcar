c26c4f01f4df7c7df811fa9d8bdcc79739e6ecf4
BackFoxx <backfoxxx@gmail.com>
2023-04-13 14:21:28 +0900

    refactor: 컨트롤러 어드바이스를 적용할 컨트롤러를 특정

--- src/main/java/racingcar/controller/ControllerAdvice.java
+++ src/main/java/racingcar/controller/ControllerAdvice.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import racingcar.dto.ExceptionMessageDTO;
 
-@RestControllerAdvice
-public class WebControllerAdvice {
+@RestControllerAdvice(assignableTypes = {RacingController.class})
+public class ControllerAdvice {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionMessageDTO sendExceptionMessage(IllegalArgumentException ex) {
