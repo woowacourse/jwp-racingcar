e4600e775ef29e1004291c4dbc5920b5fed78573
BackFoxx <backfoxxx@gmail.com>
2023-04-13 20:57:53 +0900

    refactor: 잘못된 사용자 입력 외의 예외가 발생했을 때 응답할 내용 정의

--- src/main/java/racingcar/controller/ControllerAdvice.java
+++ src/main/java/racingcar/controller/ControllerAdvice.java
@@ -8,9 +8,15 @@ import racingcar.dto.ExceptionMessageDTO;
 
 @RestControllerAdvice(assignableTypes = {RacingController.class})
 public class ControllerAdvice {
-    @ExceptionHandler
+    @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionMessageDTO sendExceptionMessage(IllegalArgumentException ex) {
         return new ExceptionMessageDTO(ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionMessageDTO sendDefaultExceptionMessage() {
+        return new ExceptionMessageDTO("서버가 응답하지 않습니다.");
     }
 }
