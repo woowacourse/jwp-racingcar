da42ea53ca76cc4e813e9a2092dd82d602be8204
dev_kong <ahdjd5@gmail.com>
2023-04-13 12:36:33 +0900

    test: test 코드 수정

--- src/test/java/racingcar/services/GameServiceTest.java
+++ src/test/java/racingcar/services/GameServiceTest.java
@@ -2,35 +2,26 @@ package racingcar.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-
-import javax.sql.DataSource;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import racingcar.dao.GameDao;
 import racingcar.model.car.Car;
 import racingcar.model.car.Cars;
-import racingcar.model.manager.ThresholdCarMoveManager;
 
+@SpringBootTest
 class GameServiceTest {
 
+    @Autowired
     GameService gameService;
+    @Autowired
     Cars cars;
 
-//    @BeforeEach
-//    public void setUp() {
-//        cars = new Cars(new ArrayList<>());
-////        gameService = new GameService(cars, new ThresholdCarMoveManager(), new GameDao(new JdbcTemplate(), DataSource new ));
-//    }
-//
-//    @Test
-//    void createCars() {
-//        String namesInput = "폴로,이리내";
-//        gameService.createCars(namesInput);
-//
-//        assertThat(cars.getCurrentResult()).containsExactlyInAnyOrder(new Car("폴로"), new Car("이리내"));
-//    }
+    @Test
+    void createCars() {
+        String namesInput = "폴로,이리내";
+        gameService.createCars(namesInput);
+
+        assertThat(cars.getCurrentResult()).containsExactlyInAnyOrder(new Car("폴로"), new Car("이리내"));
+    }
 }
