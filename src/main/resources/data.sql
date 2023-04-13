6e01a07c417acf1c00d1a565241a2e6bb88006eb
yeonkkk <tjddus0302@gmail.com>
2023-04-13 15:17:32 +0900

    chore: DDL 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-CREATE TABLE GAME (
+CREATE TABLE IF NOT EXISTS GAME (
     id          INT         NOT NULL AUTO_INCREMENT,
     trial_count  INT         NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE CAR (
+CREATE TABLE IF NOT EXISTS CAR (
     id          INT         NOT NULL AUTO_INCREMENT,
     name        VARCHAR(50) NOT NULL,
     position    INT         NOT NULL,
