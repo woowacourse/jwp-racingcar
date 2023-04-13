07e01c1ecc3b9e6dc4fa94f5e8d247c1a117897f
yujamint <woawoa0513@gmail.com>
2023-04-13 13:41:36 +0900

    test: DDL 수정에 따른 @BeforEach 세팅 제거

--- src/test/java/racingcar/dao/InMemoryGameDaoTest.java
+++ src/test/java/racingcar/dao/InMemoryGameDaoTest.java
@@ -1,33 +1,16 @@
 package racingcar.dao;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class InMemoryGameDaoTest {
 
     @Autowired
     private GameDao gameDao;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @BeforeEach
-    void setUp() {
-        jdbcTemplate.execute("DROP TABLE CAR_RESULT");
-        jdbcTemplate.execute("DROP TABLE PLAY_RESULT");
-        jdbcTemplate.execute("CREATE TABLE PLAY_RESULT (" +
-                "    id          INT         NOT NULL AUTO_INCREMENT," +
-                "    trial_count INT         NOT NULL," +
-                "    winners     VARCHAR(50) NOT NULL," +
-                "    created_at  DATETIME    NOT NULL default current_timestamp," +
-                "    PRIMARY KEY (id)" +
-                ");");
-    }
 
     @Test
     void save() {
