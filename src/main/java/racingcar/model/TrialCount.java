8a30909343573103e4b6613688586aa7b8a0ca77
kdkdhoho <hkim4410@naver.com>
2023-04-13 17:00:36 +0900

    feat(TrialCount): 시도 횟수 값 객체 구현

---
+++ src/main/java/racingcar/model/TrialCount.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class TrialCount {
+
+    private static final int MIN_VALUE = 0;
+
+    private final int trialCount;
+
+    public TrialCount(final int trialCount) {
+        validateRange(trialCount);
+        this.trialCount = trialCount;
+    }
+
+    private void validateRange(final int trialCount) {
+        if (trialCount <= MIN_VALUE) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다. 입력 : " + trialCount);
+        }
+    }
+
+    public int getTrialCount() {
+        return trialCount;
+    }
+}
