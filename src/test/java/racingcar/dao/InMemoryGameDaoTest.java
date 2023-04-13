069875bf2912a2205de83717b0de6b822d40f028
yujamint <woawoa0513@gmail.com>
2023-04-13 13:46:44 +0900

    test: @DisplayName 추가

--- src/test/java/racingcar/dao/InMemoryGameDaoTest.java
+++ src/test/java/racingcar/dao/InMemoryGameDaoTest.java
@@ -2,6 +2,7 @@ package racingcar.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ class InMemoryGameDaoTest {
     @Autowired
     private GameDao gameDao;
 
+    @DisplayName("게임 정보를 저장하며 id를 반환한다.")
     @Test
     void save() {
         String winner = "ocean";
