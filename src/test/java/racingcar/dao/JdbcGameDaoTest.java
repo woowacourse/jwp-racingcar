5c89dfcdf0da6b02af3a233d71584a12cf348134
iamjooon2 <iamjooon2@gmail.com>
2023-04-13 12:53:33 +0900

    chore: 사용하지 않는 코드 제거

--- src/test/java/racingcar/dao/JdbcGameDaoTest.java
+++ src/test/java/racingcar/dao/JdbcGameDaoTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-//@JdbcTest
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
