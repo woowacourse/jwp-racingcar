15c8f7cd4f4c922fbfc853a3d9393cda5e917408
Choi-JJunho <junho5336@gmail.com>
2023-04-13 14:34:13 +0900

    refactor: split 메소드 인자 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
     public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = SCANNER.nextLine();
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(",", -1))
                 .collect(Collectors.toList());
     }
 
