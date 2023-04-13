ab16c3700b3dfaad0188f66181947456c712df70
Rosie <dev.kayoung@gmail.com>
2023-04-13 11:52:33 +0900

    chore: DDL Drop문 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,11 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
--- TODO: 인메모리 DB를 사용할 때, 테이블이 어디에 저장되는가 (어플리케이션을 종료해도 데이터가 남아있는 이유)
-DROP TABLE car_record IF EXISTS;
-DROP TABLE racing_game IF EXISTS;
-
-CREATE TABLE racing_game(
+CREATE TABLE IF NOT EXISTS racing_game(
     id BIGINT NOT NULL AUTO_INCREMENT,
     trial_count INT,
     play_time DATETIME,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE car_record(
+CREATE TABLE IF NOT EXISTS car_record(
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     position INT,
