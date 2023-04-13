16ca5b16452c2020b9b0e6c307cc499b4ac00abe
70825 <hello70825@gmail.com>
2023-04-13 11:25:45 +0900

    refactor: 예외 메시지 상수화
    Co-authored-by: kim0914 <kosa0914@naver.com>
    Co-authored-by: This2sho <zx00018@naver.com>

--- src/main/java/racingcar/controller/RacingWebAdvice.java
+++ src/main/java/racingcar/controller/RacingWebAdvice.java
@@ -12,29 +12,35 @@ import racingcar.exception.InvalidTrialTimesFormatException;
 @RestControllerAdvice
 public class RacingWebAdvice {
 
+    private static final String DUPLICATE_CAR_NAMES_ERROR_MESSAGE = "중복된 차 이름이 존재합니다.";
+    private static final String EXCEED_CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 다섯 글자 이하여야 합니다.";
+    private static final String INVALID_CAR_NAME_FORMAT_ERROR_MESSAGE = "자동차 이름은 문자와 숫자만 가능합니다.";
+    private static final String INVALID_RANGE_TRIAL_TIMES_ERROR_MESSAGE = "시도 횟수는 1 이상 100 이하여야 합니다.";
+    private static final String INVALID_TRIAL_TIMES_FORMAT_ERROR_MESSAGE = "시도 횟수는 숫자만 입력 가능합니다.";
+
     @ExceptionHandler(DuplicateCarNamesException.class)
     public ResponseEntity<ExceptionInfo> duplicateCarNamesHandler() {
-        return ResponseEntity.badRequest().body(new ExceptionInfo("중복된 차 이름이 존재합니다."));
+        return ResponseEntity.badRequest().body(new ExceptionInfo(DUPLICATE_CAR_NAMES_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(ExceedCarNameLengthException.class)
     public ResponseEntity<ExceptionInfo> exceedCarNameLengthHandler() {
-        return ResponseEntity.badRequest().body(new ExceptionInfo("자동차 이름은 다섯 글자 이하여야 합니다."));
+        return ResponseEntity.badRequest().body(new ExceptionInfo(EXCEED_CAR_NAME_LENGTH_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(InvalidCarNameFormatException.class)
     public ResponseEntity<ExceptionInfo> invalidCarNameFormatHandler() {
-        return ResponseEntity.badRequest().body(new ExceptionInfo("자동차 이름은 문자와 숫자만 가능합니다."));
+        return ResponseEntity.badRequest().body(new ExceptionInfo(INVALID_CAR_NAME_FORMAT_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(InvalidRangeTrialTimesException.class)
     public ResponseEntity<ExceptionInfo> invalidRangeTrialTimesHandler() {
-        return ResponseEntity.badRequest().body(new ExceptionInfo("시도 횟수는 1 이상 100 이하여야 합니다."));
+        return ResponseEntity.badRequest().body(new ExceptionInfo(INVALID_RANGE_TRIAL_TIMES_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(InvalidTrialTimesFormatException.class)
-    public ResponseEntity<ExceptionInfo> InvalidTrialTimesFormatHandler() {
-        return ResponseEntity.badRequest().body(new ExceptionInfo("시도 횟수는 숫자만 입력 가능합니다."));
+    public ResponseEntity<ExceptionInfo> invalidTrialTimesFormatHandler() {
+        return ResponseEntity.badRequest().body(new ExceptionInfo(INVALID_TRIAL_TIMES_FORMAT_ERROR_MESSAGE));
     }
 
     static class ExceptionInfo {
