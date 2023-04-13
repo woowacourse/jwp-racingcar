0ad11f2e985a65854fcdbf4a2e28f3a32facf06d
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-13 09:43:53 +0900

    feat: 기존 테스트 추가
    Co-authored-by: the9kim <the9rla@gmail.com>

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarTest {
+    @ParameterizedTest(name = "Car 객체 생성 테스트 name = {0}")
+    @ValueSource(strings = {"pobi", "crong", "honux", " hi ", "  jj503   "})
+    void createCarTest(String name) {
+        Car car = new Car(new Name(name));
+        assertThat(car.getName()).isEqualTo(name.trim());
+    }
+
+    @ParameterizedTest(name = "Car 객체 생성 예외 테스트 name = {0}")
+    @ValueSource(strings = {"pobiiii", "", "  "})
+    void createExceptionCarTest(String name) {
+        assertThatThrownBy(() -> {
+            new Car(new Name(name));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @ParameterizedTest(name = "랜덤 값이 4미만일 경우 테스트 - 멈춤, randNum = {0}")
+    @ValueSource(ints = {0, 1, 2, 3})
+    void stopByRandom(int randNum) {
+        Car car = new Car(new Name("test"));
+        car.moveByNumber(randNum);
+
+        assertThat(car.getLocation()).isEqualTo(0);
+    }
+
+    @ParameterizedTest(name = "랜덤 값이 4미만일 경우 테스트 - 전진, randNum = {0}")
+    @ValueSource(ints = {4, 5, 6, 7})
+    void moveByRandom(int randNum) {
+        Car car = new Car(new Name("test"));
+        car.moveByNumber(randNum);
+
+        assertThat(car.getLocation()).isEqualTo(1);
+    }
+}
---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,76 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarsTest {
+    @Test
+    @DisplayName("Cars 객체 생성 테스트")
+    void createCarsTest() {
+        Cars cars = new Cars("pobi,crong,honux");
+
+        assertThat(cars.getCars().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Cars 객체 생성 시 중복으로 인한 예외 발생 테스트")
+    void createCarsExceptionTest() {
+        assertThatThrownBy(() -> {
+            new Cars("pobi,pobi,pobi");
+        }).isInstanceOf(IllegalArgumentException.class)
+          .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("단일 우승자인 경우 결과 테스트")
+    void getWinnerTest() {
+        String names = "ocean,jamie";
+        Cars cars = new Cars(names);
+
+        List<Integer> randomNumbers = new ArrayList<>(Arrays.asList(9, 0, 6, 7));
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(randomNumbers);
+
+        for (int i = 0; i < 2; i++) {
+            cars.moveForRound(testNumberGenerator);
+        }
+
+        assertThat(cars.getWinners()).containsExactly("ocean");
+    }
+
+    @Test
+    @DisplayName("다중 우승자인 경우 결과 테스트")
+    void getWinnersTest() {
+        String names = "ocean,jamie";
+        Cars cars = new Cars(names);
+
+        List<Integer> randomNumbers = new ArrayList<>(Arrays.asList(9, 9, 6, 6));
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(randomNumbers);
+
+        for (int i = 0; i < 2; i++) {
+            cars.moveForRound(testNumberGenerator);
+        }
+
+        assertThat(cars.getWinners()).containsExactly("ocean", "jamie");
+    }
+
+    static class TestNumberGenerator implements NumberGenerator {
+        private final List<Integer> numbers;
+
+        public TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generateNumber() {
+            return numbers.remove(0);
+        }
+    }
+}
