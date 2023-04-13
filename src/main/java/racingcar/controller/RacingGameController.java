be8db8907c28886eec3a7ee3ec65abc4756c2582
songusika <sws981202@gmail.com>
2023-04-13 16:39:16 +0900

    refactor: 원자성을 위한 @Transactional 어노테이션 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +29,7 @@ public class RacingGameController {
         this.racingCarDao = racingCarDao;
     }
 
+    @Transactional
     @PostMapping(value = "/plays", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<RacingCarResponse> play(@RequestBody final RacingCarRequest racingCarRequest) {
         RacingCars racingCars = createRacingCar(racingCarRequest);
