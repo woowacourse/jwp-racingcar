0451a2341c0f43208fceb368fcb4466728f72e82
hanueleee <ezzeezz@naver.com>
2023-04-13 11:11:48 +0900

    test(RacingGame): 게임 결과 계산 및 우승자 찾기 테스트 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,19 +1,23 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.dto.RacingCarDto;
 
-import java.util.stream.Stream;
-
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class RacingGameTest {
     @ParameterizedTest
     @MethodSource("전진_결과_데이터")
-    @DisplayName("range범위에 따른 전진 결과 테스트")
     public void 전진_결과_테스트(int num, int expectedPosition) {
         NumberGenerator numberGenerator = new DefaultNumberGenerator(num);
 
@@ -22,7 +26,7 @@ class RacingGameTest {
         RacingCarDto racingCarDto = RacingCarDto.from(racingGame.getStatus().get(0));
         Integer position = racingCarDto.getPosition();
 
-        Assertions.assertEquals(expectedPosition, position);
+        assertEquals(expectedPosition, position);
     }
 
     static Stream<Arguments> 전진_결과_데이터() {
@@ -30,5 +34,45 @@ class RacingGameTest {
                 Arguments.of(1, 1),
                 Arguments.of(9, 2)
         );
+    }
+
+    @Test
+    public void 결과_계산_테스트() {
+        RacingCar oing = new RacingCar("오잉");
+        oing.advance(9);
+        oing.advance(9);
+
+        RacingCar poi = new RacingCar("포이");
+        poi.advance(9);
+        poi.advance(1);
+
+        RacingGame racingGame = new RacingGame(List.of(oing, poi), new RandomNumberGenerator());
+
+        Map<RacingCar, GameResult> results = racingGame.calculateResult();
+        GameResult oingResult = results.get(oing);
+        GameResult poiResult = results.get(poi);
+
+        assertThat(oingResult).isEqualTo(GameResult.WIN);
+        assertThat(poiResult).isEqualTo(GameResult.LOSE);
+    }
+
+    @Test
+    public void 승자_찾기_테스트() {
+        RacingCar oing = new RacingCar("오잉");
+        oing.advance(9);
+        oing.advance(9);
+
+        RacingCar bree = new RacingCar("브리");
+        bree.advance(9);
+        bree.advance(9);
+
+        RacingCar poi = new RacingCar("포이");
+        poi.advance(9);
+        poi.advance(1);
+
+        RacingGame racingGame = new RacingGame(List.of(oing, poi, bree), new RandomNumberGenerator());
+        List<String> winners = racingGame.findWinningCarsName();
+
+        assertThat(winners).containsExactly("오잉", "브리");
     }
 }
