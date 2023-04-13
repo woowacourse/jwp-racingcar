b8701ab5363bc68f020d6eaa905ef969e67e091d
woo-chang <clllickme@naver.com>
2023-04-13 11:01:30 +0900

    refactor: ResponseEntity 사용하도록 수정
    Co-authored-by: aiaiaiai1 <cvc100326@naver.com>

--- src/main/java/racingcar/controller/RacingCarApiController.java
+++ src/main/java/racingcar/controller/RacingCarApiController.java
@@ -1,8 +1,6 @@
 package racingcar.controller;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +12,10 @@ import racingcar.dto.request.RacingCarRequestDto;
 import racingcar.dto.response.RacingGameResponseDto;
 import racingcar.service.RacingCarService;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class RacingCarApiController {
 
@@ -24,11 +26,12 @@ public class RacingCarApiController {
     }
 
     @PostMapping("/plays")
-    public RacingGameResponseDto play(@RequestBody RacingCarRequestDto racingCarRequestDto) {
+    public ResponseEntity<RacingGameResponseDto> play(@RequestBody RacingCarRequestDto racingCarRequestDto) {
         final List<String> names = Arrays.stream(racingCarRequestDto.getNames().split(","))
                 .collect(Collectors.toList());
-        return racingCarService.play(
+        final RacingGameResponseDto result = racingCarService.play(
                 new RacingGame(new RandomNumberGenerator(), new Cars(names), new Count(racingCarRequestDto.getCount()))
         );
+        return ResponseEntity.ok(result);
     }
 }
