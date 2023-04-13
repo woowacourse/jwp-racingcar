0c5072ad448a8645164f0929331e825b6e0fe59a
cpot5620 <=cpot5620@naver.com>
2023-04-13 17:07:45 +0900

    test(all): @Transactional 어노테이션 추가

--- src/test/java/racingcar/RacingCarApplicationTests.java
+++ src/test/java/racingcar/RacingCarApplicationTests.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 class RacingCarApplicationTests {
 
--- src/test/java/racingcar/dao/JdbcTemplateDAOTest.java
+++ src/test/java/racingcar/dao/JdbcTemplateDAOTest.java
@@ -5,29 +5,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.dto.CarDto;
 import racingcar.dto.GameResultDto;
 
 import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ComponentScan
+@JdbcTest
 class JdbcTemplateDAOTest {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
     @Autowired
     private JdbcTemplateDAO jdbcTemplateDAO;
 
-    @BeforeEach
-    void setUp() {
-        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
-        jdbcTemplate.execute("TRUNCATE TABLE GAME_RESULT");
-        jdbcTemplate.execute("TRUNCATE TABLE PLAYER_RESULT");
-        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
-    }
-
     @DisplayName("게임 결과를 저장할 수 있다.")
+    @Transactional
     @Test
     void saveGameResultTest() {
         GameResultDto gameResultDto = new GameResultDto(5, "dochi",
