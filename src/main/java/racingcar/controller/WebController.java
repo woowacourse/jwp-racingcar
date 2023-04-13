c6ebc0ab7da6f48acd78e9176f180bd1798faeb7
donghae <donghae1999@gmail.com>
2023-04-13 13:39:18 +0900

    refactor: /plays 응답 ResponseEntity 로 수정
    ResponeDto -> ResponseEntity

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -1,10 +1,10 @@
 package racingcar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import racingcar.dto.RequestDto;
 import racingcar.dto.ResponseDto;
 import racingcar.model.Cars;
@@ -24,13 +24,11 @@ public class WebController {
     }
 
     @PostMapping("/plays")
-    @ResponseBody
-    public ResponseDto playGame(@RequestBody RequestDto requestDto) {
+    public ResponseEntity<ResponseDto> playGame(@RequestBody RequestDto requestDto) {
         List<String> carNames = NameFormatConverter.splitNameByDelimiter(requestDto.getNames());
         Cars cars = new Cars(carNames);
 
         gameService.moveCars(cars, requestDto.getCount());
-
-        return new ResponseDto(cars.getWinners(), cars.getCars());
+        return ResponseEntity.ok().body(new ResponseDto(cars.getWinners(), cars.getCars()));
     }
 }
