95978fdf788ff4992c867022e22705dc14ec6f80
sh111-coder <ohk9134@naver.com>
2023-04-14 16:18:05 +0900

    feat(CarExceptionHandler): IllegalArgumentException 관련 핸들러 구현

---
+++ src/main/java/racingcar/exception/CarExceptionHandler.java
@@ -0,0 +1,15 @@
+package racingcar.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CarExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
+}
