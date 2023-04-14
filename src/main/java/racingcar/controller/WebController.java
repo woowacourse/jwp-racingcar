3f517efb7ba4a68e5ad91b6c773007544ff294de
iamjooon2 <iamjooon2@gmail.com>
2023-04-14 15:27:15 +0900

    refactor: @ResponseStatus 적용

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -3,9 +3,11 @@ package racingcar.controller;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.dto.RacingGameRequestDTO;
 import racingcar.dto.RacingGameResponseDTO;
@@ -22,9 +24,10 @@ public class WebController {
     }
 
     @PostMapping("/plays")
-    public ResponseEntity<RacingGameResponseDTO> createGame(@RequestBody RacingGameRequestDTO racingGameRequestDTO) {
+    @ResponseStatus(code = HttpStatus.OK)
+    public RacingGameResponseDTO createGame(@RequestBody RacingGameRequestDTO racingGameRequestDTO) {
         final List<String> carNames = Arrays.stream(racingGameRequestDTO.getNames().split(DELIMITER)).collect(Collectors.toList());
         final RacingGameResponseDTO racingGameResponseDTO = racingCarService.play(carNames, racingGameRequestDTO.getCount());
-        return ResponseEntity.ok().body(racingGameResponseDTO);
+        return racingGameResponseDTO;
     }
 }
