ebe9b5b1178a334622c1b5412fc0067c80dedbda
sh111-coder <ohk9134@naver.com>
2023-04-14 16:21:20 +0900

    feat(CarDao): PlayResultId로 모든 자동차 조회 기능 구현

--- src/main/java/racingcar/dao/car/CarDao.java
+++ src/main/java/racingcar/dao/car/CarDao.java
@@ -1,11 +1,15 @@
 package racingcar.dao.car;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import racingcar.dao.car.dto.CarRegisterRequest;
+import racingcar.domain.Car;
 
 @Repository
 public class CarDao {
@@ -27,5 +31,16 @@ public class CarDao {
                 .addValue("play_result_id", carRegisterRequest.getPlayResultId());
 
         jdbcInsert.execute(params);
+    }
+
+    public List<Car> findAllByPlayResultId(int playResultId) {
+        String sql = "SELECT * FROM car WHERE play_result_id=?";
+
+        return jdbcTemplate.query(sql, carRowMapper(), playResultId);
+    }
+
+    private RowMapper<Car> carRowMapper() {
+        return (rs, rowNum) ->
+                new Car(rs.getString("name"), rs.getInt("position"));
     }
 }
