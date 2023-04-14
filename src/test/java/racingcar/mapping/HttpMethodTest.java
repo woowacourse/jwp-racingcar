f1fa1695c041a79accfa9cd76c810c8096f8066b
jjongwa <troas96@naver.com>
2023-04-14 17:09:34 +0900

    refactor: 사용하지 않는 테스트 삭제

--- src/test/java/racingcar/mapping/HttpMethodTest.java
+++ src/test/java/racingcar/mapping/HttpMethodTest.java
@@ -8,32 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import racingcar.domain.Car;
 import racingcar.dto.RequestDto;
 
 @DisplayName("Http Method")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HttpMethodTest {
+
     @LocalServerPort
     int port;
 
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    @DisplayName("Http Method - POST")
-    @Test
-    void createUser() {
-        Car car = new Car("test");
-
-        RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(car)
-                .when().post("/http-method/car")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .header("Location", "/car/1");
     }
 
     @DisplayName("이름과 실행 횟수 POST")
