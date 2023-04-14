1a9587db65d526247fb2cb3e2e309bf9c2addbbc
HubCreator <chocomilk8604@gmail.com>
2023-04-14 12:57:31 +0900

    refactor: RacingCarController의 멤버변수 접근 제어자 수정

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RacingCarController {
 
-    final RacingGameService racingGameService;
+    private final RacingGameService racingGameService;
 
     public RacingCarController(RacingGameService racingGameService) {
         this.racingGameService = racingGameService;
