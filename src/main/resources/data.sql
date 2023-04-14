190d0d337bfd93a2573d63f3e8fce80c767ca7a1
LJW25 <dbjwlee@gmail.com>
2023-04-14 13:32:35 +0900

    refactor: DB table 생성 방식 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,16 @@
--- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-DROP TABLE racing_game if exists;
-DROP TABLE racing_car if exists;
-
-CREATE TABLE racing_game (
+CREATE TABLE IF NOT EXISTS racing_game
+(
     id          INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     trial_count INT         NOT NULL,
     winners     VARCHAR(50) NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp
 );
 
-CREATE TABLE racing_car (
-    id          INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name        VARCHAR(5)  NOT NULL,
-    position    INT         NOT NULL,
-    racing_game_id  INT     NOT NULL,
-    FOREIGN KEY (racing_game_id) REFERENCES racing_game(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS racing_car
+(
+    id             INT        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name           VARCHAR(5) NOT NULL,
+    position       INT        NOT NULL,
+    racing_game_id INT        NOT NULL,
+    FOREIGN KEY (racing_game_id) REFERENCES racing_game (id) ON DELETE CASCADE
 );
