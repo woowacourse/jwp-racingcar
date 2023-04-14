5ab1e7dda71c16b510aab95d724172587b51668d
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-04-14 09:11:18 +0900

    refactor: 접근제어자 추가

--- src/main/java/racingcar/dto/RacingCarDto.java
+++ src/main/java/racingcar/dto/RacingCarDto.java
@@ -1,8 +1,8 @@
 package racingcar.dto;
 
 public class RacingCarDto {
-    String name;
-    int position;
+    private final String name;
+    private final int position;
 
     public RacingCarDto(final String name, final int position) {
         this.name = name;
