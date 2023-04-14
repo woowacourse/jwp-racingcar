72795acb1f8e0457922f16a9e280120057c26ae6
thdwoqor <celebarte01@gmail.com>
2023-04-14 16:22:13 +0900

    test : 예외 테스트 코드 파라메터 타입 변경

--- src/test/java/racingcar/util/RepeatCountValidatorTest.java
+++ src/test/java/racingcar/util/RepeatCountValidatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RepeatCountValidatorTest {
@@ -13,22 +14,19 @@ class RepeatCountValidatorTest {
     @DisplayName("반복 횟수 입력 실패 1미만 100초과")
     @ParameterizedTest
     @ValueSource(strings = {"0", "101", "-1"})
-    void validateRange(String input) {
+    void validateRange(int input) {
         repeatCountValidator = new RepeatCountValidator();
 
         assertThatThrownBy(() -> repeatCountValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("1 이상만 가능합니다.");
+                .hasMessageContaining("시도할 횟수는 1~100 사이값만 가능합니다.");
     }
 
-    @DisplayName("반복 횟수 입력 실패 숫자 이외의값")
+    @DisplayName("반복 횟수 입력 성공 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"a", " ", "가", "@", ""})
-    void validateNumber(String input) {
+    @ValueSource(strings = {"1", "100"})
+    void validateRangeSuccess(int input) {
         repeatCountValidator = new RepeatCountValidator();
-
-        assertThatThrownBy(() -> repeatCountValidator.validate(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자만 입력 가능합니다.");
+        assertThatNoException().isThrownBy(()->repeatCountValidator.validate(input));
     }
 }
