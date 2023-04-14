3f6ecc7b3b42821d4eb28c53e32f9c12a996b61b
HubCreator <chocomilk8604@gmail.com>
2023-04-14 20:22:54 +0900

    refactor: Controller에 ExceptionHandler 구현

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,12 @@ public class RacingCarController {
     public ResponseEntity<GameResultResponseDto> racingGame(@RequestBody UserRequestDto inputDto) {
         final GameResultResponseDto gameResultResponseDto = racingGameService.getResult(inputDto);
         return ResponseEntity.ok(gameResultResponseDto);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Void> hello(RuntimeException exception) {
+        System.out.println(exception.getMessage());
+        return ResponseEntity.badRequest().build();
     }
 
 }
