f631245d4a6959e8a19be3ff10f91f379e693490
hgo641 <hgo641@gmail.com>
2023-04-14 13:32:19 +0900

    refactor: Request에서 올바른 값인지 검증하게 변경

--- src/main/java/racingcar/controller/WebRacingGameController.java
+++ src/main/java/racingcar/controller/WebRacingGameController.java
@@ -4,16 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import racingcar.domain.CarRandomNumberGenerator;
-import racingcar.domain.Cars;
-import racingcar.domain.RacingGame;
 import racingcar.dto.request.CarGameRequest;
 import racingcar.dto.response.CarGameResponse;
 import racingcar.service.RacingGameService;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.validation.Valid;
 
 @RestController
 public class WebRacingGameController {
@@ -25,7 +20,7 @@ public class WebRacingGameController {
     }
 
     @PostMapping("/plays")
-    public ResponseEntity<CarGameResponse> plays(@RequestBody CarGameRequest carGameRequest) {
+    public ResponseEntity<CarGameResponse> plays(@RequestBody @Valid CarGameRequest carGameRequest) {
         CarGameResponse result = racingGameService.play(carGameRequest);
         return ResponseEntity.ok(result);
     }
--- src/main/java/racingcar/dto/request/CarGameRequest.java
+++ src/main/java/racingcar/dto/request/CarGameRequest.java
@@ -1,7 +1,14 @@
 package racingcar.dto.request;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class CarGameRequest {
+    @NotEmpty
     private final String names;
+    @Positive
     private final int count;
 
     public CarGameRequest(String names, int count) {
