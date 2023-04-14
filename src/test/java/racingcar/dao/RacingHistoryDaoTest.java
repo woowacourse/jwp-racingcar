04a1d6a4c5ad3ded1a6021b98fd33803c4da0f27
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-04-14 10:33:39 +0900

    fix: 테스트 실패 오류 수정

--- src/test/java/racingcar/dao/RacingHistoryDaoTest.java
+++ src/test/java/racingcar/dao/RacingHistoryDaoTest.java
@@ -3,35 +3,22 @@ package racingcar.dao;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.dto.RacingCarDto;
 import racingcar.dto.RacingGameResultDto;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
 public class RacingHistoryDaoTest {
     @Autowired
     private RacingHistoryDao racingHistoryDao;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
-    @BeforeEach
-    void setUp() {
-        jdbcTemplate.execute("DROP TABLE PLAYERS_RESULT IF EXISTS");
-        jdbcTemplate.execute("DROP TABLE PLAY_RESULT IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE PLAY_RESULT\n"
-                + "(\n"
-                + "    id         INT         NOT NULL AUTO_INCREMENT,\n"
-                + "    winners    VARCHAR(50) NOT NULL,\n"
-                + "    play_count INT         NOT NULL,\n"
-                + "    created_at DATETIME    NOT NULL default current_timestamp,\n"
-                + "    PRIMARY KEY (id)\n"
-                + ");");
-    }
 
     @Test
     void insertWithMapAndCount() {
