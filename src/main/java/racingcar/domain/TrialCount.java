c12a4b06dba2f5b774d964099943b2302f02fa72
kdkdhoho <hkim4410@naver.com>
2023-04-14 15:52:33 +0900

    refactor(TrialCount): 변수명 수정

--- src/main/java/racingcar/domain/TrialCount.java
+++ src/main/java/racingcar/domain/TrialCount.java
@@ -4,20 +4,20 @@ public class TrialCount {
 
     private static final int MIN_VALUE = 0;
 
-    private final int trialCount;
+    private final int value;
 
-    public TrialCount(final int trialCount) {
-        validateRange(trialCount);
-        this.trialCount = trialCount;
+    public TrialCount(final int value) {
+        validateRange(value);
+        this.value = value;
     }
 
-    private void validateRange(final int trialCount) {
-        if (trialCount <= MIN_VALUE) {
-            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다. 입력 : " + trialCount);
+    private void validateRange(final int value) {
+        if (value <= MIN_VALUE) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다. 입력 : " + value);
         }
     }
 
-    public int getTrialCount() {
-        return trialCount;
+    public int getValue() {
+        return value;
     }
 }
