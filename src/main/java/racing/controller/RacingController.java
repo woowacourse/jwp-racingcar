1cdc3daa9427ed3d6f108cbac60c2adcbeae3ef3
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-04-14 10:54:44 +0900

    test: 컨트롤러 테스트 추가

--- src/main/java/racing/controller/RacingController.java
+++ src/main/java/racing/controller/RacingController.java
@@ -1,21 +1,20 @@
 package racing.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import racing.controller.dto.request.RacingGameInfoRequest;
 import racing.controller.dto.request.validator.RacingGameInfoRequestValidator;
 import racing.controller.dto.request.validator.RequestValidator;
-import racing.domain.CarFactory;
-import racing.controller.dto.request.RacingGameInfoRequest;
 import racing.controller.dto.response.RacingCarStateResponse;
 import racing.controller.dto.response.RacingGameResultResponse;
+import racing.domain.CarFactory;
 import racing.domain.Cars;
 import racing.service.RacingGameService;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class RacingController {
@@ -27,7 +26,7 @@ public class RacingController {
     }
 
     @PostMapping("/plays")
-    public ResponseEntity<RacingGameResultResponse> start(@RequestBody RacingGameInfoRequest request) {
+    public ResponseEntity<RacingGameResultResponse> playGame(@RequestBody RacingGameInfoRequest request) {
         RequestValidator<RacingGameInfoRequest> requestValidator = new RacingGameInfoRequestValidator();
         requestValidator.validate(request);
 
--- src/test/java/racing/controller/RacingControllerTest.java
+++ src/test/java/racing/controller/RacingControllerTest.java
@@ -1,35 +1,63 @@
 package racing.controller;
 
-import org.assertj.core.api.Assertions;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import racing.controller.dto.request.RacingGameInfoRequest;
-import racing.controller.dto.response.RacingGameResultResponse;
+import racing.repository.RacingGameDao;
+import racing.service.RacingGameService;
 
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(RacingController.class)
 class RacingControllerTest {
 
-    @LocalServerPort
-    private int randomServerPort;
+    @Autowired
+    MockMvc mockMvc;
 
-    @DisplayName("PostMapping Test")
+    @MockBean
+    RacingGameService racingGameService;
+
+    @MockBean
+    RacingGameDao racingGameDao;
+
+    @DisplayName("자동차 이름과 시도 횟수를 통해 게임을 진행할 수 있다.")
     @Test
-    void start() {
-        // given
+    void playGameTest() throws Exception {
+        when(racingGameService.saveGameByCount(anyInt())).thenReturn(1L);
+        doNothing().when(racingGameService).playGame(anyLong(), any());
         RacingGameInfoRequest request = new RacingGameInfoRequest("bebe,royce", 6);
-        String baseUrl = "http://localhost:" + randomServerPort;
 
-        // when
-        TestRestTemplate restTemplate = new TestRestTemplate();
-        ResponseEntity<RacingGameResultResponse> response =
-                restTemplate.postForEntity(baseUrl + "/plays", request, RacingGameResultResponse.class);
+        mockMvc.perform(MockMvcRequestBuilders.post("/plays")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsBytes(request)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 
-        // then
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    @DisplayName("잘못된 요청에 대해선 BAD REQUEST 상태를 응답한다.")
+    @Test
+    void invalidRequestTest() throws Exception {
+        when(racingGameService.saveGameByCount(anyInt())).thenReturn(1L);
+        doNothing().when(racingGameService).playGame(anyLong(), any());
+        RacingGameInfoRequest request = new RacingGameInfoRequest("bebe,12312312royce", 6);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/plays")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsBytes(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }
