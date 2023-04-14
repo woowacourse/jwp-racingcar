7be01a8e99a0b28cb66471b59d0d4279f1e3b32f
Ethan <kim76744dev@gmail.com>
2023-04-14 13:46:58 +0900

    test: HttpStatus 코드 추가 및 LocalServerPort 합침

--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -15,27 +15,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingGameControllerTest {
 
-    @LocalServerPort
-    int port;
-
     @BeforeEach
-    void setUp() {
+    void setUp(@LocalServerPort final int port) {
         RestAssured.port = port;
     }
 
     @DisplayName("doGame을 실행하면 게임의 결과가 반환된다.")
     @Test
     void doGame_givenCarNamesAndCount_thenReturnResult() {
-
+        // given
         final Map<String, Object> parameters = new HashMap<>();
         parameters.put("names", "브리,토미,브라운");
         parameters.put("count", 10);
 
+        // when
         final ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(parameters)
@@ -43,9 +42,10 @@ class RacingGameControllerTest {
                 .then().log().all()
                 .extract();
 
+        // then
         final JsonPath result = response.jsonPath();
-
         assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(result.getString("winners")).isNotNull(),
                 () -> assertThat(result.getList("racingCars.name", String.class)).containsExactly("브리", "토미", "브라운"),
                 () -> assertThat(result.getList("racingCars.position", Integer.class)).hasSize(3)
