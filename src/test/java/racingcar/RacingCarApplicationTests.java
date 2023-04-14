4b7e718cc6fe48e7de342d0fd335dc99a182ba61
ReO <89302528+Jaeyoung22@users.noreply.github.com>
2023-04-14 21:17:40 +0900

    [1단계 - 웹 자동차 경주] 리오(오영택) 미션 제출합니다. (#60)
    * feat: 이전 프로젝트 가져옴
    
    * docs: 기능 요구사항 정리
    
    * remove: console 뷰 삭제
    
    * feat: 검증 로직 추가
    
    * feat: 클라이언트 요청 매핑 객체 구현
    
    * feat: 응답 객체 구현
    
    * feat: Controller 구현
    
    * docs: 구현 기능 목록 체크
    
    * refactor: 라우팅 경로 변경
    
    * feat: 클라이언트로 부터 받은 이름이 null인지 체크하는 기능
    
    * refactor: names 리택터링으로 인한 테스트 코드 수정
    
    * test: 레이싱 게임 컨트롤러 테스트
    
    * feat: 레이싱 게임 스프링 컨트롤러 구현
    
    * docs: 구현 기능 목록 체크
    
    * feat: H2 데이터베이스 연동 및 INSERT 쿼리 구현
    
    * style: 코드 리포매팅
    
    * feat: RacingCarDto 도입
    
    * fix: 시간값을 저장하도록 수정
    
    * test: Log 테이블에 참여 인원수 만큼 저장되는지 테스트
    
    * refactor: id 데이터타입 수정
    
    * refactor: final 키워드 추가
    
    * docs: 기능 구현 목록 수정
    
    ---------
    
    Co-authored-by: songusika <sws981202@gmail.com>

--- src/test/java/racingcar/RacingCarApplicationTests.java
+++ src/test/java/racingcar/RacingCarApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RacingCarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
---
+++ src/test/java/racingcar/dao/RacingCarDaoTest.java
@@ -0,0 +1,64 @@
+package racingcar.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import racingcar.domain.Name;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCars;
+import racingcar.domain.TryCount;
+
+@SpringBootTest
+class RacingCarDaoTest {
+    private RacingCarDao racingCarDao;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        racingCarDao = new RacingCarDao(jdbcTemplate);
+
+        jdbcTemplate.execute("DROP TABLE LOG IF EXISTS");
+        jdbcTemplate.execute("DROP TABLE PLAY_RESULT IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE PLAY_RESULT("
+                + "    id          INT UNSIGNED  NOT NULL AUTO_INCREMENT,"
+                + "    winners     VARCHAR(50) NOT NULL,"
+                + "    trial_count INT         NOT NULL,"
+                + "    created_at  DATETIME    NOT NULL,"
+                + "    PRIMARY KEY (id))");
+
+        jdbcTemplate.execute("CREATE TABLE LOG("
+                + "    id      INT         NOT NULL AUTO_INCREMENT,"
+                + "    game_id INT         NOT NULL,"
+                + "    name    VARCHAR(50) NOT NULL,"
+                + "    move    INT         NOT NULL,"
+                + "    PRIMARY KEY (id),"
+                + "    FOREIGN KEY (game_id) REFERENCES PLAY_RESULT (id))");
+    }
+
+    @DisplayName("게임 플레이 인원수 만큼 LOG 테이블에 정보가 레코드된다.")
+    @Test
+    void insertDBTest() {
+        //given
+        final RacingCars racingCars = new RacingCars(
+                List.of(new RacingCar(new Name("블랙캣")),
+                        new RacingCar(new Name("리오"))
+                ));
+        final TryCount tryCount = new TryCount(3);
+        final String sql = "SELECT COUNT(*) FROM LOG";
+
+        //when
+        racingCarDao.insertGame(racingCars, tryCount);
+        final int count = jdbcTemplate.queryForObject(sql, Integer.class);
+
+        //then
+        assertThat(count).isEqualTo(2);
+    }
+}
