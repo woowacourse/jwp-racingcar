51c3598193e8485daa1281c7877001ec3941dba5
seokjin8678 <seokjin8678@gmail.com>
2023-04-14 16:25:53 +0900

    refactor: Controller 반환 타입 ResponseEntity로 변경

--- src/main/java/racingcar/controller/WebRacingCarController.java
+++ src/main/java/racingcar/controller/WebRacingCarController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +19,14 @@ public class WebRacingCarController {
     }
 
     @PostMapping("/plays")
-    public RacingResultResponse play(@RequestBody RacingCarRequest request) {
+    public ResponseEntity<RacingResultResponse> play(@RequestBody RacingCarRequest request) {
         List<String> carNames = getCarNames(request.getNames());
         int gameId = racingCarService.playRacingGame(carNames, request.getCount());
 
         List<String> winners = racingCarService.findWinners(gameId);
         List<RacingCarDto> racingCars = racingCarService.findRacingCars(gameId);
-        return new RacingResultResponse(winners, racingCars);
+        return ResponseEntity.ok()
+                .body(new RacingResultResponse(winners, racingCars));
     }
 
     private List<String> getCarNames(String names) {
