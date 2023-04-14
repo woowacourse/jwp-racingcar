84eba7b9624582cf2e81e51297ed1100f0cdc120
kdkdhoho <hkim4410@naver.com>
2023-04-14 14:46:23 +0900

    feat(Cars): 예외 검증 기능 구현
    - 자동차가 최소 2개 이상 생성되었는가?
    - 자동차 이름에 중복이 존재하지 않는가?

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,13 +1,38 @@
 package racingcar.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Cars {
+
+    private static final int MIN_CAR_COUNT = 2;
+
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
+        validateCount(cars);
+        validateNameDuplicate(cars);
         this.cars = cars;
+    }
+
+    private void validateCount(final List<Car> cars) {
+        if (cars.size() < MIN_CAR_COUNT) {
+            throw new IllegalArgumentException("최소 " + MIN_CAR_COUNT +"개 이상의 자동차를 입력해주세요.");
+        }
+    }
+
+    private void validateNameDuplicate(final List<Car> cars) {
+        List<String> carNames = cars.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        Set<String> nonDuplicateNames = new HashSet<>(carNames);
+
+        if (nonDuplicateNames.size() != carNames.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
     }
 
     public void moveAll() {
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,12 +1,13 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CarsTest {
 
@@ -28,6 +29,49 @@ class CarsTest {
         Cars cars = new Cars(Arrays.asList(success1, success2));
         cars.moveAll();
         assertThat(cars.getWinner()).isEqualTo(List.of(success1, success2));
+    }
+
+    @Test
+    @DisplayName("두 개 미만의 자동차가 생성되면 예외를 발생한다")
+    void 두_개_미만의_자동차가_생성되면_예외를_발생한다() {
+        Car car1 = new TestCarSuccess("1");
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () ->  new Cars(List.of(car1))
+        );
+    }
+
+    @Test
+    @DisplayName("두 개 이상의 자동차가 생성되면 객체를 정상 생성한다")
+    void 두_개_이상의_자동차가_생성되면_객체를_정상_생성한다() {
+        Car car1 = new TestCarSuccess("1");
+        Car car2 = new TestCarSuccess("2");
+
+        assertThatNoException().isThrownBy(
+                () ->  new Cars(List.of(car1, car2))
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 중복되면 예외를 발생한다")
+    void 자동차_이름이_중복되면_예외를_발생한다() {
+        Car car1 = new TestCarSuccess("name");
+        Car car2 = new TestCarSuccess("name");
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Cars(List.of(car1, car2))
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 중복되지 않으면 객체를 정상 생성한다")
+    void 자동차_이름이_중복되지_않으면_객체를_정상_생성한다() {
+        Car car1 = new TestCarSuccess("1");
+        Car car2 = new TestCarSuccess("2");
+
+        assertThatNoException().isThrownBy(
+                () ->  new Cars(List.of(car1, car2))
+        );
     }
 
     private static class TestCarSuccess extends Car {
