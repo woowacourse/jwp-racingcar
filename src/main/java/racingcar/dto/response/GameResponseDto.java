d25929c0177cbf010125b9aeb649ee1864f1f336
cpot5620 <=cpot5620@naver.com>
2023-04-15 19:10:05 +0900

    test(Service): 게임 실행 테스트 구현

--- src/main/java/racingcar/dto/response/GameResponseDto.java
+++ src/main/java/racingcar/dto/response/GameResponseDto.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 public class GameResponseDto {
     private final String winners;
-    private final List<PlayerResultDto> racingCars;
+    private final List<PlayerResultDto> playerResults;
 
-    public GameResponseDto(String winners, List<PlayerResultDto> racingCars) {
+    public GameResponseDto(String winners, List<PlayerResultDto> playerResults) {
         this.winners = winners;
-        this.racingCars = racingCars;
+        this.playerResults = playerResults;
     }
 
     public String getWinners() {
         return winners;
     }
 
-    public List<PlayerResultDto> getRacingCars() {
-        return racingCars;
+    public List<PlayerResultDto> getPlayerResults() {
+        return playerResults;
     }
 }
