a050898f4a684c65570e54410b14c28d6b9a8d72
splitube <voicesplit@daum.net>
2023-04-16 05:15:29 +0900

    refactor: 테스트시 기존 테이블 재생성

--- src/test/java/racingcar/dao/RaceDaoTest.java
+++ src/test/java/racingcar/dao/RaceDaoTest.java
@@ -2,21 +2,36 @@ package racingcar.dao;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.dto.GameInputDto;
 
 @JdbcTest
+@Transactional
 class RaceDaoTest {
 
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private RaceDao raceDao;
+
+    @BeforeEach
+    void setUp() {
+        JdbcTemplate jdbcTemplate = namedParameterJdbcTemplate.getJdbcTemplate();
+        jdbcTemplate.execute("DROP TABLE RACE CASCADE");
+        jdbcTemplate.execute("CREATE TABLE RACE (id INT  NOT NULL AUTO_INCREMENT,"
+            + "play_count  INT NOT NULL,"
+            + "created_at  DATETIME  NOT NULL default current_timestamp,"
+            + "PRIMARY KEY (id))");
+        raceDao = new RaceDao(namedParameterJdbcTemplate);
+    }
 
     @Test
     void insert() {
-        final RaceDao raceDao = new RaceDao(namedParameterJdbcTemplate);
         int raceId = raceDao.insert(new GameInputDto("아벨,스플릿", "12"));
         assertThat(raceId).isOne();
     }
