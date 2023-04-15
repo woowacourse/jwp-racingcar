deb39e2378ac7b207088999ea621d7ed8f817629
yujamint <woawoa0513@gmail.com>
2023-04-16 01:51:15 +0900

    refactor: 게임 결과 출력 뷰 API 반환 타입에 맞게 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,26 +1,27 @@
 package racingcar.view;
 
-import racingcar.model.Car;
-import racingcar.model.Cars;
-
 import java.util.List;
+import racingcar.controller.dto.CarDto;
+import racingcar.controller.dto.GamePlayResponseDto;
 
 public class OutputView {
+    private final static String NEW_LINE = System.lineSeparator();
     private final static String CAR_LOCATION_INDICATOR = "-";
-    private final static String PRINT_CAR_LOCATION = "%s : %s" + System.lineSeparator();
+    private final static String PRINT_CAR_LOCATION = "%s : %s" + NEW_LINE;
     private final static String CAR_WINNER_INDICATOR = ",";
-    private final static String PRINT_WINNER = "%s가 최종 우승했습니다." + System.lineSeparator();
-    private final static String PRINT_RESULT = System.lineSeparator() + "실행 결과";
+    private final static String PRINT_WINNER = "%s가 최종 우승했습니다." + NEW_LINE;
+    private final static String PRINT_RESULT = NEW_LINE + "실행 결과";
     private final static String PRINT_REQUEST_CAR_NAME
             = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private final static String PRINT_REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
-    public void printResult(Cars cars) {
-        for (Car car : cars.getCars()) {
-            String location = CAR_LOCATION_INDICATOR.repeat(car.getLocation());
-            System.out.printf(PRINT_CAR_LOCATION, car.getName(), location);
+    public void printResult(GamePlayResponseDto gameResult) {
+        System.out.println("----- 경주 결과 -----");
+        System.out.println("우승자: " + gameResult.getWinners() + NEW_LINE);
+        for (CarDto car : gameResult.getRacingCars()) {
+            System.out.println(car.getName() + ": " + car.getPosition() + "칸 이동");
         }
-        System.out.println();
+        System.out.println("----------------------");
     }
 
     public void printWinner(List<String> winner) {
@@ -38,6 +39,4 @@ public class OutputView {
     public void printRequestTryCount() {
         System.out.println(PRINT_REQUEST_TRY_COUNT);
     }
-
-
 }
