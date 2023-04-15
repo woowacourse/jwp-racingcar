12e64af11446e8220b21a7a66ed1e8cd0f7d3ee0
yujamint <woawoa0513@gmail.com>
2023-04-16 03:08:54 +0900

    refactor: 웹 애플리케이션으로 수정하며 불필요해진 코드 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,10 @@
 package racingcar.view;
 
-import java.util.List;
 import racingcar.controller.dto.CarDto;
 import racingcar.controller.dto.GamePlayResponseDto;
 
 public class OutputView {
     private final static String NEW_LINE = System.lineSeparator();
-    private final static String CAR_LOCATION_INDICATOR = "-";
-    private final static String PRINT_CAR_LOCATION = "%s : %s" + NEW_LINE;
-    private final static String CAR_WINNER_INDICATOR = ",";
-    private final static String PRINT_WINNER = "%s가 최종 우승했습니다." + NEW_LINE;
-    private final static String PRINT_RESULT = NEW_LINE + "실행 결과";
-    private final static String PRINT_REQUEST_CAR_NAME
-            = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private final static String PRINT_REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public void printResult(GamePlayResponseDto gameResult) {
         System.out.println("----- 경주 결과 -----");
@@ -22,21 +13,5 @@ public class OutputView {
             System.out.println(car.getName() + ": " + car.getPosition() + "칸 이동");
         }
         System.out.println("----------------------");
-    }
-
-    public void printWinner(List<String> winner) {
-        System.out.printf(PRINT_WINNER, String.join(CAR_WINNER_INDICATOR,winner));
-    }
-
-    public void printResult() {
-        System.out.println(PRINT_RESULT);
-    }
-
-    public void printRequestCarName() {
-        System.out.println(PRINT_REQUEST_CAR_NAME);
-    }
-
-    public void printRequestTryCount() {
-        System.out.println(PRINT_REQUEST_TRY_COUNT);
     }
 }
