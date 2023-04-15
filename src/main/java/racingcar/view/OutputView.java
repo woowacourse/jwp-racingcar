deb5dab5963717b557bf36bbb9aaa97ff394dbbd
kwonyj1022 <kwonyj1022@naver.com>
2023-04-15 23:56:23 +0900

    feat: console application에서 우승자와 player 별 최종 이동거리를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,26 +1,25 @@
 package racingcar.view;
 
+import racingcar.dto.CarDto;
+
 import java.util.List;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
 
 public class OutputView {
-    public static final String GAME_RESULT_FORMAT = "%s : %s\n";
-    public static final String WINNER_FORMAT = "%s가 최종 우승했습니다.\n";
-    private static final String POSITION_VIEW = "-";
-    public static final String WINNER_DELIMITER = ", ";
+    public static final String GAME_RESULT_FORMAT = "\nName: %s, Position: %d\n";
+    public static final String WINNER_FORMAT = "우승자: %s\n";
 
-    public static void printRacing(Cars cars) {
-        for (Car car : cars.getCars()) {
-            int position = car.getPosition();
-            String positionView = POSITION_VIEW.repeat(position);
-            System.out.printf(GAME_RESULT_FORMAT, car.getName(), positionView);
-        }
-        System.out.println();
+
+    private OutputView() {
     }
 
-    public static void printWinners(List<String> names) {
-        String winners = String.join(WINNER_DELIMITER, names);
+    public static void printWinners(String winners) {
         System.out.printf(WINNER_FORMAT, winners);
+    }
+
+    public static void printRacing(List<CarDto> racingCars) {
+        for (CarDto car : racingCars) {
+            int position = car.getPosition();
+            System.out.printf(GAME_RESULT_FORMAT, car.getName(), position);
+        }
     }
 }
