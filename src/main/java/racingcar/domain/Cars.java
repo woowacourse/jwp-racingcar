6ded106781c8884228b029e24786310b53b9b68d
Youngho <cyh6099@gmail.com>
2023-04-16 03:14:35 +0900

    feat: 난수 생성 위치 수정

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -17,8 +17,8 @@ public class Cars {
     }
 
     public void moveCars(final NumberGenerator numberGenerator) {
-        int moveNumber = numberGenerator.generate();
         for (Car car : cars) {
+            int moveNumber = numberGenerator.generate();
             car.move(moveNumber);
         }
     }
