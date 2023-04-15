3b1f4211896569b75cef414151bd3d78b9eb45dd
cpot5620 <=cpot5620@naver.com>
2023-04-15 19:10:05 +0900

    test(Service): 게임 실행 테스트 구현

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -10,10 +10,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import racingcar.dao.GameResultDAO;
 import racingcar.dao.PlayerResultDAO;
+import racingcar.dto.PlayerResultDto;
+import racingcar.dto.response.GameResponseDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RacingGameServiceTest {
@@ -49,4 +56,33 @@ class RacingGameServiceTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("게임을 play할 경우, 게임 결과를 반환한다.")
+    @Test
+    void playGameSuccessTest() {
+        //given
+        List<String> names = List.of("쥬니", "도치");
+        int tryCount = 10;
+
+        when(gameResultDAO.save(any()))
+                .thenReturn(1);
+
+        doNothing().when(playerResultDAO)
+                .save(anyInt(), anyList());
+
+        //when
+        GameResponseDto result = racingGameService.play(names, tryCount);
+
+        //then
+        List<String> winners = List.of(result.getWinners().split(","));
+        List<String> players = getPlayers(result);
+
+        assertThat(names).containsAll(winners);
+        assertThat(names).containsExactlyElementsOf(players);
+    }
+
+    private List<String> getPlayers(GameResponseDto gameResponseDto) {
+        return gameResponseDto.getRacingCars().stream()
+                .map(PlayerResultDto::getName)
+                .collect(Collectors.toList());
+    }
 }
