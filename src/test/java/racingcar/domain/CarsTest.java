3e661a10414cade0e2dba528c032bda8741533f0
Youngho <cyh6099@gmail.com>
2023-04-16 03:07:24 +0900

    test: 우승자 계산 테스트 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
     private Cars cars;
-    private final List<String> carNames = List.of("pobi", "crong", "honux");
 
     @Nested
     @DisplayName("CarRepository 생성에 대한 테스트")
@@ -19,7 +18,7 @@ class CarsTest {
         @Test
         @DisplayName("차 이름이 중복되는 경우 예외가 발생한다")
         void duplicatedNameTest() {
-            List<String> duplicatedCarNames = List.of("pobi", "pobi");
+            List<Car> duplicatedCarNames = List.of(new Car("pobi"), new Car("pobi"));
             assertThatThrownBy(() -> cars = new Cars(duplicatedCarNames))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("자동차 이름이 중복됩니다.");
@@ -28,8 +27,7 @@ class CarsTest {
         @Test
         @DisplayName("차 개수가 2대 미만인 경우 예외가 발생한다.")
         void carCountTest() {
-            List<String> carNames = List.of("pobi");
-            assertThatThrownBy(() -> new Cars(carNames))
+            assertThatThrownBy(() -> new Cars(List.of(new Car("pobi"))))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("자동차 수는 2대 이상이어야 합니다.");
         }
@@ -41,12 +39,29 @@ class CarsTest {
         @Test
         @DisplayName("움직이고 나면 위치가 변해야한다")
         void updateMovedCarSuccess() {
-            cars = new Cars(carNames);
+            cars = new Cars(List.of(new Car("pobi"), new Car("crong"), new Car("honux")));
             NumberGenerator numberGenerator = new RandomNumberGenerator();
             cars.moveCars(numberGenerator);
             List<Car> movedResult = cars.getLatestResult();
             assertThat(cars.getLatestResult())
                     .isEqualTo(movedResult);
+        }
+
+        @Test
+        @DisplayName("가장 멀리 움직인 자동차가 우승이다.")
+        void calculateWinnerTest() {
+            Car hyeon9makCar = new Car("hyeon9mak");
+            Car leoCar = new Car("leo");
+            Car ditooCar = new Car("ditoo");
+
+            hyeon9makCar.move(8);
+            cars = new Cars(List.of(hyeon9makCar, leoCar, ditooCar));
+            List<String> winner = cars.calculateWinners();
+
+            assertThat(winner.size())
+                    .isEqualTo(1);
+            assertThat(winner.get(0))
+                    .isEqualTo(hyeon9makCar.getCarName().getName());
         }
     }
 }
