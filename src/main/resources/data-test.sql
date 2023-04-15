ea13c2ed4a1db90765176ab64a5be99951fa17d1
yujamint <woawoa0513@gmail.com>
2023-04-15 17:19:24 +0900

    feat: 조인 쿼리 테스트를 위한 초기 데이터 쿼리 추가

---
+++ src/main/resources/data-test.sql
@@ -0,0 +1,10 @@
+INSERT INTO PLAY_RESULT (trial_count, winners) VALUES (5, '민트');
+INSERT INTO PLAY_RESULT (trial_count, winners) VALUES (10, '오션');
+INSERT INTO PLAY_RESULT (trial_count, winners) VALUES (8, '오션,민트');
+
+INSERT INTO CAR_RESULT (play_result_id, car_name, car_position) VALUES (1, '오션', 2);
+INSERT INTO CAR_RESULT (play_result_id, car_name, car_position) VALUES (1, '민트', 4);
+INSERT INTO CAR_RESULT (play_result_id, car_name, car_position) VALUES (2, '오션', 8);
+INSERT INTO CAR_RESULT (play_result_id, car_name, car_position) VALUES (2, '민트', 6);
+INSERT INTO CAR_RESULT (play_result_id, car_name, car_position) VALUES (3, '민트', 3);
+INSERT INTO CAR_RESULT (play_result_id, car_name, car_position) VALUES (3, '오션', 3);
