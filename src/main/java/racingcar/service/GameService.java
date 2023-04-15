77239e545489873a1c58cae448fdbec2f5e32675
Youngho <cyh6099@gmail.com>
2023-04-16 03:08:08 +0900

    feat: service로직 수정

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import racingcar.domain.Car;
-import racingcar.dto.response.GameResponseDto;
-import racingcar.dto.request.GameResultDto;
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.dto.request.GamePlayDto;
 import racingcar.dto.request.GameSaveDto;
 import racingcar.dto.request.PlayerResultSaveDto;
+import racingcar.dto.response.GameResponseDto;
 import racingcar.entity.Game;
 import racingcar.entity.PlayerResult;
 import racingcar.repository.GameRepository;
@@ -15,6 +18,7 @@ import racingcar.repository.PlayerResultRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GameService {
@@ -29,21 +33,34 @@ public class GameService {
     }
 
     @Transactional
-    public GameResponseDto playGame(GameResultDto gameResult) {
-        final Game game = createGame(gameResult);
-        final List<PlayerResult> playerResults = createPlayerResults(gameResult, game);
+    public GameResponseDto playGame(GamePlayDto gamePlayDto) {
+        Cars cars = makeCars(gamePlayDto.getNames());
+        RacingGame racingGame = new RacingGame(cars, gamePlayDto.getCount());
+        racingGame.race(new RandomNumberGenerator());
+
+        List<String> winners = cars.calculateWinners();
+
+        final Game game = createGame(String.join(",", winners), gamePlayDto.getCount());
+        final List<PlayerResult> playerResults = createPlayerResults(cars, game);
         return GameResponseDto.of(game, playerResults);
     }
 
-    private Game createGame(final GameResultDto gameResult) {
-        GameSaveDto gameSaveDto = new GameSaveDto(gameResult.getWinners(), gameResult.getTrialCount());
+    private Cars makeCars(List<String> carNames) {
+        List<Car> baseCars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+        return new Cars(baseCars);
+    }
+
+    private Game createGame(final String winners, final int trialCount) {
+        GameSaveDto gameSaveDto = new GameSaveDto(winners, trialCount);
         Game game = gameRepository.createGame(gameSaveDto);
         return game;
     }
 
-    private List<PlayerResult> createPlayerResults(final GameResultDto gameResult, final Game game) {
+    private List<PlayerResult> createPlayerResults(final Cars cars, final Game game) {
         final List<PlayerResult> playerResults = new ArrayList<>();
-        for (Car car : gameResult.getCars()) {
+        for (Car car : cars.getLatestResult()) {
             final PlayerResultSaveDto playerResultSaveDto = new PlayerResultSaveDto(game.getId(), car);
             final PlayerResult playerResult = playerResultRepository.savePlayerResult(playerResultSaveDto);
             playerResults.add(playerResult);
