bcbf7667cbd4153fb7434f370efed09f3ba6f730
kdkdhoho <hkim4410@naver.com>
2023-04-15 18:36:00 +0900

    feat: 자동차 경주 게임 이력 조회 기능 구현

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import racingcar.request.PlayRequest;
 import racingcar.response.PlayResponse;
 import racingcar.service.GameService;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class WebController {
@@ -25,5 +27,11 @@ public class WebController {
     public ResponseEntity<PlayResponse> plays(@RequestBody @Valid final PlayRequest playRequest) {
         PlayResponse playResponse = gameService.playRacing(playRequest.getNames(), playRequest.getCount());
         return ResponseEntity.ok().body(playResponse);
+    }
+
+    @GetMapping(value = "/plays", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PlayResponse>> plays() {
+        List<PlayResponse> allGameHistory = gameService.allGameHistory();
+        return ResponseEntity.ok().body(allGameHistory);
     }
 }
--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -7,18 +7,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import racingcar.domain.Car;
 import racingcar.dto.CarDto;
+import racingcar.dto.RecordDto;
 import racingcar.repository.RecordDao;
 import racingcar.response.PlayResponse;
 import racingcar.domain.Cars;
 import racingcar.domain.TrialCount;
 import racingcar.repository.GameDao;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class GameService {
 
     private static final String CAR_NAME_DELIMITER = ",";
+    private static final int MIN_GAME_ID = 1;
 
     private final GameDao gameDao;
     private final RecordDao recordDao;
@@ -75,5 +79,36 @@ public class GameService {
                 .stream()
                 .map(car -> new CarDto(car.getName(), car.getDistance()))
                 .collect(toList());
+    }
+
+    public List<PlayResponse> allGameHistory() {
+        List<PlayResponse> playResponses = new ArrayList<>();
+
+        int count = gameDao.countAll();
+        for (int gameId = MIN_GAME_ID; gameId <= count; gameId++) {
+            List<RecordDto> foundGame = recordDao.findAllByGameId(gameId);
+
+            playResponses.add(createPlayResponse(foundGame));
+        }
+
+        return Collections.unmodifiableList(playResponses);
+    }
+
+    private PlayResponse createPlayResponse(final List<RecordDto> foundGame) {
+        List<String> winners = new ArrayList<>();
+        List<CarDto> carDtos = new ArrayList<>();
+
+        for (final RecordDto recordDto : foundGame) {
+            decideWinner(winners, recordDto);
+            carDtos.add(new CarDto(recordDto.getPlayerName(), recordDto.getPosition()));
+        }
+
+        return new PlayResponse(winners, carDtos);
+    }
+
+    private void decideWinner(final List<String> winners, final RecordDto recordDto) {
+        if (recordDto.isWinner()) {
+            winners.add(recordDto.getPlayerName());
+        }
     }
 }
