12c44a55e1e4d02247489f1994d4935e3f01c80f
JunH <tjdtls690@gmail.com>
2023-04-16 01:20:23 +0900

    :art: refactor : 컨트롤러의 단위 테스트와 통합 테스트 분리

--- src/test/java/racingcar/controller/RacingGameControllerRestTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerRestTest.java
@@ -1,14 +1,16 @@
 package racingcar.controller;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
 import racingcar.dto.GameInputDto;
 import racingcar.service.RacingGameService;
 
@@ -19,29 +21,27 @@ import static org.hamcrest.Matchers.*;
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @Sql("/init.sql")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
-class RacingGameControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RacingGameControllerRestTest {
+    @LocalServerPort
+    private int port;
     @Autowired
     private RacingGameService racingGameService;
-    @Autowired
-    private MockMvc mockMvc;
-    private GameInputDto gameInputDto;
     
     @BeforeEach
     void setUp() {
-        gameInputDto = new GameInputDto("아벨,스플릿,포비", "12");
+        RestAssured.port = port;
     }
     
     @Test
     void 이름과_시도횟수를_전달하면_게임_결과를_반환한다() {
-        RestAssuredMockMvc.given().log().all()
-                .mockMvc(mockMvc)
+        GameInputDto gameInputDto = new GameInputDto("아벨,스플릿,포비", "12");
+        RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(gameInputDto)
                 .when().post("/plays")
                 .then().log().all()
-                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
                 .contentType(ContentType.JSON)
                 .body("winners", notNullValue())
                 .body("racingCars", hasSize(3));
@@ -49,16 +49,15 @@ class RacingGameControllerTest {
     
     @Test
     void 진행했던_모든_게임의_결과를_반환한다() {
-        racingGameService.playGameWithoutPrint(gameInputDto, () -> 5);
-        racingGameService.playGameWithoutPrint(gameInputDto, () -> 5);
+        racingGameService.playGameWithoutPrint(new GameInputDto("아벨,스플릿,포비", "12"), () -> 5);
+        racingGameService.playGameWithoutPrint(new GameInputDto("aa,bb,cc", "12"), () -> 5);
         
-        RestAssuredMockMvc.given().log().all()
-                .mockMvc(mockMvc)
+        RestAssured.given().log().all()
                 .when().get("/plays")
                 .then().log().all()
-                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
                 .contentType(ContentType.JSON)
-                .body("winners", is(List.of("아벨,스플릿,포비", "아벨,스플릿,포비")))
+                .body("winners", is(List.of("아벨,스플릿,포비", "aa,bb,cc")))
                 .body("racingCars", hasSize(2));
     }
 }
---
+++ src/test/java/racingcar/controller/RacingGameControllerUnitTest.java
@@ -0,0 +1,79 @@
+package racingcar.controller;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import racingcar.domain.Car;
+import racingcar.dto.GameInputDto;
+import racingcar.dto.RacingResultResponseDto;
+import racingcar.service.RacingGameService;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@WebMvcTest
+class RacingGameControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private RacingGameService racingGameService;
+    private GameInputDto gameInputDto;
+    
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+        gameInputDto = new GameInputDto("아벨,스플릿,포비", "12");
+    }
+    
+    @Test
+    void 이름과_시도횟수를_전달하면_게임_결과를_반환한다() {
+        List<Car> winners = List.of(new Car("아벨", 0));
+        List<Car> cars = List.of(new Car("아벨", 0), new Car("스플릿", 0), new Car("포비", 0));
+        RacingResultResponseDto racingResultResponseDto = new RacingResultResponseDto(winners, cars);
+        
+        given(racingGameService.playGameWithoutPrint(any(), any())).willReturn(racingResultResponseDto);
+        
+        RestAssuredMockMvc.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(gameInputDto)
+                .when().post("/plays")
+                .then().log().all()
+                .status(HttpStatus.OK)
+                .contentType(ContentType.JSON)
+                .body("winners", is("아벨"))
+                .body("racingCars", hasSize(3));
+    }
+    
+    @Test
+    void 진행했던_모든_게임의_결과를_반환한다() {
+        List<Car> firstWinners = List.of(new Car("아벨", 0));
+        List<Car> secondWinners = List.of(new Car("스플릿", 0));
+        List<Car> cars = List.of(new Car("아벨", 0), new Car("스플릿", 0), new Car("포비", 0));
+        RacingResultResponseDto firstResult = new RacingResultResponseDto(firstWinners, cars);
+        RacingResultResponseDto secondResult = new RacingResultResponseDto(secondWinners, cars);
+        List<RacingResultResponseDto> racingResultResponseDtos = List.of(firstResult, secondResult);
+        
+        given(racingGameService.findAllGameResult()).willReturn(racingResultResponseDtos);
+        
+        RestAssuredMockMvc.given().log().all()
+                .when().get("/plays")
+                .then().log().all()
+                .status(HttpStatus.OK)
+                .contentType(ContentType.JSON)
+                .body("winners", is(List.of("아벨", "스플릿")))
+                .body("racingCars", hasSize(2));
+    }
+}
