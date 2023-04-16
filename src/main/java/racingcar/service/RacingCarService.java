d6a996693f455cc0d2c4b283495f4a7c8589e34e
rawfishthelgh <rg970604@naver.com>
2023-04-16 19:19:11 +0900

    refactor: 변수명 수정

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -2,6 +2,7 @@ package racingcar.service;
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.dao.PlayerResultDAO;
 import racingcar.dao.PlayersInfoDAO;
 import racingcar.domain.Cars;
@@ -19,7 +20,7 @@ public class RacingCarService {
     }
 
     public void saveResult(int trialCount, Cars cars){
-        int tableId = playerResultDAO.returnTableIdAfterInsert(trialCount, cars.findWinners());
-        playersInfoDAO.insert(tableId, cars.getCars());
+        int playerResultId = playerResultDAO.returnTableIdAfterInsert(trialCount, cars.findWinners());
+        playersInfoDAO.insert(playerResultId, cars.getCars());
     }
 }
