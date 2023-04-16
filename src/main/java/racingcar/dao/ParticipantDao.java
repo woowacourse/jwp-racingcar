11976dc4f8df180130a6f2e3ac9085e2c449aa05
dooboocookie <bkh3030@gmail.com>
2023-04-16 23:01:15 +0900

    feat(ParticipantDao): 게임 아이디로 참여자들 조회하는 기능 추가

--- src/main/java/racingcar/dao/ParticipantDao.java
+++ src/main/java/racingcar/dao/ParticipantDao.java
@@ -2,8 +2,12 @@ package racingcar.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import racingcar.dto.ParticipateDto;
+import racingcar.entity.ParticipantEntity;
+
+import java.util.List;
 
 @Repository
 public class ParticipantDao {
@@ -13,6 +17,18 @@ public class ParticipantDao {
     @Autowired
     public ParticipantDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<ParticipantEntity> findByGameId(final Long gameId) {
+        final String sql = "SELECT * FROM PARTICIPANT WHERE game_id = ?";
+        RowMapper<ParticipantEntity> participantEntityRowMapper = (resultSet, rowNum) ->
+                new ParticipantEntity(
+                        resultSet.getLong("game_id"),
+                        resultSet.getLong("player_id"),
+                        resultSet.getInt("position"),
+                        resultSet.getBoolean("is_winner")
+                );
+        return jdbcTemplate.query(sql, participantEntityRowMapper, gameId);
     }
 
     public void save(final ParticipateDto participateDto) {
---
+++ src/main/java/racingcar/entity/ParticipantEntity.java
@@ -0,0 +1,32 @@
+package racingcar.entity;
+
+public class ParticipantEntity {
+
+    private final Long gameId;
+    private final Long playerId;
+    private final Integer position;
+    private final Boolean isWinner;
+
+    public ParticipantEntity(final Long gameId, final Long playerId, final Integer position, final Boolean isWinner) {
+        this.gameId = gameId;
+        this.playerId = playerId;
+        this.position = position;
+        this.isWinner = isWinner;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public Boolean getWinner() {
+        return isWinner;
+    }
+}
