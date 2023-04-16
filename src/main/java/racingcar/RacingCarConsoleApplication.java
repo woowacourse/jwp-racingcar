7d7db05731d3a797a2ac7388262a79da4107170b
JunDonghyuk <ehdgur4814@naver.com>
2023-04-17 02:04:09 +0900

    style: 개행 변경

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -3,6 +3,7 @@ package racingcar;
 import racingcar.controller.RacingGameController;
 
 public class RacingCarConsoleApplication {
+
     public static void main(String[] args) {
         RacingGameController racingCarController = new RacingGameController();
         racingCarController.run();
