a8147819187c0d5fb29d360182266a21eb963ec9
JunDonghyuk <ehdgur4814@naver.com>
2023-04-17 03:21:52 +0900

    refactor: Console 어플리케이션 설정 변경

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -1,11 +1,24 @@
 package racingcar;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import racingcar.controller.RacingGameController;
+import racingcar.dao.CarDao;
+import racingcar.dao.GameDao;
+import racingcar.domain.Car.NumberGenerator;
+import racingcar.domain.Car.RandomNumberGenerator;
+import racingcar.service.RacingGameService;
 
 public class RacingCarConsoleApplication {
 
     public static void main(String[] args) {
-        RacingGameController racingCarController = new RacingGameController();
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        GameDao gameDao = new GameDao(jdbcTemplate);
+        CarDao carDao = new CarDao(jdbcTemplate);
+        NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        RacingGameService racingGameService = new RacingGameService(gameDao, carDao, randomNumberGenerator);
+
+        RacingGameController racingCarController = new RacingGameController(racingGameService);
         racingCarController.run();
     }
 }
