adf0453097735d9a3b0fa119b975ac137f8d5930
JunDonghyuk <ehdgur4814@naver.com>
2023-04-17 02:01:52 +0900

    feat(CarDao): 조회 기능 구현

--- src/main/java/racingcar/dao/CarDao.java
+++ src/main/java/racingcar/dao/CarDao.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class CarDao {
 
@@ -23,5 +25,16 @@ public class CarDao {
     public int save(CarEntity carEntity) {
         SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(carEntity);
         return insertActor.executeAndReturnKey(sqlParameterSource).intValue();
+    }
+
+    public List<CarEntity> findByGameId(int gameId) {
+        String sql = "select name, position from car where game_id = ?";
+        List<CarEntity> cars = jdbcTemplate.query(sql, (rs, rowNum) -> {
+            CarEntity car = new CarEntity();
+            car.setName(rs.getString("name"));
+            car.setPosition(rs.getInt("position"));
+            return car;
+        }, gameId);
+        return cars;
     }
 }
