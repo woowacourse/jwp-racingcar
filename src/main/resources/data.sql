0e0e9b819f33f387751fce59519876e8b2ca6a63
kdkdhoho <hkim4410@naver.com>
2023-04-16 16:25:34 +0900

    chore: ddl NOT NULL 키워드 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,16 @@
-CREATE TABLE game (
-id          int PRIMARY KEY AUTO_INCREMENT,
-trial_count int                                 NOT NULL,
-game_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+CREATE TABLE game
+(
+    id          int PRIMARY KEY AUTO_INCREMENT      NOT NULL,
+    trial_count int                                 NOT NULL,
+    game_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE record (
-game_id     int,
-position    int     NOT NULL,
-is_winner   boolean NOT NULL,
-player_name varchar(30),
-PRIMARY KEY (game_id, player_name),
-FOREIGN KEY (game_id) REFERENCES game (id)
+CREATE TABLE record
+(
+    game_id     int         NOT NULL,
+    position    int         NOT NULL,
+    is_winner   boolean     NOT NULL,
+    player_name varchar(30) NOT NULL,
+    PRIMARY KEY (game_id, player_name),
+    FOREIGN KEY (game_id) REFERENCES game (id)
 );
