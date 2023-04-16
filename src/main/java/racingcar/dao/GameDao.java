a5d994ea055bc7926e4d8dd7ac1f2ee636c2b79c
dooboocookie <bkh3030@gmail.com>
2023-04-16 22:46:59 +0900

    feat(GameDao): 전체 게임 조회하는 기능 추가

--- src/main/java/racingcar/dao/GameDao.java
+++ src/main/java/racingcar/dao/GameDao.java
@@ -2,11 +2,16 @@ package racingcar.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import racingcar.entity.GameEntity;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Objects;
 
 @Repository
@@ -16,6 +21,15 @@ public class GameDao {
     @Autowired
     public GameDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<GameEntity> findAll() {
+        final String sql = "SELECT id, trial_count FROM GAME ";
+        final RowMapper<GameEntity> gameEntityRowMapper = (resultSet, rowNum) ->
+                new GameEntity(
+                        resultSet.getLong("id"),
+                        resultSet.getInt("trial_count"));
+        return jdbcTemplate.query(sql, gameEntityRowMapper);
     }
 
     public Long save(final int trialCount) {
---
+++ src/main/java/racingcar/entity/GameEntity.java
@@ -0,0 +1,20 @@
+package racingcar.entity;
+
+public class GameEntity {
+
+    private final Long id;
+    private final int trialCount;
+
+    public GameEntity(final Long id, final int trialCount) {
+        this.id = id;
+        this.trialCount = trialCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getTrialCount() {
+        return trialCount;
+    }
+}
