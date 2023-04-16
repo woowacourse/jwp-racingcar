edd6f5d1d836dc0abc4a615682d5dc45550eb11e
rawfishthelgh <rg970604@naver.com>
2023-04-16 19:18:25 +0900

    refactor: 테이블 컬럼명 변경

--- src/main/java/racingcar/dao/PlayersInfoDAO.java
+++ src/main/java/racingcar/dao/PlayersInfoDAO.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import racingcar.domain.Car;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 public class PlayersInfoDAO {
