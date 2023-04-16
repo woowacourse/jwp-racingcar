5e9b766f978f55fe548116495fa570232d9e91ad
jjongwa <troas96@naver.com>
2023-04-16 15:09:36 +0900

    refactor: 생성자 통해 의존성 주입, 제네릭 타입 명시

--- src/main/java/racingcar/controller/SpringController.java
+++ src/main/java/racingcar/controller/SpringController.java
@@ -1,23 +1,24 @@
 package racingcar.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.domain.SpringService;
 import racingcar.dto.RequestDto;
 import racingcar.dto.ResponseDto;
 
 @RestController
-@RequestMapping("/plays")
 public class SpringController {
-    @Autowired
-    private SpringService springService;
 
-    @PostMapping("")
-    public ResponseEntity postInput(@RequestBody RequestDto requestDto) {
+    private final SpringService springService;
+
+    public SpringController(SpringService springService) {
+        this.springService = springService;
+    }
+
+    @PostMapping("/plays")
+    public ResponseEntity<ResponseDto> postInput(@RequestBody RequestDto requestDto) {
         springService.setUpGame(requestDto.getNames());
         springService.play(requestDto.getCount());
         ResponseDto responseDto = new ResponseDto(springService.findWinners(), springService.getCars());
