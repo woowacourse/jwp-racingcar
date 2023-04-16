c036975689b3bd02e266ba5e9b3b4163034bb844
JunDonghyuk <ehdgur4814@naver.com>
2023-04-16 23:42:30 +0900

    refactor(CarFactory): 생성자 접근제어자 변경

--- src/main/java/racingcar/domain/Car/CarFactory.java
+++ src/main/java/racingcar/domain/Car/CarFactory.java
@@ -1,4 +1,4 @@
-package racingcar.domain;
+package racingcar.domain.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,17 +7,15 @@ import java.util.stream.Collectors;
 
 public class CarFactory {
 
+    private CarFactory() {
+
+    }
+
     public static List<Car> generate(List<String> input, int startPoint) {
-        final List<String> carNames = new ArrayList<>(input);
+        List<String> carNames = new ArrayList<>(input);
         validateDuplicatedCarNames(carNames);
 
         return createCarsByNames(carNames, startPoint);
-    }
-
-    private static List<Car> createCarsByNames(List<String> carNames, int startPoint) {
-        return carNames.stream()
-                .map(carName -> new Car(carName, startPoint))
-                .collect(Collectors.toUnmodifiableList());
     }
 
     private static void validateDuplicatedCarNames(List<String> carNames) {
@@ -26,4 +24,9 @@ public class CarFactory {
         }
     }
 
+    private static List<Car> createCarsByNames(List<String> carNames, int startPoint) {
+        return carNames.stream()
+                .map(carName -> new Car(carName, startPoint))
+                .collect(Collectors.toUnmodifiableList());
+    }
 }
