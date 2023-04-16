6867234eac286d7a1d4c77fb5652a9a80e684cd1
odo27 <odo27@naver.com>
2023-04-16 16:47:19 +0900

    refactor: 기존 기능 수정 - 출력 방식 수정

--- src/main/java/racingcar/controller/RacingGameConsoleController.java
+++ src/main/java/racingcar/controller/RacingGameConsoleController.java
@@ -11,31 +11,31 @@ import racingcar.view.Input;
 import racingcar.view.Output;
 
 public class RacingGameConsoleController {
+
     private static final NumberGenerator numberGenerator = new RandomNumberGenerator();
     private static final CarGenerator carGenerator = new CarGenerator();
 
-    private String[] carNames;
     private int tryCount;
     private RacingGame racingGame;
     private List<Car> cars;
 
     public void run() {
         init();
-        racingGame = new RacingGame(cars, tryCount, this.numberGenerator);
+        racingGame = new RacingGame(cars, tryCount, numberGenerator);
         racingGame.run();
-        printResult(cars, tryCount);
+        printResult(cars);
     }
 
     private void init() {
         Output.printMessage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        this.carNames = Input.getCarNames(Input.getInput());
+        String[] carNames = Input.getCarNames(Input.getInput());
         Output.printMessage("시도할 회수는 몇회인가요?");
         this.tryCount = Input.getTryCount(Input.getInput());
         this.cars = carGenerator.generateCars(carNames);
     }
 
-    private void printResult(List<Car> cars, int tryCount) {
-        Output.printEachCarResult(cars, tryCount);
+    private void printResult(List<Car> cars) {
+        Output.printCarsResult(cars);
         Output.printWinner(racingGame.getWinner());
     }
 }
