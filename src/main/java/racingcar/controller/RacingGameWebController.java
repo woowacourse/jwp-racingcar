07c9655b7445c728eb3d79d241f8f30ad8093b4f
JunDonghyuk <ehdgur4814@naver.com>
2023-04-17 01:15:03 +0900

    style: 불필요한 import 제거

--- src/main/java/racingcar/controller/RacingGameWebController.java
+++ src/main/java/racingcar/controller/RacingGameWebController.java
@@ -12,7 +12,6 @@ import racingcar.dto.RacingGameResponseDto;
 import racingcar.service.RacingGameService;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
