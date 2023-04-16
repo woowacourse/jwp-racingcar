8217daab18315786c74638170636f84e227748f3
greeng00se <thegreengoose7@gmail.com>
2023-04-16 11:19:52 +0900

    refactor: 서비스 단위테스트 Mockito 사용하도록 변경

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -7,6 +7,9 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import racingcar.dao.CarDao;
+import racingcar.dao.GameDao;
 import racingcar.domain.NumberGenerator;
 import racingcar.dto.GameRequest;
 import racingcar.dto.GameResponse;
@@ -19,13 +22,10 @@ class RacingGameServiceTest {
     @Test
     void 자동차_경주를_진행한다() {
         // given
+        final GameDao gameDao = Mockito.mock(GameDao.class);
+        final CarDao carDao = Mockito.mock(CarDao.class);
         final NumberGenerator numberGenerator = new TestNumberGenerator(Lists.newArrayList(4, 3, 3));
-        final RacingGameService racingGameService = new RacingGameService(
-                numberGenerator,
-                (trialCount, winners) -> 1,
-                (gameId, cars) -> {
-                }
-        );
+        final RacingGameService racingGameService = new RacingGameService(numberGenerator, gameDao, carDao);
         final GameRequest gameRequest = new GameRequest("브리,비버,허브", 1);
 
         // when
