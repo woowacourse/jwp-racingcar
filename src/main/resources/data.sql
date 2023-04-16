dbdd909c70acf11d5593a75b858560785767fa36
songusika <sws981202@gmail.com>
2023-04-17 03:44:01 +0900

    refactor: unsigned 키워드 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,10 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
+DROP TABLE IF EXISTS `LOG`;
+DROP TABLE IF EXISTS PLAY_RESULT;
 
 CREATE TABLE PLAY_RESULT
-(
-    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    (
+    id          INT NOT NULL AUTO_INCREMENT,
     winners     VARCHAR(50) NOT NULL,
     trial_count INT         NOT NULL,
     created_at  DATETIME    NOT NULL,
@@ -11,8 +13,8 @@ CREATE TABLE PLAY_RESULT
 
 CREATE TABLE LOG
 (
-    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    game_id INT UNSIGNED NOT NULL,
+    id      INT NOT NULL AUTO_INCREMENT,
+    game_id INT NOT NULL,
     name    VARCHAR(50) NOT NULL,
     move    INT         NOT NULL,
     PRIMARY KEY (id),
