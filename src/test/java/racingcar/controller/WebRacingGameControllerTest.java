3f180ae503ac034f1aedabf8900c376d245a32a4
greeng00se <thegreengoose7@gmail.com>
2023-04-17 08:42:54 +0900

    test: WebRacingGameController 슬라이스 테스트로 변경

--- src/test/java/racingcar/controller/WebRacingGameControllerTest.java
+++ src/test/java/racingcar/controller/WebRacingGameControllerTest.java
@@ -2,24 +2,29 @@ package racingcar.controller;
 
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.any;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import racingcar.dto.CarDto;
 import racingcar.dto.GameRequest;
+import racingcar.dto.GameResponse;
+import racingcar.service.RacingGameService;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class WebRacingGameControllerTest {
@@ -30,19 +35,30 @@ class WebRacingGameControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private RacingGameService racingGameService;
+
     @Test
-    void 게임_진행() throws Exception {
+    void 게임을_진행한다() throws Exception {
         // given
         final GameRequest gameRequest = new GameRequest("비버,허브", 1);
         final String request = objectMapper.writeValueAsString(gameRequest);
+        final GameResponse gameResponse = new GameResponse(
+                "비버",
+                List.of(new CarDto("비버", 1), new CarDto("허브", 0))
+        );
+        given(racingGameService.play(any(GameRequest.class)))
+                .willReturn(gameResponse);
 
         // expect
         mockMvc.perform(post("/plays")
                         .content(request)
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("winners").exists())
+                .andExpect(jsonPath("winners").value("비버"))
                 .andExpect(jsonPath("racingCars", hasSize(2)))
+                .andExpect(jsonPath("$.racingCars[0].name").value("비버"))
+                .andExpect(jsonPath("$.racingCars[0].position").value(1))
                 .andDo(print());
     }
 }
