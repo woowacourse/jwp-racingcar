f16bc99a33698f5ef8c8a4bfb487cf2aa9c74201
sh111-coder <ohk9134@naver.com>
2023-04-16 21:32:32 +0900

    test(RaceServiceTest): RaceServiceTest 테스트 추가

---
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -0,0 +1,45 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import racingcar.controller.dto.GameInfoRequest;
+import racingcar.controller.dto.RaceResultResponse;
+
+@SpringBootTest
+class RaceServiceTest {
+
+    @Autowired
+    private RaceService raceService;
+
+    @Test
+    @DisplayName("게임 정보를 받아서 게임 결과를 저장한다.")
+    void saveRaceResult() {
+        // given
+        GameInfoRequest gameInfoRequest = new GameInfoRequest("성하,이오,코코닥", 5);
+
+        // when
+        int savedResultId = raceService.saveRaceResult(gameInfoRequest);
+
+        // then
+        assertThat(savedResultId).isNotNull();
+    }
+
+    @Test
+    @DisplayName("게임 결과를 생성한다.")
+    void createRaceResult() {
+        // given
+        GameInfoRequest gameInfoRequest = new GameInfoRequest("성하,이오,코코닥", 5);
+        int playResultId = raceService.saveRaceResult(gameInfoRequest);
+
+        // when
+        RaceResultResponse raceResultResponse = raceService.createRaceResult(playResultId);
+
+        // then
+        assertThat(raceResultResponse.getRacingCars()).isNotNull();
+        assertThat(raceResultResponse.getWinners()).isNotNull();
+    }
+}
