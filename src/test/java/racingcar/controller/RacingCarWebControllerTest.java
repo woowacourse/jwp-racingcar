348d8362e30e7a79c8af0f6ea6f4509ea2ee6a87
sh111-coder <ohk9134@naver.com>
2023-04-17 15:11:31 +0900

    test(RacingCarWebController): Random Port 환경 테스트로 변경 & @Transactional 대신 TruncateUtile 사용

--- src/test/java/racingcar/controller/RacingCarWebControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarWebControllerTest.java
@@ -3,15 +3,39 @@ package racingcar.controller;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import racingcar.controller.dto.GameInfoRequest;
+import racingcar.util.TruncateUtil;
 
-@SpringBootTest
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingCarWebControllerTest {
+
+    @Autowired
+    private TruncateUtil truncateUtil;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = this.port;
+    }
+
+    @AfterEach
+    void truncate() {
+        truncateUtil.truncate("car");
+        truncateUtil.truncate("race_result");
+    }
 
     @Test
     @DisplayName("자동차 이름, 시도 횟수가 담긴 요청 바디를 받아서 게임 결과를 응답 바디에 반환한다.")
