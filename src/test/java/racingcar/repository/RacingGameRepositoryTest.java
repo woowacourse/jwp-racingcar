32557b28d37f331fcbe604259948c9c28b7e6a35
jeomxon <jeomxon@gmail.com>
2023-04-17 22:51:57 +0900

    test: RacingGameRepository의 findAll에 대한 검증 추가

--- src/test/java/racingcar/repository/RacingGameRepositoryTest.java
+++ src/test/java/racingcar/repository/RacingGameRepositoryTest.java
@@ -3,6 +3,7 @@ package racingcar.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,7 @@ import racingcar.repository.mapper.RacingGameDtoMapper;
 class RacingGameRepositoryTest {
 
     @Autowired
-    RacingGameRepository repository;
+    RacingGameRepository racingGameRepository;
 
     @DisplayName("게임 정보 저장 후 조회")
     @Test
@@ -26,8 +27,8 @@ class RacingGameRepositoryTest {
         final int trial = 10;
 
         // when
-        final int savedId = repository.save(winners, trial);
-        final Optional<RacingGameDtoMapper> maybeRacingGameInfo = repository.findById(savedId);
+        final int savedId = racingGameRepository.save(winners, trial);
+        final Optional<RacingGameDtoMapper> maybeRacingGameInfo = racingGameRepository.findById(savedId);
 
         assertTrue(maybeRacingGameInfo.isPresent());
 
@@ -38,5 +39,20 @@ class RacingGameRepositoryTest {
                 .hasFieldOrPropertyWithValue("id", savedId)
                 .hasFieldOrPropertyWithValue("winners", winners)
                 .hasFieldOrPropertyWithValue("trial", trial);
+    }
+
+    @DisplayName("저장된 항목에 대한 전체 조회")
+    @Test
+    void findAll() {
+        // given
+        racingGameRepository.save("저문,헤나", 10);
+        racingGameRepository.save("저문,디노,우가", 10);
+        racingGameRepository.save("저문,디노,베베,우가", 10);
+
+        // when
+        List<RacingGameDtoMapper> racingGameDtoMappers = racingGameRepository.findAll();
+
+        //then
+        assertThat(racingGameDtoMappers.size()).isEqualTo(3);
     }
 }
