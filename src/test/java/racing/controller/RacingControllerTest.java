077b82b62b0d7e2c8eacccb35a42c71ae150f337
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-04-18 00:58:16 +0900

    feat: 게임별 자동차 결과 조회 api 추가

--- src/test/java/racing/controller/RacingControllerTest.java
+++ src/test/java/racing/controller/RacingControllerTest.java
@@ -5,11 +5,13 @@ import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +22,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import racing.controller.dto.request.RacingGameInfoRequest;
+import racing.domain.Car;
+import racing.domain.CarName;
+import racing.domain.Cars;
 import racing.repository.RacingGameDao;
 import racing.service.RacingGameService;
 
@@ -61,6 +66,33 @@ class RacingControllerTest {
                         .content(new ObjectMapper().writeValueAsBytes(request)))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.errorMessage").value("자동차 이름은 1 ~ 5자 사이 이어야 합니다."));
+    }
 
+    @DisplayName("진행된 게임의 이력을 조회 할 수 있다.")
+    @Test
+    void getAllResults() throws Exception {
+        List<Cars> cars = playedCarsFixture();
+        when(racingGameService.getAllResults()).thenReturn(cars);
+        when(racingGameService.filterWinnersToCarNames(cars.get(0))).thenReturn("CarA");
+        when(racingGameService.filterWinnersToCarNames(cars.get(1))).thenReturn("CarC,CarD");
+
+        mockMvc.perform(get("/plays"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].winners").value("CarA"))
+                .andExpect(jsonPath("$.[1].winners").value("CarC,CarD"));
+    }
+
+    private List<Cars> playedCarsFixture() {
+        Cars firstGameCars = new Cars(List.of(
+                new Car(new CarName("CarA"), 3),
+                new Car(new CarName("CarB"), 1)
+        ));
+
+        Cars secondGameCars = new Cars(List.of(
+                new Car(new CarName("CarC"), 2),
+                new Car(new CarName("CarD"), 2)
+        ));
+
+        return List.of(firstGameCars, secondGameCars);
     }
 }
