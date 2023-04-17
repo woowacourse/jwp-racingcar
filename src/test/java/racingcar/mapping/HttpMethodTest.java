86cc460acacbfa8c6671e9cd53e94b1096574df9
jjongwa <troas96@naver.com>
2023-04-17 22:16:12 +0900

    test: 이력 조회 GET 테스트

--- src/test/java/racingcar/mapping/HttpMethodTest.java
+++ src/test/java/racingcar/mapping/HttpMethodTest.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import racingcar.dto.RequestDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+import racingcar.dao.GameDao;
+import racingcar.dao.GameLogDao;
+import racingcar.dto.GameRequestDto;
 
 @DisplayName("Http Method")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,12 +28,24 @@ public class HttpMethodTest {
     @DisplayName("이름과 실행 횟수 POST")
     @Test
     void postInput() {
-        RequestDto requestDto = new RequestDto("브리,토미,브라운", 10);
+        GameRequestDto gameRequestDto = new GameRequestDto("브리,토미,브라운", 10);
 
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(requestDto)
+                .body(gameRequestDto)
                 .when().post("/plays")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @DisplayName("이력 조회 GET")
+    @Test
+    void getGameLog() {
+        postInput();
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/plays")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
     }
