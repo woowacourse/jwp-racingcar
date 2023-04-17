7eb671a8b623f9f194352f88e1ba084d762769b3
parkmuhyeun <pjhg410@gmail.com>
2023-04-18 00:57:05 +0900

    chore: 패키지 이동

--- src/main/java/racingcar/api/CarController.java
+++ src/main/java/racingcar/api/CarController.java
@@ -1,4 +1,4 @@
-package racingcar.controller;
+package racingcar.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/racingcar/api/advice/CarControllerAdvice.java
+++ src/main/java/racingcar/api/advice/CarControllerAdvice.java
@@ -1,4 +1,4 @@
-package racingcar.controller.advice;
+package racingcar.api.advice;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
