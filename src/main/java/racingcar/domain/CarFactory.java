d541e3544ad10470626f86b989e72140d5001199
kiarakim <kiarakim413@gmail.com>
2023-04-17 23:06:19 +0900

    refactor: 이름 앞 뒤 공백 제거

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -7,7 +7,7 @@ public class CarFactory {
 	public static List<Car> arrangeCars(String carNames) {
 		List<Car> cars = new ArrayList<>();
 		for (String carName : carNames.split(",")) {
-			cars.add(new Car(carName));
+			cars.add(new Car(carName.trim()));
 		}
 		return cars;
 	}
