5ee83f8be62a68a680eaecf6cd9b0dc46b46f562
BGuga <tmdwns9913@gmail.com>
2023-04-17 21:12:26 +0900

    refactor: dao Insert문에 batchInsert 적용

--- src/main/java/racingcar/dao/PlayersInfoDAO.java
+++ src/main/java/racingcar/dao/PlayersInfoDAO.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.Car;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +19,15 @@ public class PlayersInfoDAO {
 
     public void insert(int parentId, List<Car> cars) {
         String sql = "insert into players_info (name, position, parent_id) values (?, ?, ?)";
-        for (Car car : cars) {
-            jdbcTemplate.update(sql, car.getName(), car.getPosition(), parentId);
-        }
+        jdbcTemplate.batchUpdate(sql,
+                cars,
+                10,
+                (PreparedStatement ps, Car car) -> {
+                    ps.setString(1, car.getName());
+                    ps.setInt(2, car.getPosition());
+                    ps.setInt(3, parentId);
+                });
+
     }
 
 }
