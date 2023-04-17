837246204a982b5f0b2868314564159613e88ff8
nuyh <hjaehyun25@gmail.com>
2023-04-18 02:21:53 +0900

    feat: 콘솔 게임 경주 중간 과정 제거

--- src/main/java/racingcar/controller/RacingCarConsoleController.java
+++ src/main/java/racingcar/controller/RacingCarConsoleController.java
@@ -1,9 +1,13 @@
 package racingcar.controller;
 
+import racingcar.domain.RacingGame;
+import racingcar.dto.CarDto;
+import racingcar.dto.PlayResultDto;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarConsoleController {
 
@@ -11,12 +15,25 @@ public class RacingCarConsoleController {
     private final OutputView outputView = new OutputView();
 
     public void run() {
+        final RacingGame racingGame = createGame();
+
+        final String winners = racingGame.getWinnerNames().stream()
+                .collect(Collectors.joining(", "));
+        final List<CarDto> cars = racingGame.getCars().stream()
+                .map(CarDto::new)
+                .collect(Collectors.toList());
+
+        outputView.printPlayResult(new PlayResultDto(cars, winners));
+    }
+
+    private RacingGame createGame() {
         final List<String> carNames = inputView.askCarNames();
         final int racingCount = inputView.askRacingCount();
 
-//        final List<JudgedCarDto> judgedCars = new RacingGame().play(racingCount, carNames);
-//
-//        outputView.printPlayResult(PlayResponseDtoConverter.from(judgedCars));
+        final RacingGame racingGame = RacingGame.of(carNames);
+        racingGame.race(racingCount);
+
+        return racingGame;
     }
 
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,14 +1,17 @@
 package racingcar.view;
 
+import racingcar.dto.CarDto;
+import racingcar.dto.PlayResultDto;
+
 public class OutputView {
 
     private static final String CAR_STATUS_MESSAGE_FORMAT = "%s : %s\n";
     private static final String WINNERS_MESSAGE_FORMAT = "%s가 최종 우승했습니다.\n";
 
-//    public void printPlayResult(final PlayResponseDto playResult) {
-//        System.out.printf(WINNERS_MESSAGE_FORMAT, playResult.getWinners());
-//        for (CarDto car : playResult.getRacingCars()) {
-//            System.out.printf(CAR_STATUS_MESSAGE_FORMAT, car.getName(), car.getPosition());
-//        }
-//    }
+    public void printPlayResult(final PlayResultDto playResult) {
+        System.out.printf(WINNERS_MESSAGE_FORMAT, playResult.getWinners());
+        for (CarDto car : playResult.getRacingCars()) {
+            System.out.printf(CAR_STATUS_MESSAGE_FORMAT, car.getName(), car.getPosition());
+        }
+    }
 }
