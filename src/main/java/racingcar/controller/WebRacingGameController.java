f2f7d91603a1b693f55119ecb346faabbc3d3f89
greeng00se <thegreengoose7@gmail.com>
2023-04-18 01:57:16 +0900

    feat: RacingGameController 조회 기능 추가

--- src/main/java/racingcar/controller/WebRacingGameController.java
+++ src/main/java/racingcar/controller/WebRacingGameController.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,10 @@ public class WebRacingGameController {
     @PostMapping("/plays")
     public GamePlayResponseDto plays(@RequestBody final GamePlayRequestDto request) {
         return racingGameService.play(request);
+    }
+
+    @GetMapping("/plays")
+    public List<GamePlayResponseDto> findAll() {
+        return racingGameService.findAll();
     }
 }
