35f4996933add247094f6ced8cb8c80058877afb
drunkenhw <drunkenhw@gmail.com>
2023-04-17 22:29:19 +0900

    feat: 예외 처리 컨트롤러 추가

---
+++ src/main/java/racingcar/controller/ControllerAdvice.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleValidationExceptions(RuntimeException e) {
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleValidationExceptions(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
