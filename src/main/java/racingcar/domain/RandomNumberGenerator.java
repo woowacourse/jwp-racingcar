dd26930ee195309c6a502749600726b6eb1fe430
Woojin Lee <dhkep03@gmail.com>
2023-04-17 17:09:31 +0900

    refactor: 랜덤 객체 재사용

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -6,8 +6,9 @@ public class RandomNumberGenerator implements NumberGenerator {
 
     private static final int DIGIT_MAX = 10;
 
+    private final Random random = new Random();
+
     public int makeDigit() {
-        Random random = new Random();
-        return random.nextInt(DIGIT_MAX);
+        return this.random.nextInt(DIGIT_MAX);
     }
 }
