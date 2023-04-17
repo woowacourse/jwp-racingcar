b935e69c375b27f1411eeea7713c748d89d1a14c
odo27 <odo27@naver.com>
2023-04-17 13:11:07 +0900

    feat: 게임 플레이 이력 조회 테스트 구현

---
+++ src/test/java/racingcar/dao/GameQueryDaoTest.java
@@ -0,0 +1,57 @@
+package racingcar.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import racingcar.domain.Car;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GameQueryDaoTest {
+
+    @Autowired
+    private GameInsertDao gameInsertDao;
+    @Autowired
+    private PlayerInsertDao playerInsertDao;
+    @Autowired
+    private GameQueryDao gameQueryDao;
+
+    @Test
+    void getGameIds() {
+        int gameId1 = gameInsertDao.insertGame(3);
+        int gameId2 = gameInsertDao.insertGame(4);
+        int gameId3 = gameInsertDao.insertGame(5);
+        List<Integer> gameIds = gameQueryDao.getGameIds();
+        assertThat(gameIds).contains(gameId1, gameId2, gameId3);
+    }
+
+    @Test
+    void getCars() {
+        int gameId = gameInsertDao.insertGame(3);
+        playerInsertDao.insertPlayers(
+                gameId,
+                List.of(new Car("jena", 1), new Car("odo", 2)),
+                List.of("odo")
+        );
+        Car jena = gameQueryDao.getCars(gameId).get(0);
+        Car odo = gameQueryDao.getCars(gameId).get(1);
+        assertThat(jena.getName()).isEqualTo("jena");
+        assertThat(jena.getDistance()).isEqualTo(1);
+        assertThat(odo.getName()).isEqualTo("odo");
+        assertThat(odo.getDistance()).isEqualTo(2);
+    }
+
+    @Test
+    void getWinners() {
+        int gameId = gameInsertDao.insertGame(3);
+        playerInsertDao.insertPlayers(
+                gameId,
+                List.of(new Car("jena", 1), new Car("odo", 1)),
+                List.of("jena", "odo")
+        );
+        assertThat(gameQueryDao.getWinners(gameId)).containsExactly("jena", "odo");
+    }
+}
--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import racingcar.MockNumberGenerator;
+import racingcar.controller.GameResponseDto;
 import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
 
@@ -34,5 +35,27 @@ class RacingGameServiceTest {
         assertThat(odo.getDistance()).isEqualTo(1);
         assertThat(jena.getName()).isEqualTo("jena");
         assertThat(jena.getDistance()).isEqualTo(2);
+    }
+
+    @Test
+    void getGameResponseDtos() {
+        String names = "odo,jena";
+        RacingGame racingGame = racingGameService.play(
+                names,
+                3,
+                new MockNumberGenerator(Lists.newArrayList(3, 3, 3, 4, 4, 4))
+        );
+        racingGameService.save(racingGame);
+
+        List<GameResponseDto> gameResponseDtos = racingGameService.getGameResponseDtos();
+        GameResponseDto gameResponseDto = gameResponseDtos.get(gameResponseDtos.size() - 1);
+        List<Car> racingCars = gameResponseDto.getRacingCars();
+        Car odo = racingCars.get(0);
+        Car jena = racingCars.get(1);
+        assertThat(odo.getName()).isEqualTo("odo");
+        assertThat(jena.getName()).isEqualTo("jena");
+        assertThat(odo.getDistance()).isEqualTo(1);
+        assertThat(jena.getDistance()).isEqualTo(2);
+        assertThat(gameResponseDto.getWinners()).isEqualTo("jena");
     }
 }
