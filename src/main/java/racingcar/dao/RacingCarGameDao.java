ecaa6f309944397c69756da02ce61f9222ea3368
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-04-17 19:30:37 +0900

    refactor : 불필요한 코드 제거

--- src/main/java/racingcar/dao/RacingCarGameDao.java
+++ src/main/java/racingcar/dao/RacingCarGameDao.java
@@ -1,8 +1,6 @@
 package racingcar.dao;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Component;
 import racingcar.dao.entity.Game;
@@ -10,7 +8,6 @@ import racingcar.dao.entity.Player;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.util.Optional;
 
 @Component
 public class RacingCarGameDao {
@@ -20,15 +17,6 @@ public class RacingCarGameDao {
     public RacingCarGameDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-
-    RowMapper<Player> playerRowMapper = (rs, rowNum) -> {
-        Long playerId = rs.getLong("player_id");
-        String playerName = rs.getString("name");
-        int position = rs.getInt("position");
-        Long gameId = rs.getLong("game_id");
-
-        return new Player(playerId, playerName, position, gameId);
-    };
 
     public Long insertGameWithKeyHolder(Game game) {
         String sql = "INSERT INTO game(play_count, winners) VALUES(?, ?)";
@@ -49,23 +37,6 @@ public class RacingCarGameDao {
         String sql = "INSERT INTO player(name, position, game_id) VALUES(?, ?, ?)";
 
         jdbcTemplate.update(sql, player.getName(), player.getPosition(), player.getGameId());
-    }
-
-    public void updatePlayer(Player player) {
-        String sql = "UPDATE player SET name = ?, position = ? WHERE player_id = ?";
-
-        jdbcTemplate.update(sql, player.getName(), player.getPosition(), player.getPlayerId());
-    }
-
-    public Optional<Player> findPlayerByGameIdAndName(Long gameId, String name) {
-        String sql = "SELECT * FROM player WHERE game_id = ? AND name = ?";
-
-        try {
-            Player player = jdbcTemplate.queryForObject(sql, playerRowMapper, gameId, name);
-            return Optional.ofNullable(player);
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
     }
 
     public void updateGame(Game game) {
