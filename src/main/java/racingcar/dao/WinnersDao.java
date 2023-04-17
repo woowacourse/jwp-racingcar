ab603d1ce739f6c39018371c8c259c345144a161
jjongwa <troas96@naver.com>
2023-04-17 17:36:17 +0900

    feat: 우승자 리스트 반환 기능 추가

--- src/main/java/racingcar/dao/WinnersDao.java
+++ src/main/java/racingcar/dao/WinnersDao.java
@@ -1,6 +1,8 @@
 package racingcar.dao;
 
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,8 +14,15 @@ public class WinnersDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    private final RowMapper<String> winnerRowMapper = (resultSet, rowNum) -> resultSet.getString("winner");
+
     public void insert(int gameNumber, String winner) {
         String sql = "insert into winners(game_number, winner) values(?,?)";
         jdbcTemplate.update(sql, gameNumber, winner);
+    }
+
+    public List<String> find(int gameNumber) {
+        String sql = "select w.winner from winners w where w.game_number = ?";
+        return jdbcTemplate.query(sql, winnerRowMapper, gameNumber);
     }
 }
