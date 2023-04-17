e1620a3fa6e0c2391792a927ac646d484f7b9fd0
iamjooon2 <iamjooon2@gmail.com>
2023-04-17 21:59:01 +0900

    test: 게임의 수 확인 테스트 추가

--- src/test/java/racingcar/dao/JdbcGameDaoTest.java
+++ src/test/java/racingcar/dao/JdbcGameDaoTest.java
@@ -2,6 +2,8 @@ package racingcar.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -16,6 +18,7 @@ import racingcar.dao.entity.GameEntity;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JdbcGameDaoTest {
 
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -26,15 +29,32 @@ public class JdbcGameDaoTest {
         gameDao = new JdbcGameDao(jdbcTemplate);
     }
 
+
     @Test
     void 게임을_저장한다() {
         // given
         final int trialCount = 5;
 
         // when
-        final long id = gameDao.insert(GameEntity.create(trialCount));
+        final Long id = gameDao.insert(GameEntity.create(trialCount));
 
         // then
         assertThat(id).isNotNull();
+    }
+
+    @Test
+    void 저장된_게임의_수를_알_수_있다() {
+        // given
+        jdbcTemplate.execute("DELETE FROM GAME");
+
+        gameDao.insert(GameEntity.create(5));
+        gameDao.insert(GameEntity.create(5));
+        gameDao.insert(GameEntity.create(5));
+
+        // when
+        final int count = gameDao.countGames();
+
+        // then
+        assertThat(count).isEqualTo(3);
     }
 }
