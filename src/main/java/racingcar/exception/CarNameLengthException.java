92217421bab7944a327fc79aca12e1f6ac922f5f
Woojin Lee <dhkep03@gmail.com>
2023-04-17 17:12:19 +0900

    refactor: 자동차 이름 예외 메시지의 정책을 상수화

--- src/main/java/racingcar/exception/CarNameLengthException.java
+++ src/main/java/racingcar/exception/CarNameLengthException.java
@@ -2,9 +2,12 @@ package racingcar.exception;
 
 public class CarNameLengthException extends RuntimeException {
 
-    private static final String MESSAGE = "[ERROR] 자동차 이름의 길이는 1자 이상, 5자 이하여야 합니다.";
+    private static final int MINIMUM_LENGTH = 1;
+    private static final int MAXIMUM_LENGTH = 5;
 
     public CarNameLengthException() {
-        super(MESSAGE);
+        super(String.format(
+                "[ERROR] 자동차 이름의 길이는 %d자 이상, %d자 이하여야 합니다.",
+                MINIMUM_LENGTH, MAXIMUM_LENGTH));
     }
 }
