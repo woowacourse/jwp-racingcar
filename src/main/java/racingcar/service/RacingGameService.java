1b5ecfe0fe253bfdecaa63c67295c0f8962527bf
jeomxon <jeomxon@gmail.com>
2023-04-17 21:49:11 +0900

    feat: 이력을 조회하는 기능 구현

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,10 +1,14 @@
 package racingcar.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import racingcar.controller.dto.CarDto;
 import racingcar.controller.dto.RacingGameResponse;
 import racingcar.domain.CarGroup;
 import racingcar.domain.Name;
@@ -12,6 +16,8 @@ import racingcar.domain.RacingGame;
 import racingcar.domain.RandomNumberGenerator;
 import racingcar.repository.PlayerRepository;
 import racingcar.repository.RacingGameRepository;
+import racingcar.repository.mapper.PlayerDtoMapper;
+import racingcar.repository.mapper.RacingGameDtoMapper;
 
 @Service
 @Transactional(readOnly = true)
@@ -37,7 +43,19 @@ public class RacingGameService {
             throw new IllegalStateException("[ERROR] 레이싱 플레이어 저장에 실패하였습니다.");
         }
 
-        return new RacingGameResponse(winners, carGroup.getCars());
+        final List<CarDto> carDtos = createCarDto(carGroup);
+
+        return new RacingGameResponse(winners, carDtos);
+    }
+
+    public List<RacingGameResponse> findHistory() {
+        final List<RacingGameDtoMapper> gameHistories = racingGameRepository.findAll();
+        final List<Integer> gameHistoryIds = getGameHistoryIds(gameHistories);
+        final List<List<CarDto>> playerHistories = getPlayerHistories(gameHistoryIds);
+
+        return IntStream.range(0, gameHistories.size())
+                .mapToObj(i -> new RacingGameResponse(gameHistories.get(i).getWinners(), playerHistories.get(i)))
+                .collect(Collectors.toList());
     }
 
     private void raceBy(final int count, final RacingGame racingGame) {
@@ -52,5 +70,38 @@ public class RacingGameService {
                 .stream()
                 .map(Name::getName)
                 .collect(Collectors.joining(","));
+    }
+
+    private List<CarDto> createCarDto(final CarGroup carGroup) {
+        return carGroup.getCars().stream()
+                .map(car -> new CarDto(car.getName().getName(), car.getPosition().getPosition()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> getGameHistoryIds(final List<RacingGameDtoMapper> gameHistories) {
+        final List<Integer> gameHistoryIds = new ArrayList<>();
+        for (final RacingGameDtoMapper gameHistory : gameHistories) {
+            gameHistoryIds.add(gameHistory.getId());
+        }
+        return gameHistoryIds;
+    }
+
+    private List<List<CarDto>> getPlayerHistories(final List<Integer> gameHistoryIds) {
+        final List<List<CarDto>> playerHistories = new ArrayList<>();
+        for (final Integer gameHistoryId : gameHistoryIds) {
+            final List<PlayerDtoMapper> playerDtoMappers = playerRepository.findAllById(gameHistoryId);
+            final List<CarDto> carDtos = createCarDtos(playerDtoMappers);
+            playerHistories.add(carDtos);
+        }
+        return playerHistories;
+    }
+
+    private List<CarDto> createCarDtos(final List<PlayerDtoMapper> playerDtoMappers) {
+        final List<CarDto> carDtos = new ArrayList<>();
+        for (final PlayerDtoMapper playerDtoMapper : playerDtoMappers) {
+            final CarDto carDto = new CarDto(playerDtoMapper.getName(), playerDtoMapper.getPosition());
+            carDtos.add(carDto);
+        }
+        return carDtos;
     }
 }
