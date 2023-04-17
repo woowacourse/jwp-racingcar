6c039f35c8f60b1c40660c7478923538c892ed25
jjongwa <troas96@naver.com>
2023-04-17 17:35:52 +0900

    test: 우승자 리스트 반환 테스트

--- src/test/java/racingcar/dao/WinnersDaoTest.java
+++ src/test/java/racingcar/dao/WinnersDaoTest.java
@@ -1,7 +1,9 @@
 package racingcar.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -17,5 +19,12 @@ public class WinnersDaoTest {
     @Test
     void insert() {
         assertThatNoException().isThrownBy(() -> winnersDao.insert(5, "달리"));
+    }
+
+    @Test
+    void find() {
+        winnersDao.insert(2, "달리");
+        winnersDao.insert(2, "디노");
+        assertThat(winnersDao.find(2)).isEqualTo(List.of("달리", "디노"));
     }
 }
