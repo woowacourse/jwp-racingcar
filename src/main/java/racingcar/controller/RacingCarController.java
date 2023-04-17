998a3fe496116ed482425608ea8c22cb19eee925
nuyh <hjaehyun25@gmail.com>
2023-04-18 00:50:51 +0900

    refactor: 컨트롤러 로직들 메서드 분리

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.dao.RacingCarDao;
-import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
 import racingcar.dto.CarDto;
 import racingcar.dto.GameResultDto;
@@ -28,22 +27,33 @@ public class RacingCarController {
     @Transactional
     @PostMapping("/plays")
     public ResponseEntity<GameResultDto> play(@RequestBody PlayRequestDto playRequestDto) {
-        RacingGame racingGame = createGame(playRequestDto.getNames());
-        int count = playRequestDto.getCount();
+        final RacingGame racingGame = createGame(playRequestDto.getNames());
+        final int count = playRequestDto.getCount();
         racingGame.race(count);
 
-        String winners = String.join(", ", racingGame.getWinnerNames());
-        final List<Car> cars = racingGame.getCars();
+        saveGame(count, racingGame);
 
-        final long resultId = racingCarDao.saveWinners(count, winners);
-        racingCarDao.saveCars(resultId, cars);
-
-        final List<CarDto> carsDto = cars.stream().map(CarDto::new).collect(Collectors.toList());
-        return ResponseEntity.ok(new GameResultDto(carsDto, winners));
+        return makeGameResultDto(racingGame);
     }
 
     private static RacingGame createGame(final String rawCarNames) {
-        List<String> carNames = TextParser.parseByDelimiter(rawCarNames, ",");
+        final List<String> carNames = TextParser.parseByDelimiter(rawCarNames, ",");
         return RacingGame.of(carNames);
+    }
+
+    private void saveGame(final int count, final RacingGame racingGame) {
+        final String winners = String.join(", ", racingGame.getWinnerNames());
+
+        final long resultId = racingCarDao.saveWinners(count, winners);
+        racingCarDao.saveCars(resultId, racingGame.getCars());
+    }
+
+    private ResponseEntity<GameResultDto> makeGameResultDto(final RacingGame racingGame) {
+        final List<CarDto> carsDto = racingGame.getCars().stream()
+                .map(CarDto::new)
+                .collect(Collectors.toList());
+        final String winners = String.join(", ", racingGame.getWinnerNames());
+
+        return ResponseEntity.ok(new GameResultDto(carsDto, winners));
     }
 }
