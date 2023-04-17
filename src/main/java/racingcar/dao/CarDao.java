3422f2d50a7478334dac5a0ce379537a16c5c820
kwonyj1022 <kwonyj1022@naver.com>
2023-04-17 22:50:54 +0900

    refactor: CarDao의 save()의 접근지정자를 private로 변경

--- src/main/java/racingcar/dao/CarDao.java
+++ src/main/java/racingcar/dao/CarDao.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public interface CarDao {
 
-    void save(Long gameId, CarDto carDto);
-
     void saveAll(Long gameId, List<CarDto> racingCars);
 
     List<CarDto> findCarsByGameId(Long gameId);
