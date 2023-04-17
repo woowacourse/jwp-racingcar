0a8c23fe55e607d965adf36b4f0daff0a24c52a5
donghae <donghae1999@gmail.com>
2023-04-17 17:17:14 +0900

    feat: getGameRecord Get 메소드 추가

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -3,13 +3,16 @@ package racingcar.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import racingcar.dto.GameRecordResponseDto;
 import racingcar.dto.RacingGameRequestDto;
 import racingcar.dto.RacingGameResponseDto;
 import racingcar.service.GameService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -25,5 +28,11 @@ public class WebController {
     public ResponseEntity<RacingGameResponseDto> playGame(final @RequestBody @Valid RacingGameRequestDto racingGameRequestDto) {
         RacingGameResponseDto racingGameResponseDto = gameService.executeRacingGame(racingGameRequestDto);
         return ResponseEntity.ok(racingGameResponseDto);
+    }
+
+    @GetMapping("/plays")
+    public ResponseEntity<List<GameRecordResponseDto>> getGameRecord(){
+        List<GameRecordResponseDto> gameRecordResponseDto = gameService.getGameRecord();
+        return ResponseEntity.ok(gameRecordResponseDto);
     }
 }
