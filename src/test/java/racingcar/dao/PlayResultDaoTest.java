466f99076d0d8ff09d1dbc731c4a8834e60bde9b
hong-sile <gurwns9325@naver.com>
2023-04-17 19:53:55 +0900

    test: SET REFERENTIAL_INTEGRITY로 PK에 대한 유혀성 검사를 테스트에서 제거
    - 더이상 테스트와 무관한 DAO에 의존할 필요가 없음

--- src/test/java/racingcar/dao/PlayResultDaoTest.java
+++ src/test/java/racingcar/dao/PlayResultDaoTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @JdbcTest
@@ -26,6 +27,7 @@ class PlayResultDaoTest {
 
     @DisplayName("게임 결과가 저장되면 id를 반환하는 기능 테스트")
     @Test
+    @Transactional
     void Should_ReturnGameId_When_InsertPlayResult() {
         Long gameId = playResultDao.insertWithKeyHolder(10, List.of("tori", "hong"));
         assertThat(gameId).isNotNull();
--- src/test/java/racingcar/dao/RacingCarDaoTest.java
+++ src/test/java/racingcar/dao/RacingCarDaoTest.java
@@ -2,35 +2,35 @@ package racingcar.dao;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @JdbcTest
 class RacingCarDaoTest {
+
     private RacingCarDao racingCarDao;
-    private Long gameId;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @BeforeEach
+    @BeforeAll
     void setup() {
-        jdbcTemplate.update("DELETE FROM RACING_CAR");
-
-        PlayResultDao playResultDao = new PlayResultDao(jdbcTemplate);
         racingCarDao = new RacingCarDao(jdbcTemplate);
-
-        gameId = playResultDao.insertWithKeyHolder(10, List.of("tori", "hong"));
+        jdbcTemplate.update("SET REFERENTIAL_INTEGRITY FALSE");
     }
 
     @DisplayName("자동차 이름과 포지션을 저장하는 기능 테스트")
     @Test
+    @Transactional
     void Should_Success_When_InsertRacingCar() {
+        long gameId = 1L;
         assertDoesNotThrow(() -> racingCarDao.insert(gameId, "tori", 9));
     }
 }
