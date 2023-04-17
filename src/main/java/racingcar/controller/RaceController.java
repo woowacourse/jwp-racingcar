586e1b693908e85c87bed35b99a4baf5db928843
jiwon <ljwon77@daum.net>
2023-04-17 12:58:36 +0900

    feat: @WebMVCTest로 변경하고 @RequestMapping을 이용하도록 변경한다

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.dto.RaceRequest;
 import racingcar.dto.RaceResponse;
@@ -13,6 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/plays")
 public class RaceController {
 
     private final RaceService raceService;
@@ -21,12 +23,12 @@ public class RaceController {
         this.raceService = raceService;
     }
 
-    @PostMapping("/plays")
+    @PostMapping
     public ResponseEntity<RaceResponse> play(@RequestBody @Valid final RaceRequest raceRequest) {
         return ResponseEntity.ok().body(raceService.play(raceRequest));
     }
 
-    @GetMapping("/plays")
+    @GetMapping
     public ResponseEntity<List<RaceResponse>> raceResult() {
         return ResponseEntity.ok().body(raceService.getRaceResult());
     }
--- src/test/java/racingcar/controller/RaceControllerTest.java
+++ src/test/java/racingcar/controller/RaceControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -24,11 +25,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(RaceController.class)
 @AutoConfigureMockMvc
 class RaceControllerTest {
 
