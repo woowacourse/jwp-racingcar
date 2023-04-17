1af63de3b50037140cefad1aa13b9a9fe95c07b7
yenawee <yaena0319@naver.com>
2023-04-17 21:56:40 +0900

    refactor : data table UNSIGNED 적용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 CREATE TABLE GAME
 (
-    game_id     INT         NOT NULL AUTO_INCREMENT,
+    game_id     INT         UNSIGNED NOT NULL AUTO_INCREMENT,
     winners     VARCHAR(50) NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
-    trial_count INT         NOT NULL,
+    trial_count INT         UNSIGNED NOT NULL,
 
     PRIMARY KEY (game_id)
 );
 
 CREATE TABLE PLAYER
 (
-    player_id INT         NOT NULL AUTO_INCREMENT,
-    game_id   INT         NOT NULL,
+    player_id INT         UNSIGNED NOT NULL AUTO_INCREMENT,
+    game_id   INT         UNSIGNED NOT NULL,
     name      VARCHAR(10) NOT NULL,
-    position  INT         NOT NULL,
+    position  INT         UNSIGNED NOT NULL,
 
     FOREIGN KEY (game_id) REFERENCES GAME (game_id),
     PRIMARY KEY (player_id)
