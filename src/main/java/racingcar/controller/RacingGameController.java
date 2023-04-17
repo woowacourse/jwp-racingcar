1c272f065231cf76b18f74dd262f9937b5e0524e
SproutMj <mjk54565@gmail.com>
2023-04-17 15:19:21 +0900

    refactor: import 최적화

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -9,12 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import racingcar.domain.Cars;
 import racingcar.domain.TryCount;
 import racingcar.dto.GameResultResponseDto;
 import racingcar.dto.StartGameRequestDto;
 import racingcar.service.RacingCarService;
-import racingcar.utils.CarsFactory;
 
 @RestController
 public class RacingGameController {
@@ -29,7 +27,8 @@ public class RacingGameController {
 	public ResponseEntity<GameResultResponseDto> startGame (@RequestBody final StartGameRequestDto request) {
 		TryCount tryCount = getTryCount(request.getCount());
 
-		return new ResponseEntity<>(racingCarService.startRace(splitCarNames(request.getNames()), tryCount), HttpStatus.OK);
+		return new ResponseEntity<>(racingCarService.startRace(splitCarNames(request.getNames()), tryCount),
+				HttpStatus.OK);
 	}
 
 	private List<String> splitCarNames (final String input) {
