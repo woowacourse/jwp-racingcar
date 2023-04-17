faf3207292c3526552a43e7f4f717665b7742b02
sh111-coder <ohk9134@naver.com>
2023-04-17 21:28:06 +0900

    refactor: 불필요한 import 제거

--- src/test/java/racingcar/dao/raceresult/RaceResultDaoTest.java
+++ src/test/java/racingcar/dao/raceresult/RaceResultDaoTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import racingcar.dao.raceresult.dto.RaceResultRegisterRequest;
-import racingcar.dao.raceresult.entity.RaceResultEntity;
 import racingcar.domain.RacingCars;
 import racingcar.util.RandomNumberGenerator;
 
