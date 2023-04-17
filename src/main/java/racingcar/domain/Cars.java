cc84a21ef518b9e3049999be677be1818c8def1b
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-04-17 18:13:28 +0900

    refactor: 자동차 목록 클래스명 수정, 자동차 목록 생성 구현 코드 리팩터링

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,49 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Cars {
+    private static final int MINIMUM_NUMBER_OF_CARS = 2;
+    private static final int MAXIMUM_NUMBER_OF_CARS = 50;
+
+    private final List<Car> racingCars;
+
+    public Cars(final String inputName) {
+        final List<String> names = List.of(inputName.split(","));
+        validate(names);
+        this.racingCars = createBy(names);
+    }
+
+    private void validate(final List<String> names) {
+        if (isDuplicated(names)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복이 허용되지 않습니다.");
+        }
+        if (isSizeNotOk(names)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 대수는 2-50대 사이이어야 합니다.");
+        }
+    }
+
+    private List<Car> createBy(final List<String> names) {
+        return names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isDuplicated(final List<String> names) {
+        return names.size() != names.stream().distinct().count();
+    }
+
+    private boolean isSizeNotOk(final List<String> names) {
+        return !(MINIMUM_NUMBER_OF_CARS <= names.size() && names.size() <= MAXIMUM_NUMBER_OF_CARS);
+    }
+
+    public void race(final NumberGenerator numberGenerator) {
+        racingCars.forEach(car -> car.move(numberGenerator));
+    }
+
+    public List<Car> getRacingCars() {
+        return new ArrayList<>(racingCars);
+    }
+}
---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,76 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class CarsTest {
+    @DisplayName("Cars의 size가 1이상 50이하일 경우 생성한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 49})
+    void create(final int namesSize) {
+        // given
+        final String nameValuesBetween1And50 = IntStream.rangeClosed(0, namesSize)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        // expect
+        assertDoesNotThrow(() -> new Cars(nameValuesBetween1And50));
+    }
+
+    @DisplayName("Cars의 size가 1일 경우 예외가 발생한다.")
+    @Test
+    void throwExceptionWhenCarsSizeIsOne() {
+        assertThatThrownBy(() -> new Cars("헤나"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Cars의 size가 50을 초과한 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {51, 52, 53, 54, 55, 100, 1000})
+    void throwExceptionWhenCarsSizeIsOver50(final int namesSize) {
+        // given
+        final String nameValuesOver50 = IntStream.rangeClosed(1, namesSize)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        // expect
+        assertThatThrownBy(() -> new Cars(nameValuesOver50))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 중복될 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"헤나,헤나", "헤나,헤나,찰리", "헤나,헤나,헤나", "헤나,헤나,찰리,헤찰,리찰,나헤"})
+    void throwExceptionWhenCarNameIsDuplicated(final String nameValues) {
+        assertThatThrownBy(() -> new Cars(nameValues))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("모든 자동차들이 경주를 진행한다.")
+    @Test
+    void race() {
+        // given
+        final Cars cars = new Cars("헤나,찰리");
+        final NumberGenerator movableNumberGenerator = new MovableNumberGenerator();
+
+        // when
+        cars.race(movableNumberGenerator);
+        final List<Position> carPositions = cars.getRacingCars()
+                .stream()
+                .map(Car::getPosition)
+                .collect(Collectors.toList());
+
+        // then
+        assertThat(carPositions).containsExactly(new Position(1), new Position(1));
+    }
+}
