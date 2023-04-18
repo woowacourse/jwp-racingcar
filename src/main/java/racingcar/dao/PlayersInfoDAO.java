09b8a9f733e3b18594e310e329b8525d54831838
rawfishthelgh <rg970604@naver.com>
2023-04-18 19:40:44 +0900

    refactor : batchUpdate 활용한 쿼리 횟수 최적화

--- src/main/java/racingcar/dao/PlayersInfoDAO.java
+++ src/main/java/racingcar/dao/PlayersInfoDAO.java
@@ -1,9 +1,12 @@
 package racingcar.dao;
 
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.Car;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -17,9 +20,20 @@ public class PlayersInfoDAO {
 
     public void insert(int playResultId, List<Car> cars) {
         String sql = "insert into players_info (name, position, play_result_id) values (?, ?, ?)";
-        for (Car car : cars) {
-            jdbcTemplate.update(sql, car.getName(), car.getPosition(), playResultId);
-        }
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                Car car = cars.get(i);
+                ps.setString(1, car.getName());
+                ps.setInt(2, car.getPosition());
+                ps.setInt(3, playResultId);
+            }
+
+            @Override
+            public int getBatchSize() {
+                return cars.size();
+            }
+        });
     }
 
 }
