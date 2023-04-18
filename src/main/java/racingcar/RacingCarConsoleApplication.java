c50fa2955d603c2501c282b5f1aff0db189f5089
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-04-18 20:32:32 +0900

    feat: 콘솔 레이싱 게임 실행 컨트롤러 추가

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 import racingcar.controller.RacingGameConsoleController;
-
-import java.io.IOException;
+import racingcar.service.RacingGameConsoleService;
+import racingcar.service.RacingGameService;
 
 public class RacingCarConsoleApplication {
-    public static void main(String[] args) throws IOException {
-        RacingGameConsoleController racingGameConsoleController = new RacingGameConsoleController();
+    public static void main(String[] args) {
+        final RacingGameService racingGameConsoleService = new RacingGameConsoleService();
+        final RacingGameConsoleController racingGameConsoleController = new RacingGameConsoleController(racingGameConsoleService);
         racingGameConsoleController.run();
     }
 }
--- src/main/java/racingcar/controller/RacingGameConsoleController.java
+++ src/main/java/racingcar/controller/RacingGameConsoleController.java
@@ -1,56 +1,42 @@
 package racingcar.controller;
 
+import racingcar.controller.dto.RacingGameResponse;
 import racingcar.domain.Cars;
-import racingcar.domain.RacingGame;
-import racingcar.domain.RacingResult;
-import racingcar.domain.RandomNumberGenerator;
+import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGameConsoleController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final RacingGameService racingGameService;
 
-    public RacingGameConsoleController() {
+    public RacingGameConsoleController(final RacingGameService racingGameService) {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.racingGameService = racingGameService;
     }
 
     public void run() {
-        Cars cars = createCarGroup();
-        int movingTrial = createMovingTrial();
-
-        RacingGame racingGame = new RacingGame(cars, new RandomNumberGenerator());
-
-        outputView.printNotice();
-        raceWithHistory(movingTrial, racingGame);
-        outputView.printWinner(racingGame.createRacingResult().pickWinner());
+        final RacingGameResponse racingGameResponse = racingGameService.race(readCars(), readTrial());
+        outputView.printRacingResult(racingGameResponse);
     }
 
-    private Cars createCarGroup() {
+    private Cars readCars() {
         try {
             return new Cars(inputView.readCarNames());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return createCarGroup();
+            return readCars();
         }
     }
 
-    private int createMovingTrial() {
+    private int readTrial() {
         try {
             return inputView.readMovingTrial();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return createMovingTrial();
-        }
-    }
-
-    private void raceWithHistory(int movingTrial, RacingGame racingGame) {
-        for (int i = 0; i < movingTrial; i++) {
-//            racingGame.race();
-
-            RacingResult racingResult = racingGame.createRacingResult();
-            outputView.printRacingResult(racingResult.getHistory());
+            return readTrial();
         }
     }
 }
