bb66ccb7b7cc93750acdb7cf9666e69e1e2b4b5e
cpot5620 <=cpot5620@naver.com>
2023-04-18 16:11:55 +0900

    test(Dao): 게임 및 플레이어 결과 find 테스트 구현
    GAME_RESULT, PLAYER_RESULT join 연산 수행

--- src/test/java/racingcar/dao/GameResultDAOTest.java
+++ src/test/java/racingcar/dao/GameResultDAOTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.Transactional;
+import racingcar.dto.CarDto;
 import racingcar.dto.GameResultDto;
+import racingcar.dto.PlayerResultDto;
 
 import java.util.List;
 
@@ -18,7 +20,10 @@ class GameResultDAOTest {
     @Autowired
     private GameResultDAO gameResultDAO;
 
-    @DisplayName("게임 결과를 저장할 수 있다.")
+    @Autowired
+    private PlayerResultDAO playerResultDAO;
+
+    @DisplayName("최종 게임 결과를 저장할 수 있다.")
     @Transactional
     @Test
     void findAllGameResultTest() {
@@ -39,5 +44,29 @@ class GameResultDAOTest {
             assertThat(findResults.get(i).getTrialCount())
                     .isEqualTo(results.get(i).getTrialCount());
         }
+    }
+
+    @DisplayName("게임 결과 및 플레이어들의 결과를 모두 가져올 수 있다.")
+    @Transactional
+    @Test
+    void findAllGameAndPlayerResultsTest() {
+        //given
+        GameResultDto gameResult = GameResultDto.from(5);
+        int savedId = gameResultDAO.save(gameResult);
+        PlayerResultDto playerResult = PlayerResultDto.of(List.of(CarDto.of("쥬니", 1)), savedId);
+        playerResultDAO.saveAll(playerResult);
+
+        //when
+        GameAndPlayerResultEntity gameAndPlayerResult = gameResultDAO.findAllWithPlayerResults().get(0);
+
+        //then
+        assertThat(gameAndPlayerResult.getGameId())
+                .isEqualTo(savedId);
+        assertThat(gameAndPlayerResult.getTrialCount())
+                .isEqualTo(5);
+        assertThat(gameAndPlayerResult.getName())
+                .isEqualTo("쥬니");
+        assertThat(gameAndPlayerResult.getPosition())
+                .isEqualTo(1);
     }
 }
