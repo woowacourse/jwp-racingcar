4fa2a3888f440a3f80786ef56e22011b7abfcead
dooboocookie <bkh3030@gmail.com>
2023-04-18 16:45:59 +0900

    test(WebController, RacingCarService): 이력 조회 관련 테스트 추가 및 Mockito 사용

--- src/test/java/racingcar/controller/WebControllerTest.java
+++ src/test/java/racingcar/controller/WebControllerTest.java
@@ -1,34 +1,81 @@
 package racingcar.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import racingcar.dto.NamesAndCountRequest;
+import racingcar.dto.RacingCarResponse;
 import racingcar.dto.ResultResponse;
+import racingcar.service.RacingCarService;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@WebMvcTest(WebController.class)
 class WebControllerTest {
 
-    private final WebController webController;
-
     @Autowired
-    public WebControllerTest(final WebController webController) {
-        this.webController = webController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RacingCarService racingCarService;
+
+    @DisplayName("/plays GET 요청 시, 전체 게임에 대하여 승자와 결과를 200 상태 코드로 반환한다.")
+    @Test
+    void searchAllHistories() throws Exception {
+        List<ResultResponse> resultResponses = List.of(
+                new ResultResponse("망고",
+                        List.of(new RacingCarResponse("망고", 7),
+                                new RacingCarResponse("루카", 5))),
+                new ResultResponse("소니",
+                        List.of(new RacingCarResponse("소니", 20),
+                                new RacingCarResponse("현구막", 15)))
+        );
+        //mocking
+        Mockito.when(racingCarService.searchAllGame()).thenReturn(resultResponses);
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/plays"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].winners").value("망고"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].racingCars[0].name").value("망고"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].racingCars[0].position").value(7))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].racingCars[1].name").value("루카"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].racingCars[1].position").value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].winners").value("소니"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].racingCars[0].name").value("소니"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].racingCars[0].position").value(20))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].racingCars[1].name").value("현구막"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].racingCars[1].position").value(15));
     }
 
-    @DisplayName("정상 작동 후 200 상태 코드 반환한다.")
+    @DisplayName("/plays POST 요청 시, 정상 작동 후, 승자와 결과를 200 상태 코드로 반환한다.")
     @Test
-    void play() {
+    void play() throws Exception {
         //given
-        NamesAndCountRequest namesAndCountRequest = new NamesAndCountRequest("망고,루카,소니,현구막", 10);
-        //when
-        ResponseEntity<ResultResponse> responseEntity = webController.play(namesAndCountRequest);
+        NamesAndCountRequest namesAndCountRequest = new NamesAndCountRequest("망고,루카", 10);
+        ResultResponse resultResponse = new ResultResponse(
+                "망고",
+                List.of(new RacingCarResponse("망고", 7),
+                        new RacingCarResponse("루카", 5)));
+        //mocking
+        Mockito.when(racingCarService.playGame(Mockito.any())).thenReturn(resultResponse);
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        mockMvc.perform(MockMvcRequestBuilders.post("/plays")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(namesAndCountRequest)))
+                .andExpect(MockMvcResultMatchers.status().isOk()) // 200
+                .andExpect(MockMvcResultMatchers.jsonPath("$.winners").value("망고"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.racingCars[0].name").value("망고"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.racingCars[0].position").value(7))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.racingCars[1].name").value("루카"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.racingCars[1].position").value(5));
     }
+
 }
--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -2,37 +2,95 @@ package racingcar.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import racingcar.dao.GameDao;
+import racingcar.dao.ParticipantDao;
+import racingcar.dao.PlayerDao;
 import racingcar.dto.NamesAndCountRequest;
 import racingcar.dto.RacingCarResponse;
 import racingcar.dto.ResultResponse;
+import racingcar.entity.GameEntity;
+import racingcar.entity.ParticipantEntity;
+import racingcar.entity.PlayerEntity;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ExtendWith(MockitoExtension.class)
 class RacingCarServiceTest {
 
-    private final RacingCarService racingCarService;
+    @Mock
+    private GameDao gameDao;
+    @Mock
+    private PlayerDao playerDao;
+    @Mock
+    private ParticipantDao participantDao;
+    @InjectMocks
+    private RacingCarService racingCarService;
 
-    @Autowired
-    RacingCarServiceTest(final RacingCarService racingCarService) {
-        this.racingCarService = racingCarService;
+    @DisplayName("모든 이력을 반환한다.")
+    @Test
+    void searchAllGame() {
+        //mocking -> DAO에서 조회되는 값 제어
+        Mockito.when(gameDao.findAll()).thenReturn(List.of(
+                new GameEntity(1L, 10),
+                new GameEntity(2L, 20)));
+
+        Mockito.when(playerDao.findById(1L)).thenReturn(Optional.of(new PlayerEntity(1L, "망고")));
+        Mockito.when(playerDao.findById(2L)).thenReturn(Optional.of(new PlayerEntity(2L, "루카")));
+        Mockito.when(playerDao.findById(3L)).thenReturn(Optional.of(new PlayerEntity(3L, "소니")));
+        Mockito.when(playerDao.findById(4L)).thenReturn(Optional.of(new PlayerEntity(4L, "현구막")));
+
+        Mockito.when(participantDao.findByGameId(1L)).thenReturn(
+                List.of(
+                        new ParticipantEntity(1L, 1L, 10, true),
+                        new ParticipantEntity(1L, 2L, 5, false)));
+        Mockito.when(participantDao.findByGameId(2L)).thenReturn(
+                List.of(
+                        new ParticipantEntity(2L, 3L, 20, true),
+                        new ParticipantEntity(2L, 4L, 15, false)));
+        //when
+        List<ResultResponse> resultResponses = racingCarService.searchAllGame();
+        ResultResponse mangoAndLuca = resultResponses.get(0);
+        ResultResponse sonyAndHyeon9mak = resultResponses.get(1);
+        //then
+        assertThat(mangoAndLuca.getWinners()).isEqualTo("망고");
+        assertThat(mangoAndLuca.getRacingCars().get(0).getName()).isEqualTo("망고");
+        assertThat(mangoAndLuca.getRacingCars().get(0).getPosition()).isEqualTo(10);
+        assertThat(mangoAndLuca.getRacingCars().get(1).getName()).isEqualTo("루카");
+        assertThat(mangoAndLuca.getRacingCars().get(1).getPosition()).isEqualTo(5);
+        assertThat(sonyAndHyeon9mak.getWinners()).isEqualTo("소니");
+        assertThat(sonyAndHyeon9mak.getRacingCars().get(0).getName()).isEqualTo("소니");
+        assertThat(sonyAndHyeon9mak.getRacingCars().get(0).getPosition()).isEqualTo(20);
+        assertThat(sonyAndHyeon9mak.getRacingCars().get(1).getName()).isEqualTo("현구막");
+        assertThat(sonyAndHyeon9mak.getRacingCars().get(1).getPosition()).isEqualTo(15);
     }
 
+    // TODO: 이 테스트에서 호출되는 서비스 안에서 도메인 객체를 만들어 실행시키는데, 그 객체는 제어할 수 없나?
     @DisplayName("이름들과 시도 횟수를 입력받아 결과를 반환한다.")
     @Test
     void playGame() {
         //given
         NamesAndCountRequest namesAndCountRequest = new NamesAndCountRequest("망고,루카,소니,현구막", 10);
+        //mocking
+        Mockito.when(gameDao.save(Mockito.anyInt())).thenReturn(1L);
+        Mockito.when(playerDao.save(Mockito.any())).thenReturn(1L, 2L, 3L, 4L);
+        Mockito.doNothing().when(participantDao).save(Mockito.any());
         //when
         ResultResponse resultResponse = racingCarService.playGame(namesAndCountRequest);
         //then
-        List<String> names = resultResponse.getRacingCars().stream().map(RacingCarResponse::getName).collect(Collectors.toList());
-        assertThat(resultResponse.getWinners()).isNotNull();
+        List<String> names = resultResponse.getRacingCars().stream()
+                .map(RacingCarResponse::getName)
+                .collect(Collectors.toList());
+        assertThat(resultResponse.getWinners()).isNotNull(); //어떻게 돌아가는지는 이미 도메인테스트에서 끝남
         assertThat(names).containsExactly("망고", "루카", "소니", "현구막");
     }
 }
+
