5717ebd933bc6cb9950149bd26e24f54a64677c0
the9kim <the9rla@gmail.com>
2023-04-18 17:03:32 +0900

    refactor: Dao테스트 어노테이션 변경(@SpringBootTest -> JdbcTest)

--- src/test/java/racingcar/dao/ResultDaoTest.java
+++ src/test/java/racingcar/dao/ResultDaoTest.java
@@ -3,19 +3,23 @@ package racingcar.dao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@JdbcTest
 class ResultDaoTest {
 
     @Autowired
-    ResultDao resultDao;
+    JdbcTemplate jdbcTemplate;
 
     @Test
     @DisplayName("결과 추가 시 id가 반환되는지 확인")
     void insert() {
+
+        ResultDao resultDao = new ResultDao(jdbcTemplate);
+
         long id = resultDao.insert(4, "roy");
 
         assertThat(id).isEqualTo(1);
