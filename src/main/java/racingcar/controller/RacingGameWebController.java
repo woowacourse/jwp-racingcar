197ad1a34fd5cbdfbd20a33273d9a58af6b8433e
sosow0212 <sosow0212@naver.com>
2023-04-18 13:43:01 +0900

    refactor: ResponseEntity 사용할 때 생성자로 반환하는 방식에서 빌더 패턴으로 변경

--- src/main/java/racingcar/controller/RacingGameWebController.java
+++ src/main/java/racingcar/controller/RacingGameWebController.java
@@ -1,19 +1,16 @@
 package racingcar.controller;
 
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import racingcar.domain.Cars;
 import racingcar.domain.TryCount;
 import racingcar.dto.GameHistoriesResponseDto;
 import racingcar.dto.GameResultResponseDto;
 import racingcar.dto.StartGameRequestDto;
 import racingcar.service.RacingCarService;
+
+import java.util.List;
 
 @RequestMapping("/plays")
 @RestController
@@ -29,7 +26,8 @@ public class RacingGameWebController {
 
     @GetMapping
     public ResponseEntity<List<GameHistoriesResponseDto>> findGameHistories() {
-        return new ResponseEntity<>(racingCarService.findAllGameHistories(), HttpStatus.OK);
+        return ResponseEntity.ok()
+                .body(racingCarService.findAllGameHistories());
     }
 
     @PostMapping
@@ -37,7 +35,8 @@ public class RacingGameWebController {
         Cars cars = makeCars(request.getNames());
         TryCount tryCount = makeTryCount(request.getCount());
 
-        return new ResponseEntity<>(racingCarService.startRace(cars, tryCount), HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(racingCarService.startRace(cars, tryCount));
     }
 
     public Cars makeCars(final String input) {
