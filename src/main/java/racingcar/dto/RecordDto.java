8235671fa1ec5cf98cc3aa1ebe255f3c7eda6913
kdkdhoho <hkim4410@naver.com>
2023-04-18 16:36:39 +0900

    refactor(RecordDto): 필요없는 애노테이션 제거

--- src/main/java/racingcar/dto/RecordDto.java
+++ src/main/java/racingcar/dto/RecordDto.java
@@ -3,11 +3,9 @@ package racingcar.dto;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @EqualsAndHashCode
-@NoArgsConstructor(force = true)
 @AllArgsConstructor
 public class RecordDto {
 
