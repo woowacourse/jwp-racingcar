b36c4a45893bb4b48a56844694c99875f0f5fe47
splitube <voicesplit@daum.net>
2023-04-19 00:58:03 +0900

    feat: MemoryGameDao 구현

---
+++ src/main/java/racing/dao/game/MemoryGameDao.java
@@ -0,0 +1,26 @@
+package racing.dao.game;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import racing.domain.Game;
+import racing.dto.GameDto;
+
+public class MemoryGameDao implements GameDao {
+
+    private static int gameId = 1;
+    private static final Map<Integer, GameDto> games = new LinkedHashMap<>();
+
+    @Override
+    public int insert(final Game game) {
+        final int currentId = gameId;
+        final GameDto gameDto = new GameDto(game.getTotalCount());
+        games.put(gameId++, gameDto);
+        return currentId;
+    }
+
+    @Override
+    public List<Integer> getAllId() {
+        return List.copyOf(games.keySet());
+    }
+}
