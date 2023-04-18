8c8ac59cde4a3d32ed2c8dc1dd0335a85810601f
splitube <voicesplit@daum.net>
2023-04-19 00:59:46 +0900

    feat: GameDto 구현

---
+++ src/main/java/racing/dto/GameDto.java
@@ -0,0 +1,14 @@
+package racing.dto;
+
+public class GameDto {
+
+    private final int count;
+
+    public GameDto(final int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
