c0d929b42b6a00bc6e4ca0f0f2c7f441c5f84776
splitube <voicesplit@daum.net>
2023-04-19 00:45:42 +0900

    feat: Configuration 설정

---
+++ src/main/java/racing/config/RacingGameConfig.java
@@ -0,0 +1,21 @@
+package racing.config;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+@Configuration
+public class RacingGameConfig {
+
+    private final DataSource dataSource;
+
+    public RacingGameConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
