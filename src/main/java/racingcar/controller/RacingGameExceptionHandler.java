0dc7201fec758b474f9939bad427425f81ba32c4
Ethan <kim76744dev@gmail.com>
2023-04-18 14:38:14 +0900

    refactor: 예외가 발생하면 오류를 출력하도록 변경

--- src/main/java/racingcar/controller/RacingGameExceptionHandler.java
+++ src/main/java/racingcar/controller/RacingGameExceptionHandler.java
@@ -1,7 +1,11 @@
 package racingcar.controller;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,10 +15,26 @@ import racingcar.exception.RuntimeExceptionImpl;
 @ControllerAdvice
 public class RacingGameExceptionHandler {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(value = {RuntimeExceptionImpl.class, DataAccessException.class})
     public ResponseEntity<Map<String, String>> handle(Exception exception) {
+        printLog(exception);
+
         final Map<String, String> exceptionResponse = new HashMap<>();
         exceptionResponse.put("exception", exception.getMessage());
         return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    private void printLog(final Exception exception) {
+        log.warn("[예외 발생]  = {}", createExceptionStackTrace(exception));
+    }
+
+    private String createExceptionStackTrace(final Exception exception) {
+        final StringWriter stringWriter = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(stringWriter);
+        exception.printStackTrace(printWriter);
+
+        return stringWriter.toString();
     }
 }
