c7fd7e9bae6c4eacf02804f9452922f7b9b6641d
java-saeng <gusdn85554@naver.com>
2023-04-18 14:34:36 +0900

    feat : CarService Unit Test

---
+++ src/test/java/racingcar/service/CarServiceTest.java
@@ -0,0 +1,42 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.dao.car.CarDao;
+import racingcar.domain.RacingCars;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@DisplayName("CarService Unit Test")
+class CarServiceTest {
+
+    private CarService carService;
+
+    private CarDao carDao;
+
+    @BeforeEach
+    void init() {
+        carDao = mock(CarDao.class);
+
+        carService = new CarService(carDao);
+    }
+
+    @Test
+    @DisplayName("registerCars() : 자동차들을 저장합니다.")
+    void test_registerCars() throws Exception {
+        //given
+        final RacingCars racingCars = RacingCars.makeCars("a,b,c");
+        final int savedId = 1;
+
+        //when
+        carService.registerCars(racingCars, savedId);
+
+        //then
+        verify(carDao, times(3)).save(any());
+    }
+}
