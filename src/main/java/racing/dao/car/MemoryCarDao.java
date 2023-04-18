1495d5763689a4182c46690c392aad6b8323aa37
splitube <voicesplit@daum.net>
2023-04-19 00:56:49 +0900

    feat: MemoryCarDao 구현

---
+++ src/main/java/racing/dao/car/MemoryCarDao.java
@@ -0,0 +1,28 @@
+package racing.dao.car;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import racing.domain.Car;
+import racing.dto.CarDto;
+
+public class MemoryCarDao implements CarDao {
+
+    private static int carId = 0;
+    private static final Map<Integer, CarDto> cars = new LinkedHashMap<>();
+
+    @Override
+    public void insert(final Car car, final int gameId) {
+        final CarDto currentCar = new CarDto(car);
+        currentCar.setGameId(gameId);
+        cars.put(carId++, currentCar);
+    }
+
+    @Override
+    public List<CarDto> findByGameId(final Integer gameId) {
+        return cars.values().stream()
+            .filter((carDto -> carDto.getGameId() == gameId))
+            .collect(Collectors.toUnmodifiableList());
+    }
+}
