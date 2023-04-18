39e5cd169cbd2afea73755bdf5ce6e51110b314c
sosow0212 <sosow0212@naver.com>
2023-04-18 19:00:30 +0900

    fix: 테스트 개별 실행시에는 성공하지만, 전체 실행시 실패하는 문제 해결 (profile 설정)

--- src/test/java/racingcar/service/RacingCarServiceIntegrationTest.java
+++ src/test/java/racingcar/service/RacingCarServiceIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import racingcar.dao.GameResultDao;
 import racingcar.domain.Car;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class RacingCarServiceIntegrationTest {
 
