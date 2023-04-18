640c5e53efcf460c34237b173337de95800493bc
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-04-18 20:44:05 +0900

    refactor: 사용하지 않는 콘솔 출력 메서드 삭제, 레이싱 결과 출력 메서드 리팩터링

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,48 +2,15 @@ package racingcar.view;
 
 import racingcar.controller.dto.CarDto;
 import racingcar.controller.dto.RacingGameResponse;
-import racingcar.domain.Name;
-import racingcar.domain.Position;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final String RESULT_MESSAGE = "\n실행 결과";
-    private static final String WINNER_MESSAGE = "%s가 최종 우승했습니다.";
-
-    public void printNotice() {
-        System.out.println("실행 결과");
-    }
-
     public void printRacingResult(final RacingGameResponse response) {
         final List<CarDto> cars = response.getRacingCars();
-        cars.forEach(this::printStatusBy);
+
+        cars.forEach(car ->
+                System.out.printf("%s : %s%n", car.getName(), "-" .repeat(car.getPosition())));
         System.out.printf("%n%n %s가 최종 우승했습니다.%n", response.getWinners());
-    }
-
-    private void printStatusBy(final CarDto car) {
-        System.out.printf("%s : %s%n", car.getName(), "-".repeat(car.getPosition()));
-    }
-
-    public void printRacingResult(Map<Name, Position> history) {
-        for (Name name : history.keySet()) {
-            Position position = history.get(name);
-            System.out.println(name.getValue() + " : " + "-".repeat(position.getValue()));
-        }
-        System.out.println();
-    }
-
-    public void printWinner(List<Name> winners) {
-        String winnerNames = findWinnerName(winners);
-        System.out.printf(WINNER_MESSAGE, winnerNames.substring(1, winnerNames.length()-1));
-    }
-
-    private static String findWinnerName(List<Name> winners) {
-        return winners.stream()
-                .map(Name::getValue)
-                .collect(Collectors.toList())
-                .toString();
     }
 }
