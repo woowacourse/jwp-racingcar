ba89a5c7f74fa5edafbf2f53aca956397c5746b6
splitube <voicesplit@daum.net>
2023-04-19 00:43:27 +0900

    remove: 리펙토링을 위해 전체 삭제

--- src/main/java/RacingCarApplication.java
+++ src/main/java/RacingCarApplication.java
@@ -1,5 +1,3 @@
-package racingcar;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/RacingCarApplicationTests.java
+++ src/test/java/RacingCarApplicationTests.java
@@ -1,5 +1,3 @@
-package racingcar;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
