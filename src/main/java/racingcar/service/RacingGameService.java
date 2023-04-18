321624c8c39911253c5014e99216f2c7fd51d93b
hanueleee <ezzeezz@naver.com>
2023-04-18 15:05:29 +0900

    feat(Service): 플레이 이력을 조회하는 기능 구현

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
@@ -21,24 +22,44 @@ public class RacingGameService {
     }
 
     public List<RacingCarResultDto> run(List<String> carNames, int count) {
-        RacingGame racingGame = initializeGame(carNames);
+        List<RacingCar> racingCars = carNames.stream().map(RacingCar::new).collect(Collectors.toUnmodifiableList());
+        RacingGame racingGame = initializeGame(racingCars);
 
         for (int i = 0; i < count; i++) {
             racingGame.runRound();
         }
         long gameId = gameDao.save(count);
-        List<RacingCarResultDto> results = racingGame.calculateResult()
-                .entrySet()
-                .stream()
-                .map((e) -> RacingCarResultDto.of(e.getKey(), e.getValue().getValue(), gameId))
-                .collect(Collectors.toUnmodifiableList());
+        List<RacingCarResultDto> results = calculateResults(racingGame, gameId);
         carDao.saveAll(results);
 
         return results;
     }
 
-    private RacingGame initializeGame(List<String> carNames) {
-        List<RacingCar> racingCars = carNames.stream().map(RacingCar::new).collect(Collectors.toUnmodifiableList());
-        return new RacingGame(racingCars, new RandomNumberGenerator());
+    public List<List<RacingCarResultDto>> history() {
+        List<List<RacingCarResultDto>> history = new ArrayList<>();
+        List<Long> ids = gameDao.findAllId();
+        for (Long id : ids) {
+            List<RacingCar> racingCars = carDao.findCarsById(id)
+                    .stream()
+                    .map(it -> new RacingCar(it.getName(), it.getPosition()))
+                    .collect(Collectors.toList());
+            RacingGame racingGame = initializeGame(racingCars);
+            List<RacingCarResultDto> results = calculateResults(racingGame, id);
+            history.add(results);
+        }
+        return history;
+    }
+
+    private RacingGame initializeGame(List<RacingCar> cars) {
+        return new RacingGame(cars, new RandomNumberGenerator());
+    }
+
+    private static List<RacingCarResultDto> calculateResults(RacingGame racingGame, long gameId) {
+        List<RacingCarResultDto> results = racingGame.calculateResult()
+                .entrySet()
+                .stream()
+                .map((e) -> RacingCarResultDto.of(e.getKey(), e.getValue().getValue(), gameId))
+                .collect(Collectors.toUnmodifiableList());
+        return results;
     }
 }
