585304b033c483408242599a5a00de21708e3456
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-19 00:34:53 +0900

    test: batch, search 테스트 코드 추가

--- src/test/java/racingcar/dao/RacingWebDaoTest.java
+++ src/test/java/racingcar/dao/RacingWebDaoTest.java
@@ -1,13 +1,14 @@
 package racingcar.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import racingcar.dao.dto.CarDto;
 import racingcar.dao.dto.TrackDto;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingWebDaoTest {
@@ -32,5 +33,33 @@ class RacingWebDaoTest {
         final CarDto carDto = new CarDto("pobi", 5, true, savedId);
 
         assertDoesNotThrow(() -> racingWebDao.saveCar(carDto));
+    }
+
+    @Test
+    void createCarsWithBatch() {
+        final TrackDto trackDto = new TrackDto(5);
+        final Integer savedId = racingWebDao.saveTrack(trackDto);
+
+        final CarDto carDto1 = new CarDto("gray", 5, true, savedId);
+        final CarDto carDto2 = new CarDto("logun", 3, false, savedId);
+        final CarDto carDto3 = new CarDto("hoy", 2, false, savedId);
+        List<CarDto> carDtos = List.of(carDto1, carDto2, carDto3);
+
+        assertDoesNotThrow(() -> racingWebDao.saveWithBatch(carDtos));
+    }
+
+    @Test
+    void findAllCarsWithTrackId() {
+        final TrackDto trackDto = new TrackDto(5);
+        final Integer savedId = racingWebDao.saveTrack(trackDto);
+        final CarDto carDto1 = new CarDto("gray", 5, true, savedId);
+        final CarDto carDto2 = new CarDto("logun", 3, false, savedId);
+        final CarDto carDto3 = new CarDto("hoy", 2, false, savedId);
+        List<CarDto> carDtos = List.of(carDto1, carDto2, carDto3);
+        racingWebDao.saveWithBatch(carDtos);
+
+        List<CarDto> carsByTrackId = racingWebDao.findAllCarsByTrackId(savedId);
+
+        assertThat(carsByTrackId).hasSameSizeAs(carDtos);
     }
 }
