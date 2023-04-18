7b0071a4289d3e1a3d63dc5520a98a4f6514b641
mcodnjs <codnjs8989@gmail.com>
2023-04-19 01:29:25 +0900

    test: DB에 저장된 게임 이력 가져오는 테스트 구현

--- src/test/java/racingcar/repository/RacingCarRepositoryTest.java
+++ src/test/java/racingcar/repository/RacingCarRepositoryTest.java
@@ -1,8 +1,6 @@
 package racingcar.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +9,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import racingcar.domain.Car;
 import racingcar.dto.RacingCarDto;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 class RacingCarRepositoryTest {
@@ -23,6 +26,12 @@ class RacingCarRepositoryTest {
     @BeforeEach
     void setUp() {
         racingCarRepository = new RacingCarRepository(jdbcTemplate);
+    }
+
+    @Test
+    @DisplayName("게임이 정상적으로 저장되어야 한다.")
+    void saveGame_success() {
+        Assertions.assertDoesNotThrow(() -> racingCarRepository.saveGame(10));
     }
 
     @Test
@@ -56,5 +65,55 @@ class RacingCarRepositoryTest {
                 .hasSize(2);
         assertThat(findCars.get(0).getName())
                 .containsAnyOf("glen", "raon");
+    }
+
+    @Test
+    @DisplayName("게임에 저장된 우승자들의 모든 결과를 출력해야 한다.")
+    void findWinners_success() {
+        // given
+        List<String> winnersTest1 = List.of("glen");
+        List<String> winnersTest2 = List.of("raon");
+
+        int gameId1 = racingCarRepository.saveGame(1);
+        int gameId2 = racingCarRepository.saveGame(1);
+
+        racingCarRepository.saveWinners(gameId1, winnersTest1);
+        racingCarRepository.saveWinners(gameId2, winnersTest2);
+
+        // expect
+        Map<Integer, List<String>> playHistory = racingCarRepository.findWinners();
+
+        assertThat(playHistory).hasSize(2);
+        assertThat(playHistory.get(gameId1)).hasSize(winnersTest1.size());
+        assertThat(playHistory.get(gameId2)).hasSize(winnersTest2.size());
+        assertThat(playHistory.get(gameId1).get(0)).contains("glen");
+        assertThat(playHistory.get(gameId2).get(0)).contains("raon");
+    }
+
+    @Test
+    @DisplayName("게임에 저장된 자동차들의 모든 결과를 출력해야 한다.")
+    void findRacingCars_success() {
+        // given
+        Car car1 = new Car("glen");
+        Car car2 = new Car("raon");
+        Car car3 = new Car("test");
+
+        List<Car> carsTest1 = List.of(car1, car2);
+        List<Car> carsTest2 = List.of(car1, car2, car3);
+
+        int gameId1 = racingCarRepository.saveGame(1);
+        int gameId2 = racingCarRepository.saveGame(1);
+
+        racingCarRepository.saveCars(gameId1, carsTest1);
+        racingCarRepository.saveCars(gameId2, carsTest2);
+
+        // expect
+        Map<Integer, List<RacingCarDto>> playHistory = racingCarRepository.findRacingCars();
+
+        assertThat(playHistory).hasSize(2);
+        assertThat(playHistory.get(gameId1)).hasSize(carsTest1.size());
+        assertThat(playHistory.get(gameId2)).hasSize(carsTest2.size());
+        assertThat(playHistory.get(gameId1).get(0).getName()).containsAnyOf("glen", "raon");
+        assertThat(playHistory.get(gameId2).get(0).getName()).containsAnyOf("glen", "raon", "test");
     }
 }
--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -1,15 +1,20 @@
 package racingcar.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 
 import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import racingcar.dto.RacingCarDto;
+import racingcar.dto.RacingResultResponse;
 import racingcar.repository.RacingCarRepository;
 import racingcar.utils.NumberGenerator;
 
@@ -32,7 +37,6 @@ class RacingCarServiceTest {
         List<String> carNames = List.of("glen", "raon");
         int tryCount = 10;
 
-        // when
         doReturn(1)
                 .when(numberGenerator)
                 .generateNumber();
@@ -41,9 +45,37 @@ class RacingCarServiceTest {
                 .when(racingCarRepository)
                 .saveGame(10);
 
-        // then
+        // expect
         int gameId = racingCarService.playRacingGame(carNames, tryCount);
         assertThat(gameId)
                 .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("레이싱 게임의 플레이 이력을 조회하면 RacingResultResponse의 List를 반환한다.")
+    void searchGameHistory_success() {
+        // given
+        RacingCarDto racingCarDto1 = new RacingCarDto("glen", 1);
+        RacingCarDto racingCarDto2 = new RacingCarDto("raon", 1);
+
+        doReturn(Map.of(1, List.of(racingCarDto1, racingCarDto2)))
+                .when(racingCarRepository)
+                .findRacingCars();
+
+        doReturn(Map.of(1, List.of("glen", "raon")))
+                .when(racingCarRepository)
+                .findWinners();
+
+        // expect
+        List<RacingResultResponse> racingResultResponses = racingCarService.searchGameHistory();
+
+        assertThat(racingResultResponses)
+                .hasSize(1);
+        assertThat(racingResultResponses.get(0).getRacingCars())
+                .hasSize(2)
+                .contains(racingCarDto1, racingCarDto2);
+        assertThat(racingResultResponses.get(0).getWinners())
+                .hasSize(2)
+                .contains("glen", "raon");
     }
 }
