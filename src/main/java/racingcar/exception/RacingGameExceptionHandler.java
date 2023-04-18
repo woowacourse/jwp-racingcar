c2de3f3f4d735a77fe85b4843f88bc592cef1041
ingpyo <juninpyo@naver.com>
2023-04-18 17:11:55 +0900

    feat: 예외헨들러 생성

---
+++ src/main/java/racingcar/exception/RacingGameExceptionHandler.java
@@ -0,0 +1,22 @@
+package racingcar.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RacingGameExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("서버가 응답할 수 없습니다.");
+    }
+}
