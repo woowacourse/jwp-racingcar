7dd3089b648458c4dfe9f0bd5f167f860042d60f
JunDonghyuk <ehdgur4814@naver.com>
2023-04-19 04:30:02 +0900

    refactor: NamedParameterJdbcTemplate을 사용

--- src/main/java/racingcar/dao/CarDao.java
+++ src/main/java/racingcar/dao/CarDao.java
@@ -1,23 +1,26 @@
 package racingcar.dao;
 
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
 public class CarDao {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate template;
     private final SimpleJdbcInsert insertActor;
 
-    public CarDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.insertActor = new SimpleJdbcInsert(jdbcTemplate)
+    public CarDao(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.insertActor = new SimpleJdbcInsert(dataSource)
                 .withTableName("car")
                 .usingGeneratedKeyColumns("car_id");
     }
@@ -28,13 +31,20 @@ public class CarDao {
     }
 
     public List<CarEntity> findByGameId(int gameId) {
-        String sql = "select name, position from car where game_id = ?";
-        List<CarEntity> cars = jdbcTemplate.query(sql, (rs, rowNum) -> {
-            CarEntity car = new CarEntity();
-            car.setName(rs.getString("name"));
-            car.setPosition(rs.getInt("position"));
-            return car;
-        }, gameId);
-        return cars;
+        String sql = "select name, position from car where game_id = :gameId";
+
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("gameId", gameId);
+        return template.query(sql, parameterSource, rowMapper(gameId));
+    }
+
+    public RowMapper<CarEntity> rowMapper(int gameId) {
+        return ((rs, rowNum) -> {
+            int carId = rs.getInt("car_id");
+            String name = rs.getString("name");
+            int position = rs.getInt("position");
+            CarEntity carEntity = new CarEntity(carId, name, position, gameId);
+            return carEntity;
+        });
     }
 }
