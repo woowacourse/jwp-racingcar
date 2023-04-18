f21dd49907b1819218427780f28493366ce62380
splitube <voicesplit@daum.net>
2023-04-19 01:01:08 +0900

    feat: ConsoleRacingGameService 구현

---
+++ src/main/java/racing/service/ConsoleRacingGameService.java
@@ -0,0 +1,44 @@
+package racing.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racing.dao.car.CarDao;
+import racing.dao.game.GameDao;
+import racing.domain.Game;
+import racing.dto.CarDto;
+import racing.dto.GameInputDto;
+import racing.dto.GameResultDto;
+import racing.util.NumberGenerator;
+
+public class ConsoleRacingGameService implements RacingGameService {
+
+    private final CarDao carDao;
+    private final GameDao gameDao;
+
+    public ConsoleRacingGameService(final CarDao carDao, final GameDao gameDao) {
+        this.carDao = carDao;
+        this.gameDao = gameDao;
+    }
+
+    @Override
+    public GameResultDto playGame(final GameInputDto gameInputDto, final NumberGenerator numberGenerator) {
+        final Game game = new Game(gameInputDto.getCount());
+        final int gameId = gameDao.insert(game);
+        game.joinCars(gameInputDto.getNames());
+        game.playGame(numberGenerator);
+        game.getCars()
+            .forEach(car -> carDao.insert(car, gameId));
+        return new GameResultDto(game);
+    }
+
+    @Override
+    public List<GameResultDto> showGames() {
+        final List<Integer> allId = gameDao.getAllId();
+        return allId.stream()
+            .map(gameId -> {
+                final List<CarDto> carsInGame = carDao.findByGameId(gameId);
+                return new GameResultDto(carsInGame);
+            })
+            .collect(Collectors.toUnmodifiableList());
+    }
+}
