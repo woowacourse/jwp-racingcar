dd3d6b1e5a9b0a8db090b6d0759f57b995b2523f
ingpyo <juninpyo@naver.com>
2023-04-18 14:10:42 +0900

    refactor: id select기능 삭제

--- src/main/java/racingcar/dao/GameDao.java
+++ src/main/java/racingcar/dao/GameDao.java
@@ -2,10 +2,6 @@ package racingcar.dao;
 
 import racingcar.entity.Game;
 
-import java.util.List;
-
 public interface GameDao {
     int saveAndGetId(final Game game);
-
-    List<Game> findAll();
 }
--- src/main/java/racingcar/dao/GameJdbcDao.java
+++ src/main/java/racingcar/dao/GameJdbcDao.java
@@ -1,13 +1,11 @@
 package racingcar.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import racingcar.entity.Game;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -22,12 +20,6 @@ public class GameJdbcDao implements GameDao {
                 .usingGeneratedKeyColumns("id");
     }
 
-    private final RowMapper<Game> actorRowMapper = (resultSet, rowNum) -> new Game(
-            resultSet.getInt("game_id"),
-            resultSet.getInt("trial_count"),
-            resultSet.getTimestamp("created_at").toLocalDateTime()
-    );
-
     public int saveAndGetId(final Game game) {
         final Map<String, Object> parameters = new HashMap<>();
 
@@ -35,12 +27,5 @@ public class GameJdbcDao implements GameDao {
         parameters.put("created_at", game.getCreatedAt());
 
         return (int) insertActor.executeAndReturnKey(parameters).longValue();
-    }
-
-    @Override
-    public List<Game> findAll() {
-        final String sql = "SELECT game_id, created_at, trial_count FROM GAME";
-
-        return jdbcTemplate.query(sql, actorRowMapper);
     }
 }
