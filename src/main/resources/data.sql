87fed6b8d71d92dc95af8f3f824f473ca34a9a2a
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-04-18 17:18:07 +0900

    refactor: 테이블명 컬럼명 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
 -- TODO: 기능 구현에 필요한 내용을 추가하거나 수정하세요.
-CREATE TABLE IF NOT EXISTS cars (
-    car_id bigint NOT NULL AUTO_INCREMENT,
-    car_name VARCHAR(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS car (
+    id bigint NOT NULL AUTO_INCREMENT,
+    name VARCHAR(8) NOT NULL,
     step int(3) NOT NULL,
-    winner boolean default false,
+    is_winner boolean default false,
     game_id bigint not null,
-    PRIMARY KEY (car_id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS games (
-    game_id bigint NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS game (
+    id bigint NOT NULL AUTO_INCREMENT,
     count int(3) NOT NULL,
     create_time timestamp NOT NULL,
-    PRIMARY KEY (game_id)
+    PRIMARY KEY (id)
 );
