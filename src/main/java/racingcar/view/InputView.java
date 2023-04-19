89da043e64c4149ffd677059009bf28e1efc6241
dooboocookie <bkh3030@gmail.com>
2023-04-19 21:43:17 +0900

    feat(ConsoleController): ConsoleController 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,21 +1,10 @@
 package racingcar.view;
 
-import racingcar.validator.CarValidator;
-import racingcar.validator.DefaultCarValidator;
-import racingcar.validator.DefaultGameRoundValidator;
-import racingcar.validator.GameRoundValidator;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static racingcar.exception.ErrorMessages.INPUT_EXCEPTION;
 
 public class InputView {
-    private static final String SEPARATOR_REGEX = "\\s*,\\s";
     private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String GAME_ROUND_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
@@ -23,23 +12,13 @@ public class InputView {
 
     public String inputCarName() {
         System.out.println(CAR_NAMES_INPUT_MESSAGE);
-        String input = readLine();
 
-        List<String> carNames = stringToListConverter(input);
-
-        CarValidator carValidator = new DefaultCarValidator();
-        carValidator.validateCarNames(carNames);
-
-        return input;
+        return readLine();
     }
 
     public int inputGameRound() {
         System.out.println(GAME_ROUND_INPUT_MESSAGE);
         String inputGameRound = readLine();
-
-        GameRoundValidator gameRoundValidator = new DefaultGameRoundValidator();
-        gameRoundValidator.validateGameRoundInput(inputGameRound);
-
         return stringToIntConverter(inputGameRound);
     }
 
@@ -48,17 +27,12 @@ public class InputView {
         try {
             input = bufferedReader.readLine();
         } catch (IOException e) {
-            throw new RuntimeException(INPUT_EXCEPTION.getMessage());
+            throw new IllegalArgumentException("다시 입력해주세요.");
         }
         return input;
     }
 
     public int stringToIntConverter(String input) {
         return Integer.parseInt(input);
-    }
-
-    public List<String> stringToListConverter(String input) {
-        String[] splitInput = input.split(SEPARATOR_REGEX);
-        return new ArrayList<>(Arrays.asList(splitInput));
     }
 }
