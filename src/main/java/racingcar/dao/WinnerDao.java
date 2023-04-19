adb95192fae6f10ce7d74e680e72c78bb64364d7
zillionme <beatus96@naver.com>
2023-04-19 17:18:16 +0900

    refactor: winner dao, entity 생성

---
+++ src/main/java/racingcar/dao/WinnerDao.java
@@ -0,0 +1,29 @@
+package racingcar.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import racingcar.dao.entity.WinnerEntity;
+
+import java.util.List;
+
+@Repository
+public class WinnerDao {
+
+    private final SimpleJdbcInsert insertActor;
+    private final JdbcTemplate jdbcTemplate;
+
+    public WinnerDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.insertActor = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("winner");
+    }
+
+    public int[] saveAll(List<WinnerEntity> winners) {
+        SqlParameterSource[] sqlParameterSource = SqlParameterSourceUtils.createBatch(winners);
+        return insertActor.executeBatch(sqlParameterSource);
+    }
+}
+
