87f69cd4f2195af3672fe21c4f29557ac2294853
echo <echo.backend@gmail.com>
2023-04-19 09:18:49 +0900

    test: CarPositionTest 추가

---
+++ src/test/java/racingcar/car/model/CarPositionTest.java
@@ -0,0 +1,26 @@
+package racingcar.car.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarPositionTest {
+    
+    @Test
+    @DisplayName("자동차 위치 생성 테스트")
+    void create() {
+        final int position = 0;
+        final Position carPosition = CarPosition.create(position);
+        assertEquals(position, carPosition.getValue());
+    }
+    
+    @Test
+    @DisplayName("자동차 위치가 0보다 작을 경우 예외 발생 테스트")
+    void createException() {
+        final int position = -1;
+        assertThrows(IllegalArgumentException.class, () -> CarPosition.create(position));
+    }
+    
+}
