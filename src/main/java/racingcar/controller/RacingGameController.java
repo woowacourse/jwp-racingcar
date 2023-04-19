7545f2aacf01f524e4e3812a796a9d4ad6ce1de0
shin-mallang <huipulci1@naver.com>
2023-04-19 11:18:17 +0900

    feat: 컨트롤러에서 전체 게임 조회 기능 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import racingcar.application.RacingGameService;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static racingcar.application.RacingGameService.CarDto;
 import static racingcar.application.RacingGameService.GameResult;
@@ -28,6 +30,13 @@ public class RacingGameController {
         final Long id = racingGameService.play(playGameRequest.getNames(), playGameRequest.getCount());
         final GameResult result = racingGameService.findResultById(id);
         return ResponseEntity.ok(PlayGameResponse.from(result));
+    }
+
+    @GetMapping("/plays")
+    ResponseEntity<List<PlayGameResponse>> findAll() {
+        return ResponseEntity.ok(racingGameService.findAll().stream()
+                .map(PlayGameResponse::from)
+                .collect(Collectors.toList()));
     }
 
     public static class PlayGameRequest {
--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import racingcar.application.RacingGameService;
+import racingcar.application.RacingGameService.GameResult;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.Winners;
@@ -23,6 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -50,7 +52,7 @@ class RacingGameControllerTest {
                 new Car("브라운", 10))
         );
         given(service.findResultById(1L))
-                .willReturn(new RacingGameService.GameResult(cars.getCars(), new Winners(cars)));
+                .willReturn(new GameResult(cars.getCars(), new Winners(cars)));
 
         final MvcResult mvcResult = mockMvc.perform(post("/plays").content(
                                 "{\"names\": \"브리,토미,브라운\", \"count\": 10}")
@@ -79,6 +81,75 @@ class RacingGameControllerTest {
                 "        }\n" +
                 "    ]\n" +
                 "}";
+        final String s = StringUtils.replaceWhitespaceCharacters(ex, "");
+        assertThat(contentAsString)
+                .isEqualTo(s);
+    }
+
+    @Test
+    void 전체_게임_목록을_반환한다() throws Exception {
+        // given
+        final Cars cars1 = new Cars(List.of(
+                new Car("브리", 6),
+                new Car("토미", 4),
+                new Car("브라운", 3)
+        ));
+        final Cars cars2 = new Cars(List.of(
+                new Car("브리", 6),
+                new Car("토미", 6),
+                new Car("브라운", 6)
+        ));
+        given(service.findAll())
+                .willReturn(List.of(
+                        new GameResult(cars1.getCars(), new Winners(cars1)),
+                        new GameResult(cars2.getCars(), new Winners(cars2))
+                ));
+
+        // when
+        final MvcResult mvcResult = mockMvc.perform(get("/plays"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        final String contentAsString = mvcResult.getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        final String ex = "[\n" +
+                "    {\n" +
+                "        \"winners\": \"브리\",\n" +
+                "        \"racingCars\": [\n" +
+                "            {\n" +
+                "                \"name\": \"브리\",\n" +
+                "                \"position\": 6\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"토미\",\n" +
+                "                \"position\": 4\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"브라운\",\n" +
+                "                \"position\": 3\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"winners\": \"브리,토미,브라운\",\n" +
+                "        \"racingCars\": [\n" +
+                "            {\n" +
+                "                \"name\": \"브리\",\n" +
+                "                \"position\": 6\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"토미\",\n" +
+                "                \"position\": 6\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"name\": \"브라운\",\n" +
+                "                \"position\": 6\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }\n" +
+                "]";
         final String s = StringUtils.replaceWhitespaceCharacters(ex, "");
         assertThat(contentAsString)
                 .isEqualTo(s);
