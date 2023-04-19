08aec4762721ef792a181f93b466fdf3b6d37790
zillionme <beatus96@naver.com>
2023-04-19 17:05:42 +0900

    refactor: winner 테이블 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,38 @@
-create TABLE GAME (
+--create TABLE GAME (
+--    game_id     INT         NOT NULL AUTO_INCREMENT,
+--    winners     VARCHAR(50) NOT NULL,
+--    try_count   INT         NOT NULL,
+--    created_at  DATETIME    NOT NULL,
+--    PRIMARY KEY (game_id)
+--);
+--
+--create TABLE CAR (
+--    car_id      INT          NOT NULL AUTO_INCREMENT,
+--    name        VARCHAR(5)   NOT NULL,
+--    position    INT          NOT NULL DEFAULT 0,
+--    game_id     INT          NOT NULL,
+--    PRIMARY KEY (car_id),
+--    FOREIGN KEY(game_id) REFERENCES GAME (game_id)
+--);
+
+CREATE TABLE GAME (
     game_id     INT         NOT NULL AUTO_INCREMENT,
-    winners     VARCHAR(50) NOT NULL,
     try_count   INT         NOT NULL,
-    created_at  DATETIME    NOT NULL default current_timestamp,
+    created_at  DATETIME    NOT NULL,
     PRIMARY KEY (game_id)
 );
 
-create TABLE CAR (
+CREATE TABLE CAR (
     car_id      INT          NOT NULL AUTO_INCREMENT,
     name        VARCHAR(5)   NOT NULL,
-    position    INT          NOT NULL DEFAULT 0,
+    position    INT          NOT NULL,
     game_id     INT          NOT NULL,
     PRIMARY KEY (car_id),
     FOREIGN KEY(game_id) REFERENCES GAME (game_id)
 );
+
+CREATE TABLE WINNER (
+    winner_id   INT     NOT NULL AUTO_INCREMENT,
+    car_id      INT     NOT NULL,
+    game_id     INT     NOT NULL
+)
