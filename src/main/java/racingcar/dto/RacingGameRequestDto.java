6953208d09a06259765a20ff94bbef1b30372a90
seokhwan-an <asok369258@naver.com>
2023-04-19 15:18:27 +0900

    fix : Integer 타입에 잘못된 valid 에노테이션 수정

--- src/main/java/racingcar/dto/RacingGameRequestDto.java
+++ src/main/java/racingcar/dto/RacingGameRequestDto.java
@@ -1,14 +1,16 @@
 package racingcar.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class RacingGameRequestDto {
 
     @NotBlank(message = "플레이어의 이름은 빈 값이 될 수 없습니다.")
     private final String names;
 
-    @NotBlank(message = "시도횟수는 빈 값이 될 수 없습니다.")
-    private final int count;
+    @Min(value = 1, message = "시도횟수는 빈 값이 될 수 없습니다.")
+    private final Integer count;
 
     public RacingGameRequestDto(String names, int count) {
         this.names = names;
