2d3a08dabfbea37449977a3ea42f1926c7703025
donghae <donghae1999@gmail.com>
2023-04-19 15:39:32 +0900

    refactor: WebController @RestController 수정

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import racingcar.dto.GameRecordResponseDto;
 import racingcar.dto.RacingGameRequestDto;
 import racingcar.dto.RacingGameResponseDto;
@@ -14,7 +15,7 @@ import racingcar.service.GameService;
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@RestController
 public class WebController {
 
     private final GameService gameService;
