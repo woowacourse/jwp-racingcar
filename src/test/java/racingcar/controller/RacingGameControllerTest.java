7661677dc3f7839e3e8519264026686a107f32e0
shin-mallang <huipulci1@naver.com>
2023-04-19 11:13:26 +0900

    refactor: 컨트롤러 테스트 리팩토링

--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -1,24 +1,27 @@
 package racingcar.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import racingcar.domain.numbergenerator.NumberGenerator;
+import racingcar.application.RacingGameService;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Winners;
 
-import javax.annotation.Priority;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,33 +29,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@SpringBootTest
-@Import(value = RacingGameControllerTest.MockNumberGenerator.class)
+@WebMvcTest(RacingGameController.class)
 @DisplayName("RacingGameControllerTest 은")
 class RacingGameControllerTest {
 
+    @MockBean
+    private RacingGameService service;
+
     @Autowired
-    RacingGameController racingGameController;
-
-    @Priority(1)
-    @TestComponent
-    public static class MockNumberGenerator implements NumberGenerator {
-
-        @Override
-        public int generateNumber() {
-            return 9;
-        }
-    }
-
     private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(racingGameController).build();
-    }
 
     @Test
     void 게임_진행_요청을_처리하고_결과를_반환한다() throws Exception {
+        // given
+        given(service.play(any(), eq(10)))
+                .willReturn(1L);
+        final Cars cars = new Cars(List.of(
+                new Car("브리", 10),
+                new Car("토미", 10),
+                new Car("브라운", 10))
+        );
+        given(service.findResultById(1L))
+                .willReturn(new RacingGameService.GameResult(cars.getCars(), new Winners(cars)));
+
         final MvcResult mvcResult = mockMvc.perform(post("/plays").content(
                                 "{\"names\": \"브리,토미,브라운\", \"count\": 10}")
                         .contentType(APPLICATION_JSON))
