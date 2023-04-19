40fd44302142218b210c6e27e7af0cfdc11d98da
thdwoqor <celebarte01@gmail.com>
2023-04-19 10:32:21 +0900

    refactor: 사용하지 않는 주석 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,20 +14,3 @@ CREATE TABLE IF NOT EXISTS record
     PRIMARY KEY (player_name, game_id),
     FOREIGN KEY (game_id) REFERENCES game (id)
 );
---
--- CREATE TABLE game
--- (
---     id          int PRIMARY KEY AUTO_INCREMENT      NOT NULL,
---     trial_count int                                 NOT NULL,
---     game_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
--- );
---
--- CREATE TABLE record
--- (
---     player_name         varchar(30) NOT NULL,
---     game_id   int         NOT NULL,
---     position  int         NOT NULL,
---     is_winner boolean     NOT NULL,
---     PRIMARY KEY (player_name, game_id),
---     FOREIGN KEY (game_id) REFERENCES game (id)
--- );
