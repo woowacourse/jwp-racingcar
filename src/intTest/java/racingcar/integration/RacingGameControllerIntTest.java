ce6cb8ba48bde2674499901eb256c1cbdd0cfb8d
shin-mallang <huipulci1@naver.com>
2023-04-19 17:42:57 +0900

    test: 인수테스트 성능 최적화

--- src/intTest/java/racingcar/integration/RacingGameControllerIntTest.java
+++ src/intTest/java/racingcar/integration/RacingGameControllerIntTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,8 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("/truncate.sql")
 public class RacingGameControllerIntTest {
 
     @LocalServerPort
