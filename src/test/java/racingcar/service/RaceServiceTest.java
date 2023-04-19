eda19ca982e1d80b1d538d28c2fb223fe897b6c3
sh111-coder <ohk9134@naver.com>
2023-04-19 15:02:00 +0900

    test(RaceServiceTest): @Transactional 추가

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.controller.dto.GameInfoRequest;
 import racingcar.controller.dto.RaceResultResponse;
 
+@Transactional
 @SpringBootTest
 class RaceServiceTest {
 
