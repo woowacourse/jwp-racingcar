ff4645b20cc8fe36a523d4deb5c818d0ef585a6b
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-20 08:46:56 +0900

    feat: 아이디에 따른 레이싱카 정보 가져오 기능 추가

--- src/main/java/racingcar/dao/RacingCarDao.java
+++ src/main/java/racingcar/dao/RacingCarDao.java
@@ -3,6 +3,9 @@ package racingcar.dao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.Car;
+import racingcar.domain.RacingCarDto;
+
+import java.util.List;
 
 @Repository
 public class RacingCarDao {
@@ -16,5 +19,16 @@ public class RacingCarDao {
     public void insert(Car car, long resultId) {
         String sql = "insert into racing_cars (name, position, result_id) values (?, ?, ?)";
         jdbcTemplate.update(sql, car.getName(), car.getLocation(), resultId);
+    }
+
+    public List<RacingCarDto> findBy(long resultId) {
+        String sql = "select name, position from racing_cars where result_id = ?";
+        return jdbcTemplate.query(sql,
+                (rs, rowNum) -> {
+                    String name = rs.getString("name");
+                    int position = rs.getInt("position");
+                    RacingCarDto racingCarDto = new RacingCarDto(name, position);
+                    return racingCarDto;
+                }, resultId);
     }
 }
---
+++ src/test/java/racingcar/dao/RacingCarDaoTest.java
@@ -0,0 +1,48 @@
+package racingcar.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import racingcar.domain.Car;
+import racingcar.domain.Name;
+import racingcar.domain.RacingCarDto;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@JdbcTest
+class RacingCarDaoTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private RacingCarDao racingCarDao;
+
+    @BeforeEach
+    void init() {
+        ResultDao resultDao = new ResultDao(jdbcTemplate);
+        resultDao.insert(5, "test1");
+        resultDao.insert(7, "test6");
+
+        racingCarDao = new RacingCarDao(jdbcTemplate);
+
+        racingCarDao.insert(new Car(new Name("test1")), 1L);
+        racingCarDao.insert(new Car(new Name("test2")), 1L);
+        racingCarDao.insert(new Car(new Name("test3")), 1L);
+        racingCarDao.insert(new Car(new Name("test4")), 1L);
+        racingCarDao.insert(new Car(new Name("test5")), 2L);
+        racingCarDao.insert(new Car(new Name("test6")), 2L);
+    }
+
+    @Test
+    @DisplayName("특정 result_id의 데이터를 모두 제대로 반환하는지 확인")
+    void findBy() {
+        List<RacingCarDto> racingCars = racingCarDao.findBy(1L);
+
+        assertThat(racingCars).hasSize(4);
+    }
+}
