dfb62de785f882a2001ea5bafee4819c60930443
BGuga <tmdwns9913@gmail.com>
2023-04-20 00:36:27 +0900

    refactor: PlayerResultDto SimpleJdbcInsert 적용

--- src/main/java/racingcar/dao/PlayerResultDAO.java
+++ src/main/java/racingcar/dao/PlayerResultDAO.java
@@ -2,34 +2,38 @@ package racingcar.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.Car;
 import racingcar.entity.PlayerResultEntity;
 
-import java.sql.PreparedStatement;
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Repository
 public class PlayerResultDAO {
 
+    private final SimpleJdbcInsert simpleJdbcInsert;
     private final JdbcTemplate jdbcTemplate;
 
-    public PlayerResultDAO(JdbcTemplate jdbcTemplate) {
+    public PlayerResultDAO(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("play_result")
+                .usingGeneratedKeyColumns("id");
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public int returnTableIdAfterInsert(int count, List<Car> winners) {
-        String sql = "insert into play_result (count, winners) values (?, ?)";
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setInt(1, count);
-            ps.setString(2, makeWinnersString(winners));
-            return ps;
-        }, keyHolder);
-        return (int) keyHolder.getKey();
+    public int returnTableIdAfterInsert(Integer count, List<Car> winners) {
+        Map<String, Object> parameters = new HashMap<>(2);
+        parameters.put("count", count);
+        parameters.put("winners", makeWinnersString(winners));
+        parameters.put("created_at", LocalDateTime.now());
+        return simpleJdbcInsert.executeAndReturnKey(parameters).intValue();
     }
 
     private String makeWinnersString(List<Car> winners) {
