8697f11bee5ba5a3d4c149c8685b22ceb0ac0506
jeonghoon <shb03323@naver.com>
2023-04-19 14:25:33 +0900

    test: given-when-then 적용

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -1,12 +1,11 @@
 package racingcar.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import racingcar.domain.Car;
-import racingcar.domain.TestNumberGenerator;
 import racingcar.dto.request.GameRequestDto;
 import racingcar.dto.response.GameResponseDto;
 import racingcar.entity.Game;
@@ -31,40 +30,22 @@ public class GameServiceTest {
     GameService gameService;
 
     @Test
+    @DisplayName("게임 저장 및 플레이어 결과 저장 테스트")
     public void playGameTest() {
+        // given
         GameRequestDto requestDto = new GameRequestDto("ditoo,leo", 10);
-        final String ditoo = requestDto.getNames().split(",")[0];
-        final String leo = requestDto.getNames().split(",")[1];
-        Game game = new Game(requestDto.getCount(), ditoo);
+        Game game = new Game(requestDto.getCount(), "ditoo");
         given(gameRepository.save(any()))
                 .willReturn(game);
-
-        PlayerResult ditooResult = getDitooResult();
-        PlayerResult leoResult = getLeoResult();
-
         given(playerResultRepository.save(any()))
-                .willReturn(ditooResult);
+                .willReturn(new PlayerResult(1, new PlayerResult("ditoo", 5, 1)));
         given(playerResultRepository.save(any()))
-                .willReturn(leoResult);
+                .willReturn(new PlayerResult(2, new PlayerResult("leo", 3, 1)));
 
+        // when
         GameResponseDto gameResponseDto = gameService.playGame(requestDto);
+
+        // then
         assertThat(gameResponseDto.getWinners()).isEqualTo(game.getWinners());
-    }
-
-    private PlayerResult getDitooResult() {
-        Car ditooCar = new Car("ditoo");
-        ditooCar.move(new TestNumberGenerator(7));
-        ditooCar.move(new TestNumberGenerator(7));
-        PlayerResult ditoo = new PlayerResult(
-                ditooCar.getCarName().getName(), ditooCar.getCurrentPosition().getPosition(), 1);
-        return new PlayerResult(1, ditoo);
-    }
-
-    private PlayerResult getLeoResult() {
-        Car leoCar = new Car("leo");
-        leoCar.move(new TestNumberGenerator(7));
-        PlayerResult leo = new PlayerResult(
-                leoCar.getCarName().getName(), leoCar.getCurrentPosition().getPosition(), 1);
-        return new PlayerResult(1, leo);
     }
 }
