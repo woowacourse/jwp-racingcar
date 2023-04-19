36129734627009b74cc80f3609c973b0abf4e4df
yenawee <yaena0319@naver.com>
2023-04-19 17:39:36 +0900

    fix : @Transactional 관련 테스트 코드 수정

--- src/test/java/racingcar/dao/GameDaoTest.java
+++ src/test/java/racingcar/dao/GameDaoTest.java
@@ -1,6 +1,5 @@
 package racingcar.dao;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Transactional
 class GameDaoTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     private GameDao jdbcGameDao;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         jdbcGameDao = new JdbcGameDao(jdbcTemplate);
     }
 
     @Test
-    @Transactional
     void insertGameTest() {
         // given, when
         int gameId1 = jdbcGameDao.insert("jena", 3);
@@ -34,33 +33,30 @@ class GameDaoTest {
         assertThat(gameId2 - gameId1).isEqualTo(1);
     }
 
-    // TODO:: @transactional 이 안먹히는 거 확인 필요
-//    @Test
-//    @Transactional
-//    void findLastIdTest() {
-//        // given
-//        jdbcGameDao.insert("jena2", 3);
-//        jdbcGameDao.insert("odo2", 6);
-//
-//        // when
-//        int lastGameId = jdbcGameDao.findLastId().orElseThrow(() -> new IllegalStateException("존재하는 게임 정보가 없습니다"));
-//
-//        // then
-//        assertThat(lastGameId).isEqualTo(2);
-//    }
+    @Test
+    void findLastIdTest() {
+        // given
+        jdbcGameDao.insert("jena2", 3);
+        int gameId = jdbcGameDao.insert("odo2", 6);
+
+        // when
+        Integer lastGameId = jdbcGameDao.findLastId().orElseThrow(() -> new IllegalStateException("존재하는 게임 정보가 없습니다"));
+
+        // then
+        assertThat(lastGameId).isEqualTo(gameId);
+    }
 
     @Test
-    @Transactional
     void findWinnersTest() {
         // given
         jdbcGameDao.insert("jena2", 3);
-        jdbcGameDao.insert("odo2", 6);
+        jdbcGameDao.insert("odo3", 6);
 
         // when
         String winners = jdbcGameDao.findWinners(2);
 
         // then
-        assertThat(winners).isEqualTo("odo2");
+        assertThat(winners).isEqualTo("odo3");
 
     }
 }
