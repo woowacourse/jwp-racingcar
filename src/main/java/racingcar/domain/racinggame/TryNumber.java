86aa6acceb448b50a945d924cbc1d0d3f963a165
JunH <tjdtls690@gmail.com>
2023-04-19 15:40:43 +0900

    :sparkles: feat : 시도 횟수가 1 감소하는 기능 구현

--- src/main/java/racingcar/domain/racinggame/TryNumber.java
+++ src/main/java/racingcar/domain/racinggame/TryNumber.java
@@ -1,19 +1,26 @@
 package racingcar.domain.racinggame;
 
-public class TruNumber {
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class TryNumber {
     private static final int MIN_TRU_NUMBER = 1;
     private static final int MAX_TRY_NUMBER = 1_000_000;
     
-    private final int tryNumber;
+    private final int number;
     
-    public TruNumber(int tryNumber) {
-        validateOutOfRange(tryNumber);
-        this.tryNumber = tryNumber;
+    public TryNumber(int number) {
+        validateOutOfRange(number);
+        this.number = number;
     }
     
     private void validateOutOfRange(int tryNumber) {
         if (tryNumber < MIN_TRU_NUMBER || tryNumber > MAX_TRY_NUMBER) {
             throw new IllegalArgumentException("시도 횟수 범위를 벗어났습니다. 다시 입력해주세요.");
         }
+    }
+    
+    public TryNumber decrease() {
+        return new TryNumber(number - 1);
     }
 }
--- src/test/java/racingcar/domain/racinggame/TryNumberTest.java
+++ src/test/java/racingcar/domain/racinggame/TryNumberTest.java
@@ -2,11 +2,11 @@ package racingcar.domain.racinggame;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -15,14 +15,20 @@ class TryNumberTest {
     @ValueSource(ints = {1, 1_000_000})
     void 숫자가_1이상_100만_이하인_경우_예외가_발생하지_않는다(int tryNumber) {
         assertThatNoException()
-                .isThrownBy(() -> new TruNumber(tryNumber));
+                .isThrownBy(() -> new TryNumber(tryNumber));
     }
     
     @ParameterizedTest(name = "{displayName} : tryNumber = {0}")
     @ValueSource(ints = {0, 1_000_001})
     void 숫자가_1미만_또는_100만_초과인_경우_예외가_발생한다(int tryNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new TruNumber(tryNumber))
+                .isThrownBy(() -> new TryNumber(tryNumber))
                 .withMessage("시도 횟수 범위를 벗어났습니다. 다시 입력해주세요.");
+    }
+    
+    @Test
+    void tryNumber가_1_감소한다() {
+        TryNumber tryNumber = new TryNumber(3);
+        assertThat(tryNumber.decrease()).isEqualTo(new TryNumber(2));
     }
 }
