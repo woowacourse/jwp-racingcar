8d0145abfaea6f754591823dca322dd5e892969f
nuyh <hjaehyun25@gmail.com>
2023-04-19 20:54:39 +0900

    build: DB Car 테이블 pk id 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS PLAY_RESULT
 (
-    id          BIGINT   NOT NULL AUTO_INCREMENT,
+    id          BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY,
     winners     VARCHAR(255),
     trial_count INT      NOT NULL,
-    created_at  DATETIME NOT NULL default current_timestamp,
-    PRIMARY KEY (id)
+    created_at  DATETIME NOT NULL default current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS CAR
 (
+    id             BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
     play_result_id BIGINT      NOT NULL,
     name           VARCHAR(10) NOT NULL,
     position       INT         NOT NULL,
