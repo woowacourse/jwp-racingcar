d4bdc1c4e5695021bf054696d7fb70ba5085e39b
BGuga <tmdwns9913@gmail.com>
2023-04-19 20:28:50 +0900

    feat: 콘솔 어플리케이션 복구

---
+++ src/main/java/racingcar/controller/GamePlay.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import racingcar.domain.Cars;
+import racingcar.genertor.NumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GamePlay {
+
+    private final Cars cars;
+    private final NumberGenerator numberGenerator;
+
+    public GamePlay(Cars cars, NumberGenerator numberGenerator) {
+        this.cars = cars;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void gameStart() {
+        OutputView.printInputCarNamesNotice();
+        OutputView.printInputTryTimesNotice();
+        int tryTimes = InputView.inputTryTimes();
+        OutputView.printResultNotice();
+        play(cars, tryTimes);
+        OutputView.printWinner(cars.findWinners());
+    }
+
+    public void play(Cars cars, int tryTimes) {
+        validateTryTimes(tryTimes);
+        while (tryTimes-- > 0) {
+            cars.moveCars(numberGenerator);
+            OutputView.printCarNameAndPosition(cars);
+        }
+    }
+
+    private void validateTryTimes(int tryTimes) {
+        if (tryTimes < 1) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 최소 1회 이상입니다.");
+        }
+    }
+}
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String CAR_NAME_DELIMITER = ",";
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static List<String> inputCarNames() {
+        return List.of(scanner.nextLine().split(CAR_NAME_DELIMITER));
+    }
+
+    public static int inputTryTimes() {
+        return scanner.nextInt();
+    }
+}
