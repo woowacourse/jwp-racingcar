67730dbaaf88009fba89877b90a8a152bcb89934
yujamint <woawoa0513@gmail.com>
2023-04-20 23:07:08 +0900

    feat: Exception Handler 로깅 추가

--- src/main/java/racingcar/exception/ApiExceptionHandler.java
+++ src/main/java/racingcar/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package racingcar.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException e) {
@@ -17,6 +21,7 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleUnCaughtException(Exception e) {
+        logger.error("정의하지 않은 예외가 발생했습니다.");
         final ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exceptionResponse);
