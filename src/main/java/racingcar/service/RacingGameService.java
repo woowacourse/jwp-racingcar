0c79602c1863de20e77e3c94713765606a77ee88
zillionme <beatus96@naver.com>
2023-04-21 06:43:41 +0900

    chore: 주석 제거 및 필요없는 클래스 제거

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Service;
 import racingcar.dao.CarDao;
 import racingcar.dao.GameDao;
 import racingcar.dao.WinnerDao;
+import racingcar.dao.entity.CarEntity;
+import racingcar.dao.entity.GameEntity;
 import racingcar.dao.entity.WinnerEntity;
-import racingcar.domain.*;
+import racingcar.domain.Car;
+import racingcar.domain.NumberGenerator;
+import racingcar.domain.RacingGame;
 import racingcar.dto.CarStatusDto;
 import racingcar.dto.RacingGameRequestDto;
 import racingcar.dto.RacingGameResponseDto;
-import racingcar.dao.entity.CarEntity;
-import racingcar.dao.entity.GameEntity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -82,50 +84,8 @@ public class RacingGameService {
         List<Integer> listOfGameId = gameDao.findGameIds();
 
         return listOfGameId.stream()
-                .peek(gameId -> System.out.println(gameId))
-                .map(gameId -> findResultByGameId(gameId))
+                .map(this::findResultByGameId)
                 .collect(Collectors.toList());
     }
-
-
-    //1차
-//    public RacingGameResponseDto run1(RacingGameRequestDto racingGameRequestDto) {
-//        //todo 2 : String으로 넘어온 자동차 이름들을 구분자(,)를 기준으로 리스트로 변환 후에, 넘겨주는 게 맞을까? RacingGame에 String을 넘기는 것이 맞을까?
-//        //뷰에서 넘어오는 구분자의 기준이 뭐가 될지 모른다. 따라서 컨트롤러에서 하는 게 맞는 것 같다 -- 추후 리팩터링 할것
-//        List<String> carNames = List.of(racingGameRequestDto.getNames().split(DELIMITER));
-//        RacingGame racingGame = RacingGame.of(carNames, racingGameRequestDto.getCount());
-//        racingGame.race(numberGenerator);
-//
-//        int gameId = gameDao.save(GameEntity.of(racingGame.getTryCount(), racingGame.getCreated_at()));;
-//        saveCars(racingGame, gameId);
-//        saveWinners(racingGame);
-//
-//        return createResult(racingGame);
-//    }
-//
-//
-//    private void saveCars1(final RacingGame racingGame, final int gameId) {
-//        for (Car car : racingGame.getCars()) {
-//            CarEntity carEntity = CarEntity.of(car.getName(), car.getPosition(), gameId);
-//            int carId = carDao.save(carEntity);
-//            car.setCarId(carId);
-//        }
-//    }
-//
-//    private void saveWinners1(RacingGame racingGame) {
-//        List<WinnerEntity> winnerEntities = racingGame.pickWinningCars().stream()
-//                        .map(car -> new WinnerEntity(car.getCarId(), car.getGameId()))
-//                        .collect(Collectors.toList());
-//        winnerDao.saveAll(winnerEntities);
-//    }
-//    private RacingGameResponseDto createResult1(final RacingGame racingGame) {
-//        List<String> winnerCars = racingGame.pickWinnerCarNames();
-//        List<CarStatusDto> carStatuses = racingGame.getCars().stream()
-//                .map(car -> new CarStatusDto(car.getName(), car.getPosition()))
-//                .collect(Collectors.toUnmodifiableList());
-//
-//        return new RacingGameResponseDto(winnerCars, carStatuses);
-//    }
-//
 
 }
