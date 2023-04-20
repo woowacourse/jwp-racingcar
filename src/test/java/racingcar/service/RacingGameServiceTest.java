1a6fea96c37a854df4f06406d344b82f62c19499
woosung1223 <dntjd991223@naver.com>
2023-04-20 15:33:12 +0900

    test: Repository Mock을 통한 테스트 개선

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -1,55 +1,76 @@
 package racingcar.service;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import racingcar.dto.GameResultResponse;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import racingcar.domain.RacingGame;
+import racingcar.domain.RandomNumberGenerator;
 import racingcar.dto.RacingGameRequest;
+import racingcar.persistence.repository.RacingGameRepository;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingGameServiceTest {
 
-    @LocalServerPort
-    private int port;
-    @Autowired
+    @MockBean
     private RacingGameService racingGameService;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
 
     @Test
     @DisplayName("플레이어가 한명일 때 GameResultDto가 잘 반환된다")
     void shouldReturnCorrectlyWhenInputOnePlayer() {
+        RacingGameRepository racingGameRepository = Mockito.mock(RacingGameRepository.class);
+        this.racingGameService = new RacingGameService(racingGameRepository);
+
         RacingGameRequest racingGameRequest = new RacingGameRequest(
                 "브리",
                 3
         );
 
-        GameResultResponse gameResultResponse = racingGameService.playRacingGame(racingGameRequest);
+        RacingGame racingGame = racingGameService.playRacingGame(racingGameRequest);
 
-        assertThat(gameResultResponse.getRacingCars()).hasSize(1);
-        assertThat(gameResultResponse.getWinners()).isEqualTo("브리");
+        assertThat(racingGame.getCars()).hasSize(1);
+        assertThat(racingGame.getWinners()).hasSize(1);
     }
 
     @Test
-    @DisplayName("플레이어가 여려명일 때 GameResultDto가 잘 반환된다")
+    @DisplayName("플레이어가 여려 명일 때 GameResultDto가 잘 반환된다")
     void shouldReturnCorrectlyWhenInputManyPlayer() {
-        RacingGameRequest racingGameRequest = new RacingGameRequest(
+        List<RacingGame> allRacingGames = List.of(
+                new RacingGame(
+                        List.of("브리", "토미", "브라운"),
+                        3,
+                        new RandomNumberGenerator()
+                ),
+                new RacingGame(
+                        List.of("제이슨", "구구", "네오"),
+                        3,
+                        new RandomNumberGenerator()
+                )
+        );
+
+        RacingGameRepository racingGameRepository = Mockito.mock(RacingGameRepository.class);
+        given(racingGameRepository.selectAllGames())
+                .willReturn(allRacingGames);
+
+        this.racingGameService = new RacingGameService(racingGameRepository);
+
+        RacingGameRequest firstRacingGameRequest = new RacingGameRequest(
                 "브리,토미,브라운",
                 3
         );
+        RacingGameRequest secondRacingGameRequest = new RacingGameRequest(
+                "제이슨,구구,네오",
+                3
+        );
 
-        GameResultResponse gameResultResponse = racingGameService.playRacingGame(racingGameRequest);
+        racingGameService.playRacingGame(firstRacingGameRequest);
+        racingGameService.playRacingGame(secondRacingGameRequest);
 
-        assertThat(gameResultResponse.getRacingCars()).hasSize(3);
-        assertThat(gameResultResponse.getWinners()).isNotNull();
+        List<RacingGame> allRacingGames1 = racingGameService.makeGameRecords();
+        assertThat(allRacingGames1.size()).isEqualTo(2);
     }
 }
