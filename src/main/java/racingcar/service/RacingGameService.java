27a859bcda3dc30372a6f69e28946153168825a8
yenawee <yaena0319@naver.com>
2023-04-20 21:12:20 +0900

    style : 사용안하는 메소드 삭제

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -60,17 +60,4 @@ public class RacingGameService {
         Map<Integer, PlayResultResponseDto> result = playerDao.findAll();
         return new ArrayList<>(result.values());
     }
-
-//    N + 1 문제 발생하는 코드
-//    public List<PlayResultResponseDto> getPlayHistory() {
-//        List<PlayResultResponseDto> playResultResponseDtos = new ArrayList<>();
-//
-//        List<Integer> gameIds = gameDao.findAllIds();
-//
-//        gameIds.forEach(gameId ->
-//                playResultResponseDtos.add(new PlayResultResponseDto
-//                        (new Winner(Arrays.asList(gameDao.findWinners(gameId).split(","))), playerDao.find(gameId))));
-//
-//        return playResultResponseDtos;
-//    }
 }
