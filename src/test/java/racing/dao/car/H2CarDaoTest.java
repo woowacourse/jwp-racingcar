00217430ce3c8b37e0b9674e0d879c75772d23b3
splitube <voicesplit@daum.net>
2023-04-20 15:49:18 +0900

    style: 테스트 DisplayName, given when then 적용

--- src/test/java/racing/dao/car/H2CarDaoTest.java
+++ src/test/java/racing/dao/car/H2CarDaoTest.java
@@ -2,6 +2,7 @@ package racing.dao.car;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -19,27 +20,41 @@ class H2CarDaoTest {
     @Autowired
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    @DisplayName("DB에 name과 gameId(fk) 를 이용하여 데이터를 삽압한다.")
     @Test
     void insert() {
+        //given
         final H2GameDao h2GameDao = new H2GameDao(namedParameterJdbcTemplate);
-        h2GameDao.insert(new Game("5"));
         final CarDao carDao = new H2CarDao(namedParameterJdbcTemplate);
-        carDao.insert(new Car("스플릿"), 1);
+        final int gameId = h2GameDao.insert(new Game("5"));
+
+        //when
+        carDao.insert(new Car("스플릿"), gameId);
+
+        //then
         final List<CarDto> carsInGame = carDao.findByGameId(1);
         Assertions.assertThat(carsInGame).hasSize(1);
         Assertions.assertThat(carsInGame.get(0).getName()).isEqualTo("스플릿");
     }
 
+    @DisplayName("해당 게임의 아이디를 가지고 있는 모든 데이터를 조회한다.")
     @Test
     void findByGameId() {
+        //given
         final H2GameDao h2GameDao = new H2GameDao(namedParameterJdbcTemplate);
-        h2GameDao.insert(new Game("5"));
-        h2GameDao.insert(new Game("7"));
         final CarDao carDao = new H2CarDao(namedParameterJdbcTemplate);
-        carDao.insert(new Car("게임1"), 1);
+        final int firstGameId = h2GameDao.insert(new Game("5"));
+        final int secondGameId = h2GameDao.insert(new Game("7"));
+        carDao.insert(new Car("게임1-1"), 1);
+        carDao.insert(new Car("게임1-2"), 1);
         carDao.insert(new Car("게임2"), 2);
-        final List<CarDto> carsInGame = carDao.findByGameId(2);
-        Assertions.assertThat(carsInGame).hasSize(1);
-        Assertions.assertThat(carsInGame.get(0).getName()).isEqualTo("게임2");
+
+        //when
+        final List<CarDto> carsInFirstGame = carDao.findByGameId(firstGameId);
+        final List<CarDto> carsInSecondGame = carDao.findByGameId(secondGameId);
+
+        //then
+        Assertions.assertThat(carsInFirstGame).hasSize(2);
+        Assertions.assertThat(carsInSecondGame).hasSize(1);
     }
 }
--- src/test/java/racing/service/WebRacingGameServiceTest.java
+++ src/test/java/racing/service/WebRacingGameServiceTest.java
@@ -2,6 +2,7 @@ package racing.service;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,20 +22,32 @@ class WebRacingGameServiceTest {
     @Autowired
     GameDao gameDao;
 
+    @DisplayName("게임을 플레이 하면 승자와 참가 차량에 대한 정보를 반환한다.")
     @Test
     void playGame() {
+        //given
         final WebRacingGameService webRacingGameService = new WebRacingGameService(carDao, gameDao);
+
+        //when
         final GameResultDto gameResultDto = webRacingGameService.playGame(new GameInputDto("스플릿,아벨", "5"), () -> 1);
+
+        //then
         Assertions.assertThat(gameResultDto.getWinners()).isEqualTo("스플릿,아벨");
         Assertions.assertThat(gameResultDto.getRacingCars()).hasSize(2);
     }
 
+    @DisplayName("플레이한 모든 게임의 정보를 반환한다.")
     @Test
     void showGames() {
+        //given
         final WebRacingGameService webRacingGameService = new WebRacingGameService(carDao, gameDao);
         webRacingGameService.playGame(new GameInputDto("스플릿,아벨", "5"), () -> 1);
         webRacingGameService.playGame(new GameInputDto("아벨,스플릿", "5"), () -> 1);
+
+        //when
         final List<GameResultDto> gameResultDtos = webRacingGameService.showGames();
+
+        //then
         Assertions.assertThat(gameResultDtos).hasSize(2);
         Assertions.assertThat(gameResultDtos.get(0).getWinners()).isEqualTo("스플릿,아벨");
         Assertions.assertThat(gameResultDtos.get(0).getRacingCars()).hasSize(2);
