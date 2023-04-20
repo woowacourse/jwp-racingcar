ddd3a5a0969411b3da73e5f8f4c522e9dd97f2a8
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-20 09:06:20 +0900

    feat: 웹 컨트롤러 핸들러 메서드 매핑

--- src/main/java/racingcar/controller/RacingCarWebController.java
+++ src/main/java/racingcar/controller/RacingCarWebController.java
@@ -1,11 +1,14 @@
 package racingcar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.domain.GameInforamtionDto;
 import racingcar.domain.GameResultDto;
 import racingcar.service.RacingCarService;
+
+import java.util.List;
 
 @RestController
 public class RacingCarWebController {
@@ -19,5 +22,10 @@ public class RacingCarWebController {
     @PostMapping("plays")
     public GameResultDto createGame(@RequestBody GameInforamtionDto gameInforamtionDto) {
         return racingCarService.play(gameInforamtionDto);
+    }
+
+    @GetMapping("plays")
+    public List<GameResultDto> loadAllGame() {
+        return racingCarService.findAllGame();
     }
 }
