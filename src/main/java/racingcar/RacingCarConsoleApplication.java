4c7f8162b730a12c84f2faa15c3cf9330195e079
zillionme <beatus96@naver.com>
2023-04-21 06:44:22 +0900

    refactor: 콘솔 컨트롤러 수정 및 애플리케이션 완성

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -1,10 +1,26 @@
 package racingcar;
 
 import racingcar.controller.console.RacingGameConsoleController;
+import racingcar.dao.console.ConsoleCarDao;
+import racingcar.dao.console.ConsoleGameDao;
+import racingcar.dao.console.ConsoleWinnerDao;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.service.RacingGameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingCarConsoleApplication {
     public static void main(String[] args) {
-        RacingGameConsoleController racingCarController = new RacingGameConsoleController();
-        racingCarController.run();
+        RacingGameService racingGameService = new RacingGameService(
+                new ConsoleGameDao(),
+                new ConsoleCarDao(),
+                new ConsoleWinnerDao(),
+                new RandomNumberGenerator()
+        );
+        RacingGameConsoleController controller = new RacingGameConsoleController(new InputView(), new OutputView(),
+                racingGameService);
+        controller.run();
+
+
     }
 }
--- src/main/java/racingcar/controller/console/RacingGameConsoleController.java
+++ src/main/java/racingcar/controller/console/RacingGameConsoleController.java
@@ -1,74 +1,54 @@
 package racingcar.controller.console;
 
-import racingcar.domain.*;
-import racingcar.dto.CarStatus;
+import org.springframework.stereotype.Controller;
+import racingcar.dto.RacingGameRequestDto;
+import racingcar.dto.RacingGameResponseDto;
+import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
 
+@Controller
 public class RacingGameConsoleController {
 
-    private static final int RACE_START_POINT = 0;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingGameService racingGameService;
+
+    public RacingGameConsoleController(final InputView inputView, final OutputView outputView, final RacingGameService racingGameService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingGameService = racingGameService;
+    }
 
     public void run() {
-        RacingCars cars = createCars();
-        TryCount tries = getTries();
-
-        OutputView.printResultMessage();
-        raceCars(cars, tries.getTries());
-        showFinalResult(cars);
-    }
-
-    private RacingCars createCars() {
         try {
-            List<String> carNames = InputView.inputCarNames();
-            List<Car> cars = CarFactory.generate(carNames, RACE_START_POINT);
-            return new RacingCars(cars);
-
+            RacingGameRequestDto requestDto = createRacingGameRequest();
+            RacingGameResponseDto responseDto = racingGameService.run(requestDto);
+            outputView.printResult(responseDto);
         } catch (IllegalArgumentException e) {
-            OutputView.printError(e.getMessage());
-            return createCars();
+            outputView.printError(e.getMessage());
         }
     }
 
-    private TryCount getTries() {
-        try {
-            int tries = InputView.inputTries();
-            return new TryCount(tries);
+    private RacingGameRequestDto createRacingGameRequest() {
+        String carNames = repeatUntilNoException(inputView::inputCarNames);
+        int tryCount = repeatUntilNoException(inputView::inputTryCount);
+        return new RacingGameRequestDto(carNames, tryCount);
+    }
 
-        } catch (IllegalArgumentException e) {
-            OutputView.printError(e.getMessage());
-            return getTries();
+
+    private <T>T repeatUntilNoException(Supplier<T> supplier) {
+        T obj = null;
+        while (obj == null) {
+            try {
+                obj = supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
         }
+        return obj;
     }
 
-    private void raceCars(RacingCars cars, int tries) {
-        NumberGenerator numberGenerator = new RandomNumberGenerator();
-
-        for (int i = 0; i < tries; i++) {
-            cars.moveCars(numberGenerator);
-            List<CarStatus> raceResult = showRaceResult(cars);
-            OutputView.printRaceResult(raceResult);
-        }
-    }
-
-    private List<CarStatus> showRaceResult(RacingCars racingCars) {
-        return racingCars.getCars().stream()
-                .map(car -> new CarStatus(car.getName(), car.getPosition()))
-                .collect(Collectors.toUnmodifiableList());
-    }
-
-    private void showFinalResult(RacingCars cars) {
-        List<CarStatus> raceResult = showRaceResult(cars);
-        OutputView.printRaceResult(raceResult);
-
-        showWinners(cars);
-    }
-
-    private void showWinners(RacingCars cars) {
-        List<String> winnersName = cars.pickWinnerCarNames();
-        OutputView.printFinalResult(winnersName);
-    }
 }
