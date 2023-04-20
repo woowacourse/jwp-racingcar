270876fa07aad55ae5af206c0919148b34993ef3
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-20 16:14:35 +0900

    refactor: 중복 예외처리 코드 제거

--- src/main/java/racingcar/controller/web/RacingWebAdvice.java
+++ src/main/java/racingcar/controller/web/RacingWebAdvice.java
@@ -3,52 +3,17 @@ package racingcar.controller.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import racingcar.exception.DuplicateCarNamesException;
-import racingcar.exception.ExceedCarNameLengthException;
-import racingcar.exception.InvalidCarNameFormatException;
-import racingcar.exception.InvalidRangeTrialTimesException;
-import racingcar.exception.InvalidTrialTimesFormatException;
+import racingcar.exception.CustomException;
 
 @RestControllerAdvice
 public class RacingWebAdvice {
 
     private final Logger logger = LoggerFactory.getLogger(RacingWebAdvice.class);
 
-    @ExceptionHandler(DuplicateCarNamesException.class)
-    public ResponseEntity<ExceptionInfo> duplicateCarNamesHandler(DuplicateCarNamesException e) {
-        logger.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new ExceptionInfo(e.getMessage()));
-    }
-
-    @ExceptionHandler(ExceedCarNameLengthException.class)
-    public ResponseEntity<ExceptionInfo> exceedCarNameLengthHandler(ExceedCarNameLengthException e) {
-        logger.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new ExceptionInfo(e.getMessage()));
-    }
-
-    @ExceptionHandler(InvalidCarNameFormatException.class)
-    public ResponseEntity<ExceptionInfo> invalidCarNameFormatHandler(InvalidCarNameFormatException e) {
-        logger.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new ExceptionInfo(e.getMessage()));
-    }
-
-    @ExceptionHandler(InvalidRangeTrialTimesException.class)
-    public ResponseEntity<ExceptionInfo> invalidRangeTrialTimesHandler(InvalidRangeTrialTimesException e) {
-        logger.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new ExceptionInfo(e.getMessage()));
-    }
-
-    @ExceptionHandler(InvalidTrialTimesFormatException.class)
-    public ResponseEntity<ExceptionInfo> invalidTrialTimesFormatHandler(InvalidTrialTimesFormatException e) {
-        logger.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new ExceptionInfo(e.getMessage()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionInfo> methodArgsNotValidException(MethodArgumentNotValidException e) {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionInfo> customExceptionHandler(CustomException e) {
         logger.error(e.getMessage());
         return ResponseEntity.badRequest().body(new ExceptionInfo(e.getMessage()));
     }
