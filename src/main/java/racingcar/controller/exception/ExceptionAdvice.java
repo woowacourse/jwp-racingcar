59cadb90516631a97a2da741f6893b229718aa89
woosung1223 <dntjd991223@naver.com>
2023-04-20 10:56:22 +0900

    feat: `@Valid`를 통해 검출된 예외도 메세지를 반환하도록 수정

--- src/main/java/racingcar/controller/exception/ExceptionAdvice.java
+++ src/main/java/racingcar/controller/exception/ExceptionAdvice.java
@@ -1,18 +1,38 @@
 package racingcar.controller.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import racingcar.dto.ExceptionResponse;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ExceptionAdvice {
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class
+    })
+    public ResponseEntity<List<ExceptionResponse>> requestFormatException(final MethodArgumentNotValidException e) {
+        List<ExceptionResponse> exceptions = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .map(ExceptionResponse::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest()
+                .body(exceptions);
+    }
 
     @ExceptionHandler({
             Exception.class
     })
     public ResponseEntity<ExceptionResponse> unhandledException(final Exception e) {
         return ResponseEntity.badRequest()
-                .body(new ExceptionResponse("에상치 못한 예외가 발생했습니다."));
+                .body(new ExceptionResponse("예상치 못한 예외가 발생했습니다."));
     }
 }
