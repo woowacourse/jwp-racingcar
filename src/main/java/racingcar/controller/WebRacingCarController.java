04295cd39b019bee762b1797871b6274938e134e
jaehee329 <jjhs9803@gmail.com>
2023-04-20 13:49:54 +0900

    fix: 웹 컨트롤러가 @RestController를 사용하도록 변경

--- src/main/java/racingcar/controller/WebRacingCarController.java
+++ src/main/java/racingcar/controller/WebRacingCarController.java
@@ -3,18 +3,17 @@ package racingcar.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import racingcar.dto.RequestDto;
 import racingcar.dto.ResponseDto;
 import racingcar.service.MainRacingCarService;
 
-@Controller
+@RestController
 @RequestMapping("/plays")
 public class WebRacingCarController {
 
@@ -31,7 +30,6 @@ public class WebRacingCarController {
     }
 
     @PostMapping
-    @ResponseBody
     public ResponseEntity<ResponseDto> playRacingCar(@RequestBody RequestDto requestDto) {
         final ResponseDto responseDto = mainRacingCarService.raceCar(requestDto);
         return ResponseEntity.ok().body(responseDto);
