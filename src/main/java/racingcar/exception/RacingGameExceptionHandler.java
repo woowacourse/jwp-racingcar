e87d5d8ad0b5c3f8f8a8e9723d63b675643c2614
ingpyo <juninpyo@naver.com>
2023-04-20 11:14:57 +0900

    refactor: 애러메시지 출력형식 변경

--- src/main/java/racingcar/exception/RacingGameExceptionHandler.java
+++ src/main/java/racingcar/exception/RacingGameExceptionHandler.java
@@ -1,13 +1,18 @@
 package racingcar.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class RacingGameExceptionHandler {
+
+    private static final String DELIMITER = ", ";
 
     @ExceptionHandler
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
@@ -17,8 +22,12 @@ public class RacingGameExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidException(final MethodArgumentNotValidException e) {
+        final String errorMessage = e.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(DELIMITER));
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
+                .body(errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
