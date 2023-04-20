67443304d43e223b538803e8a5c8b16d7d64f4b5
hum02 <ehdgma02@gmail.com>
2023-04-20 15:50:45 +0900

    refactor: 생성자에  @Autowired 제거

--- src/main/java/racingcar/controller/WebRacingGameController.java
+++ src/main/java/racingcar/controller/WebRacingGameController.java
@@ -16,7 +16,6 @@ import java.util.List;
 public class WebRacingGameController {
     private final RacingGameService racingGameService;
 
-    @Autowired
     public WebRacingGameController(RacingGameService racingGameService) {
         this.racingGameService = racingGameService;
     }
