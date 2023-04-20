941a31070ad6443eb257bc8261c11415c9e2a607
zillionme <beatus96@naver.com>
2023-04-20 12:09:38 +0900

    feat: carDao 자동차 조회기능 추가

--- src/main/java/racingcar/dao/CarDao.java
+++ src/main/java/racingcar/dao/CarDao.java
@@ -2,7 +2,6 @@ package racingcar.dao;
 
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -23,13 +22,20 @@ public class CarDao {
                 .usingGeneratedKeyColumns("car_id");
     }
 
-    public int save(CarEntity carEntity) {
-        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(carEntity);
-        return insertActor.executeAndReturnKey(sqlParameterSource).intValue();
-    }
-
     public int[] saveAll(List<CarEntity> carEntities) {
         SqlParameterSource[] sqlParameterSource = SqlParameterSourceUtils.createBatch(carEntities);
         return insertActor.executeBatch(sqlParameterSource);
+    }
+
+    public List<CarEntity> findCarsByGameID(final int gameId) {
+        String sql = "SELECT * FROM CAR WHERE game_id =?";
+        return jdbcTemplate.query(sql,(resultSet, rowNum)-> {
+            int carId = resultSet.getInt("car_id");
+            String name = resultSet.getString("name");
+            int position = resultSet.getInt("position");
+            int game_id = resultSet.getInt("game_id");                    
+            return new CarEntity(carId, name, position, game_id);
+        }, gameId);
+
     }
 }
