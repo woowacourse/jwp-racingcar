39884bb7c9d1bad4159b180491ba9bc5712774a9
jeonghoon <shb03323@naver.com>
2023-04-20 14:59:49 +0900

    feat: 전체 게임 조회 controller 로직 구현

--- src/main/java/racingcar/controller/ApiController.java
+++ src/main/java/racingcar/controller/ApiController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import racingcar.service.dto.GameResponseDto;
 import racingcar.service.GameService;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class ApiController {
@@ -26,5 +28,11 @@ public class ApiController {
     public ResponseEntity<GameResponseDto> playGame(@RequestBody GameRequestDto request) {
         final GameResponseDto gameResponseDto = gameService.playGame(request);
         return ResponseEntity.created(URI.create("/plays")).body(gameResponseDto);
+    }
+
+    @GetMapping(value = "/plays", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<GameResponseDto>> getAllGames() {
+        final List<GameResponseDto> gameResponseDtos = gameService.getAll();
+        return ResponseEntity.ok().body(gameResponseDtos);
     }
 }
--- src/test/java/racingcar/jwp/ApiControllerTest.java
+++ src/test/java/racingcar/jwp/ApiControllerTest.java
@@ -1,6 +1,11 @@
 package racingcar.jwp;
 
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,8 +15,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import racingcar.service.dto.GameRequestDto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApiControllerTest {
@@ -41,5 +48,47 @@ public class ApiControllerTest {
                 .body("racingCars.size()", is(2))
                 .body("racingCars[0].name", is("ditoo"))
                 .body("racingCars[1].name", is("leo"));
+    }
+
+    @Test
+    @DisplayName("전체 게임 조회 테스트")
+    void getAllTest() {
+        // given
+        final GameRequestDto gameRequestDto1 = new GameRequestDto("디투,레오", 10);
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(gameRequestDto1)
+                .post("/plays");
+
+        final GameRequestDto gameRequestDto2 = new GameRequestDto("디투,에단,홍실,블랙캣", 10);
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(gameRequestDto2)
+                .post("/plays");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .when().get("/plays")
+                .then().log().all()
+                .extract();
+        Configuration conf = Configuration.defaultConfiguration();
+        DocumentContext documentContext = JsonPath.using(conf).parse(response.asString());
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat((int) documentContext.read("$.size()")).isEqualTo(2),
+                () -> assertThat((int) documentContext.read("$[0]['racingCars'].size()")).isEqualTo(2),
+                () -> assertThat((int) documentContext.read("$[1]['racingCars'].size()")).isEqualTo(4),
+                () -> assertThat((String) documentContext.read("$[0]['racingCars'][0]['name']")).isEqualTo("디투"),
+                () -> assertThat((String) documentContext.read("$[0]['racingCars'][1]['name']")).isEqualTo("레오"),
+                () -> assertThat((String) documentContext.read("$[1]['racingCars'][0]['name']")).isEqualTo("디투"),
+                () -> assertThat((String) documentContext.read("$[1]['racingCars'][1]['name']")).isEqualTo("에단"),
+                () -> assertThat((String) documentContext.read("$[1]['racingCars'][2]['name']")).isEqualTo("홍실"),
+                () -> assertThat((String) documentContext.read("$[1]['racingCars'][3]['name']")).isEqualTo("블랙캣")
+        );
     }
 }
