081af72a403bba82570455f82328fa2664ae2f13
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-20 15:56:13 +0900

    refactor: 콘솔 dao 인메모리로 구현

--- src/main/java/racingcar/dao/RacingConsoleDao.java
+++ src/main/java/racingcar/dao/RacingConsoleDao.java
@@ -1,33 +1,60 @@
 package racingcar.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import racingcar.dao.dto.CarDto;
 import racingcar.dao.dto.TrackDto;
+import racingcar.model.car.Car;
 import racingcar.model.car.Cars;
 
 public class RacingConsoleDao implements RacingDao {
+
+    private static final Map<Integer, CarDto> carDtoRepository = new HashMap<>();
+    private static final Map<Integer, TrackDto> trackDtoRepository = new HashMap<>();
+
     @Override
     public void saveCar(CarDto carDto) {
-
+        Integer carDtoId = generateCarId();
+        carDtoRepository.put(carDtoId, carDto);
     }
 
     @Override
     public void saveWithBatch(List<CarDto> carDtos) {
-
+        for (CarDto carDto : carDtos) {
+            Integer carDtoId = generateCarId();
+            carDtoRepository.put(carDtoId, carDto);
+        }
     }
 
     @Override
     public Integer saveTrack(TrackDto trackDto) {
-        return null;
+        Integer trackDtoId = generateTrackId();
+        trackDtoRepository.put(trackDtoId, trackDto);
+        return trackDtoId;
     }
 
     @Override
     public List<Integer> findAllTrackIds() {
-        return null;
+        return new ArrayList<>(trackDtoRepository.keySet());
     }
 
     @Override
     public Cars findAllCarsByTrackId(int trackId) {
-        return null;
+        List<Car> cars = carDtoRepository.values()
+                .stream()
+                .map(carDto -> new Car(carDto.getName(), carDto.getPosition()))
+                .collect(Collectors.toList());
+        return new Cars(cars);
+    }
+
+    private Integer generateCarId() {
+        return carDtoRepository.size() + 1;
+    }
+
+    private Integer generateTrackId() {
+        return trackDtoRepository.size() + 1;
     }
 }
