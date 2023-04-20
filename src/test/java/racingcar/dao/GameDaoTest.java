5d41054ddd5f2318ca982acee043f66696c04411
yenawee <yaena0319@naver.com>
2023-04-20 13:29:09 +0900

    test : gameDaoTest 테스트 추가

--- src/test/java/racingcar/dao/GameDaoTest.java
+++ src/test/java/racingcar/dao/GameDaoTest.java
@@ -1,10 +1,13 @@
 package racingcar.dao;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -21,12 +24,20 @@ class GameDaoTest {
 
     @Test
     void insertGameTest() {
-        // given, when
-        int gameId1 = jdbcGameDao.insert("jena", 3);
-        int gameId2 = jdbcGameDao.insert("odo", 6);
+        Assertions.assertThatNoException().isThrownBy(() -> jdbcGameDao.insert("jena,odo", 1));
+    }
+
+    @Test
+    void findAllIdsTest() {
+        // given
+        jdbcGameDao.insert("jena1", 2);
+        jdbcGameDao.insert("odo1", 3);
+
+        // when
+        List<Integer> gameIds = jdbcGameDao.findAllIds();
 
         // then
-        assertThat(gameId2 - gameId1).isEqualTo(1);
+        assertThat(gameIds.size()).isEqualTo(2);
     }
 
     @Test
