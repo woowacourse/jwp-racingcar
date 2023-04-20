f8d86e077f3d5bbdde77ca9b5381ee6aec620a00
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-04-20 16:23:52 +0900

    refactor : Application 테스트 추가

--- src/test/java/racingcar/RacingCarApplicationTests.java
+++ src/test/java/racingcar/RacingCarApplicationTests.java
@@ -1,13 +1,20 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingCarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private RacingCarApplication racingCarApplication;
+
+    @Test
+    void contextLoads() {
+        assertThat(racingCarApplication).isNotNull();
+    }
 
 }
