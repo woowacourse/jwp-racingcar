8d27d4a2bc4e9bdcad13d623afcdf82ae8314ab8
ingpyo <juninpyo@naver.com>
2023-04-20 16:02:53 +0900

    teat: 게임 결과 테스트 추가

--- src/test/java/racingcar/controller/WebRacingGameControllerTest.java
+++ src/test/java/racingcar/controller/WebRacingGameControllerTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -60,6 +61,32 @@ class WebRacingGameControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("winners").exists())
                 .andExpect(jsonPath("racingCars", hasSize(2)))
+                .andDo(print());
+    }
+
+    @Test
+    void 게임_진행_결과() throws Exception {
+        List<GameResponseDto> gameResponseDtos = List.of(
+                new GameResponseDto(
+                        List.of("허브"),
+                        List.of(
+                                new PlayerDto("허브", 4),
+                                new PlayerDto("비버", 0),
+                                new PlayerDto("내꺼", 2)
+                        ))
+        );
+
+        given(racingGameService.findAll())
+                .willReturn(gameResponseDtos);
+
+        mockMvc.perform(get("/plays")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].winners[0]").value("허브"))
+                .andExpect(jsonPath("$[0].racingCars", hasSize(3)))
+                .andExpect(jsonPath("$[0].racingCars[0].name").value("허브"))
+                .andExpect(jsonPath("$[0].racingCars[0].position").value(4))
                 .andDo(print());
     }
 }
