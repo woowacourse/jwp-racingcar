8148fec6e8a4d4e04742287578e94b0b65ad52a8
somsom13 <somin1113@naver.com>
2023-04-20 15:44:32 +0900

    test: 게임 이력 조회 테스트 추가

--- src/test/java/racingcar/RacingGameRestTest.java
+++ src/test/java/racingcar/RacingGameRestTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import io.restassured.RestAssured;
@@ -36,6 +37,23 @@ class RacingGameRestTest {
                 .statusCode(HttpStatus.OK.value())
                 .body("winners", notNullValue())
                 .body("racingCars", hasSize(3));
+    }
+
+    @DisplayName("게임 이력을 조회할 수 있다")
+    @Test
+    void viewAllGameHistories() {
+        RacingGameRequest racingGameRequest = new RacingGameRequest("바론,로지,져니", 10);
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(racingGameRequest)
+                .post("/plays");
+
+        RestAssured.given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/plays")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", is(1));
     }
 
     @DisplayName("잘못된 이동 횟수를 입력할 경우 400에러가 반환된다.")
