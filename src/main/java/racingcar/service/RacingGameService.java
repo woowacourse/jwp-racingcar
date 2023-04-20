31697bcfc1149bc77e4061072a8a3982eff8a65f
zillionme <beatus96@naver.com>
2023-04-20 12:12:52 +0900

    refactor: 레이싱 게임 서비스 수정

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -18,8 +18,6 @@ import java.util.stream.Collectors;
 
 @Service
 public class RacingGameService {
-
-    private static final int RACE_START_POINT = 0;
     private static final String DELIMITER = ",";
 
     private final GameDao gameDao;
@@ -38,29 +36,31 @@ public class RacingGameService {
         //todo 2 : String으로 넘어온 자동차 이름들을 구분자(,)를 기준으로 리스트로 변환 후에, 넘겨주는 게 맞을까? RacingGame에 String을 넘기는 것이 맞을까?
         //뷰에서 넘어오는 구분자의 기준이 뭐가 될지 모른다. 따라서 컨트롤러에서 하는 게 맞는 것 같다 -- 추후 리팩터링 할것
         List<String> carNames = List.of(racingGameRequestDto.getNames().split(DELIMITER));
+
         RacingGame racingGame = RacingGame.of(carNames, racingGameRequestDto.getCount());
         racingGame.race(numberGenerator);
 
-        int gameId = gameDao.save(GameEntity.of(racingGame.getTryCount(), racingGame.getCreated_at()));;
-        saveCars(racingGame, gameId);
-        saveWinners(racingGame);
+        int gameId = gameDao.save(GameEntity.from(racingGame));
+        saveCars(racingGame.getCars(), gameId);
+        saveWinners(racingGame.pickWinnerCarNames(), gameId);
 
         return createResult(racingGame);
     }
 
-
-    private void saveCars(final RacingGame racingGame, final int gameId) {
-        for (Car car : racingGame.getCars()) {
-            CarEntity carEntity = CarEntity.of(car.getName(), car.getPosition(), gameId);
-            int carId = carDao.save(carEntity);
-            car.setCarId(carId);
-        }
+    private void saveCars(final List<Car> cars, final int gameId) {
+        List<CarEntity> carEntities = cars.stream()
+                .map(car -> CarEntity.from(car, gameId))
+                .collect(Collectors.toCollection(ArrayList::new));
+        carDao.saveAll(carEntities);
     }
 
-    private void saveWinners(RacingGame racingGame) {
-        List<WinnerEntity> winnerEntities = racingGame.pickWinningCars().stream()
-                        .map(car -> new WinnerEntity(car.getCarId(), car.getGameId()))
-                        .collect(Collectors.toList());
+    private void saveWinners(List<String> winnerNames, int gameId) {
+        List<CarEntity> cars = carDao.findCarsByGameID(gameId);
+        List<WinnerEntity> winnerEntities = cars.stream()
+                .filter(car -> winnerNames.contains(car.getName()))
+                .map(car -> new WinnerEntity(car.getCarId(), car.getGameId()))
+                .collect(Collectors.toList());
+
         winnerDao.saveAll(winnerEntities);
     }
 
@@ -72,21 +72,65 @@ public class RacingGameService {
 
         return new RacingGameResponseDto(winnerCars, carStatuses);
     }
+    private RacingGameResponseDto selectResultByGameId(int gameId) {
+        List<CarEntity> carEntities = carDao.findCarsByGameID(gameId);
+        List<String> winnerCarNames = findWinnerCarNames(gameId, carEntities);
 
-    //to remove
+        List<CarStatusDto> carStatuses = carEntities.stream()
+                .map(car -> new CarStatusDto(car.getName(), car.getPosition()))
+                .collect(Collectors.toUnmodifiableList());
 
-    private void saveCars2(RacingGame racingGame, final int gameId) {
-        List<CarEntity> carEntities = new ArrayList<>();
-        for (Car car : racingGame.getCars()) {
-            carEntities.add(CarEntity.of(car.getName(), car.getPosition(), gameId));
-        }
-        carDao.saveAll(carEntities);
+        return new RacingGameResponseDto(winnerCarNames, carStatuses);
+    }
+
+    private List<String> findWinnerCarNames(final int gameId, final List<CarEntity> cars) {
+        List<Integer> winnerCarsId = winnerDao.findWinnerCarIdsByGameId(gameId);
+        return cars.stream()
+                .filter(car -> winnerCarsId.contains(car.getCarId()))
+                .map(CarEntity::getName)
+                .collect(Collectors.toList());
     }
 
 
-    //비즈니스 로직과 분리된 entity의 생성 책임은 gameDao에 있다고 생각한다
-    private int saveGame1(final RacingGame racingGame) {
-        String winnerCars = String.join(",", racingGame.pickWinnerCarNames());
-        return gameDao.save(GameEntity.of(racingGame.getTryCount(), racingGame.getCreated_at()));
-    }
+
+
+    //1차
+//    public RacingGameResponseDto run1(RacingGameRequestDto racingGameRequestDto) {
+//        //todo 2 : String으로 넘어온 자동차 이름들을 구분자(,)를 기준으로 리스트로 변환 후에, 넘겨주는 게 맞을까? RacingGame에 String을 넘기는 것이 맞을까?
+//        //뷰에서 넘어오는 구분자의 기준이 뭐가 될지 모른다. 따라서 컨트롤러에서 하는 게 맞는 것 같다 -- 추후 리팩터링 할것
+//        List<String> carNames = List.of(racingGameRequestDto.getNames().split(DELIMITER));
+//        RacingGame racingGame = RacingGame.of(carNames, racingGameRequestDto.getCount());
+//        racingGame.race(numberGenerator);
+//
+//        int gameId = gameDao.save(GameEntity.of(racingGame.getTryCount(), racingGame.getCreated_at()));;
+//        saveCars(racingGame, gameId);
+//        saveWinners(racingGame);
+//
+//        return createResult(racingGame);
+//    }
+//
+//
+//    private void saveCars1(final RacingGame racingGame, final int gameId) {
+//        for (Car car : racingGame.getCars()) {
+//            CarEntity carEntity = CarEntity.of(car.getName(), car.getPosition(), gameId);
+//            int carId = carDao.save(carEntity);
+//            car.setCarId(carId);
+//        }
+//    }
+//
+//    private void saveWinners1(RacingGame racingGame) {
+//        List<WinnerEntity> winnerEntities = racingGame.pickWinningCars().stream()
+//                        .map(car -> new WinnerEntity(car.getCarId(), car.getGameId()))
+//                        .collect(Collectors.toList());
+//        winnerDao.saveAll(winnerEntities);
+//    }
+//    private RacingGameResponseDto createResult1(final RacingGame racingGame) {
+//        List<String> winnerCars = racingGame.pickWinnerCarNames();
+//        List<CarStatusDto> carStatuses = racingGame.getCars().stream()
+//                .map(car -> new CarStatusDto(car.getName(), car.getPosition()))
+//                .collect(Collectors.toUnmodifiableList());
+//
+//        return new RacingGameResponseDto(winnerCars, carStatuses);
+//    }
+
 }
