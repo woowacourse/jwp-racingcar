cb33c11ea7505a273b9b798ece0e7d71ff20e79d
echo <echo.backend@gmail.com>
2023-04-20 12:03:40 +0900

    feat: Get plays 응답 기능 추가

--- src/main/java/racingcar/game/controller/WebController.java
+++ src/main/java/racingcar/game/controller/WebController.java
@@ -1,6 +1,8 @@
 package racingcar.game.controller;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,11 @@ public class WebController {
     public ResponseEntity<GameResponseDTO> play(@RequestBody final GameRequestDTO gameRequestDTO) {
         final GameResponseDTO gameResponseDTO = this.racingCarGameService.createGame(gameRequestDTO);
         return ResponseEntity.ok().body(gameResponseDTO);
+    }
+    
+    @GetMapping(path = "/plays")
+    public ResponseEntity<List<GameResponseDTO>> retrieveAllGames() {
+        final List<GameResponseDTO> gameResponseDTOs = this.racingCarGameService.retrieveAllGames();
+        return ResponseEntity.ok().body(gameResponseDTOs);
     }
 }
