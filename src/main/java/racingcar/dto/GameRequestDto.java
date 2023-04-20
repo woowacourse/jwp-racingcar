15bbe1154b3241738916a94d17a4c8c2c326f8c6
bjk1649 <qhwns1649@naver.com>
2023-04-20 16:10:31 +0900

    refactor: 웹에서의 요청이 문자열의 배열로 전달되도록 변경

--- src/main/java/racingcar/dto/GameRequestDto.java
+++ src/main/java/racingcar/dto/GameRequestDto.java
@@ -1,26 +1,22 @@
 package racingcar.dto;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 public class GameRequestDto {
     @NotNull
-    private final String names;
+    private final List<String> names;
     @Positive
     private final int count;
 
-    public GameRequestDto(String names, int count) {
+    public GameRequestDto(List<String> names, int count) {
         this.names = names;
         this.count = count;
     }
 
     public List<String> getNames() {
-        return Arrays.stream(names.split(","))
-                .map(String::strip)
-                .collect(Collectors.toUnmodifiableList());
+        return names;
     }
 
     public int getCount() {
