b7a27d2f67800612532044b196fecb1dee6bffaa
jjongwa <troas96@naver.com>
2023-04-20 09:41:31 +0900

    refactor: Autowired 생략

--- src/main/java/racingcar/dao/GameDao.java
+++ src/main/java/racingcar/dao/GameDao.java
@@ -1,25 +1,22 @@
 package racingcar.dao;
 
+import java.util.HashMap;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
+import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 import racingcar.domain.TrialCount;
 
 @Repository
 public class GameDao {
     private final SimpleJdbcInsert simpleJdbcInsert;
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     private final RowMapper<Integer> gameNumberMapper = (resultSet, rowNum) -> resultSet.getInt("game_number");
 
-    @Autowired
     public GameDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
                 .withTableName("game")
