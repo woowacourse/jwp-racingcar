68e6ae553239a6d2e5d9c925e9d50bcf6b7d374a
woosung1223 <dntjd991223@naver.com>
2023-04-20 14:30:18 +0900

    test: 프로덕션 코드 수정으로 인한 테스트 코드 수정

--- src/test/java/racingcar/persistence/RacingGameRepositoryTest.java
+++ src/test/java/racingcar/persistence/RacingGameRepositoryTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import racingcar.dto.CarData;
-import racingcar.dto.GameResultResponse;
+import racingcar.domain.DeterminedNumberGenerator;
+import racingcar.domain.RacingGame;
+import racingcar.domain.RandomNumberGenerator;
 import racingcar.persistence.dao.GameResultDao;
 import racingcar.persistence.dao.PlayerResultDao;
 import racingcar.persistence.repository.RacingGameRepository;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,14 +37,13 @@ class RacingGameRepositoryTest {
     @Test
     @DisplayName("게임 결과가 데이터베이스에 저장될 수 있다")
     void shouldSaveGameResultWhenRequest() {
-        String expectedWinners = "브리,브라운";
-        racingGameRepository.saveGameRecord(
-                new GameResultResponse(
-                        expectedWinners,
-                        Collections.emptyList()
-                ),
-                3
+        RacingGame racingGame = new RacingGame(
+                List.of("브리", "브라운"),
+                3,
+                new RandomNumberGenerator()
         );
+
+        racingGameRepository.saveGame(racingGame);
 
         String actualWinners = jdbcTemplate.queryForObject("SELECT winners FROM GAME_RESULT", String.class);
         int trialCount = jdbcTemplate.queryForObject("SELECT trial_count FROM GAME_RESULT", Integer.class);
@@ -55,17 +54,21 @@ class RacingGameRepositoryTest {
     @Test
     @DisplayName("플레이어 별 정보가 데이터베이스에 저장될 수 있다")
     void shouldSaveEachPlayerResultWhenRequest() {
-        String expectedWinners = "브리,브라운";
-        List<CarData> carData = List.of(
-                new CarData("브리", 2),
-                new CarData("토미", 1),
-                new CarData("브라운", 2)
+        DeterminedNumberGenerator determinedNumberGenerator = new DeterminedNumberGenerator();
+        determinedNumberGenerator.readRepository(List.of(
+                4, 0, 4,
+                4, 4, 4,
+                0, 0, 0
+        ));
+
+        RacingGame racingGame = new RacingGame(
+                List.of("브리", "토미", "브라운"),
+                3,
+                determinedNumberGenerator
         );
 
-        racingGameRepository.saveGameRecord(
-                new GameResultResponse(expectedWinners, carData),
-                3
-        );
+        racingGame.start();
+        racingGameRepository.saveGame(racingGame);
 
         int positionOfBri = jdbcTemplate.queryForObject("SELECT position FROM PLAYER_RESULT WHERE name = '브리'", Integer.class);
         int positionOfTomi = jdbcTemplate.queryForObject("SELECT position FROM PLAYER_RESULT WHERE name = '토미'", Integer.class);
