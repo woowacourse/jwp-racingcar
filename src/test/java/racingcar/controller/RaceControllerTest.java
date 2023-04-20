057751b220dd24d9d402172cf1ed3c0b2bd0c293
ChoiYoungHoon <dudgns2865@naver.com>
2023-04-20 12:46:44 +0900

    test: controller test 수정

--- src/test/java/racingcar/controller/RaceControllerTest.java
+++ src/test/java/racingcar/controller/RaceControllerTest.java
@@ -1,13 +1,16 @@
 package racingcar.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import racingcar.domain.CarName;
 import racingcar.domain.CarPosition;
 import racingcar.domain.dao.entity.CarEntity;
@@ -43,30 +47,40 @@ class RaceControllerTest {
     @DisplayName("자동차 경주 성공")
     void plays_success() throws Exception {
         // given
-        final RaceRequest raceRequest = new RaceRequest("두둠,져니", 10);
+        final RaceRequest raceRequest = new RaceRequest("test1,test2", 10);
         final String request = objectMapper.writeValueAsString(raceRequest);
-        final CarStatusDto carStatusDto = CarStatusDto.of(CarName.create("두둠"),
+        final CarStatusDto expectedCarStatusDto = CarStatusDto.of(CarName.create("test1"),
             new CarPosition(6));
-        final RaceResponse raceResponse = RaceResponse.of(List.of("두둠"), List.of(carStatusDto));
+        final RaceResponse expectedResponse = RaceResponse.of(List.of("test1"),
+            List.of(expectedCarStatusDto));
 
         when(raceService.play(any()))
-            .thenReturn(raceResponse);
+            .thenReturn(expectedResponse);
 
         // when, then
-        mockMvc.perform(post("/plays")
+        final MvcResult result = mockMvc.perform(post("/plays")
                 .content(request)
                 .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andExpect(jsonPath("$.winners").value("두둠"))
-            .andExpect(jsonPath("$.racingCars[0].name").value("두둠"))
-            .andExpect(jsonPath("$.racingCars[0].position").value(6));
+            .andExpect(status().isOk())
+            .andReturn();
+
+        final RaceResponse response = jsonToObject(result, RaceResponse.class);
+
+        final List<CarStatusDto> carStatusDtos = response.getRacingCars();
+        for (final CarStatusDto carStatusDto : carStatusDtos) {
+            assertThat(carStatusDto.getName())
+                .isEqualTo(expectedCarStatusDto.getName());
+            assertThat(carStatusDto.getPosition())
+                .isEqualTo(expectedCarStatusDto.getPosition());
+        }
+        assertThat(response.getWinners()).isEqualTo(expectedResponse.getWinners());
     }
 
     @Test
     @DisplayName("자동차 경주 실패 - 비즈니스 로직 예외")
     void plays_fail_business_exception() throws Exception {
         // given
-        final RaceRequest raceRequest = new RaceRequest("두둠,져니", 10);
+        final RaceRequest raceRequest = new RaceRequest("test1,test2", 10);
         final String request = objectMapper.writeValueAsString(raceRequest);
 
         // when
@@ -81,24 +95,49 @@ class RaceControllerTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("자동차 경주 기록 조회")
     public void testFindAllRace() throws Exception {
         //given
         final RaceEntity raceEntity = new RaceEntity(1L, 1, "test");
         final List<CarEntity> carEntities = List.of(new CarEntity(1L, "test", 1));
-        final List<RaceResponse> raceResponses = List.of(RaceResponse.of(raceEntity, carEntities));
+        final List<RaceResponse> expectedResponse = List.of(
+            RaceResponse.of(raceEntity, carEntities));
 
         //when
         when(raceService.findAllRace())
-            .thenReturn(raceResponses);
+            .thenReturn(expectedResponse);
 
         //then
-        mockMvc.perform(get("/plays")
+        final MvcResult result = mockMvc.perform(get("/plays")
                 .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is2xxSuccessful())
-            .andExpect(jsonPath("$[0].winners").value(raceEntity.getWinners()))
-            .andExpect(jsonPath("$[0].racingCars[0].name").value(carEntities.get(0).getName()))
-            .andExpect(
-                jsonPath("$[0].racingCars[0].position").value(carEntities.get(0).getPosition()));
+            .andExpect(status().isOk())
+            .andReturn();
+
+        final List<RaceResponse> responses = Arrays.asList(
+            jsonToObject(result, RaceResponse[].class));
+
+        for (int i = 0; i < responses.size(); i++) {
+            final RaceResponse raceResponse = responses.get(i);
+            assertThat(raceResponse.getWinners())
+                .isEqualTo(expectedResponse.get(i).getWinners());
+
+            final List<CarStatusDto> carStatusDtos = raceResponse.getRacingCars();
+            final List<CarStatusDto> expectedCarStatusDtos = expectedResponse.get(i)
+                .getRacingCars();
+
+            for (int j = 0; j < carStatusDtos.size(); j++) {
+                assertThat(carStatusDtos.get(j).getName())
+                    .isEqualTo(expectedCarStatusDtos.get(j).getName());
+                assertThat(carStatusDtos.get(j).getPosition())
+                    .isEqualTo(expectedCarStatusDtos.get(j).getPosition());
+            }
+        }
+    }
+
+    private <T> T jsonToObject(final MvcResult result, final Class<T> valueType)
+        throws UnsupportedEncodingException, JsonProcessingException {
+        final String responseString = result.getResponse()
+            .getContentAsString();
+        return objectMapper.readValue(responseString, valueType);
     }
 }
