2d318435c86c2abc737aa4c888c024344aaacaf3
Combi153 <jcm04@naver.com>
2023-04-21 01:48:29 +0900

    feat: WebController 예외 처리 로직 추가

--- src/main/java/racingcar/controller/WebController.java
+++ src/main/java/racingcar/controller/WebController.java
@@ -1,16 +1,20 @@
 package racingcar.controller;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 import racingcar.dto.RacingResultDTO;
 import racingcar.dto.RacingStartDTO;
 import racingcar.service.RacingCarService;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class WebController {
@@ -31,5 +35,29 @@ public class WebController {
     public ResponseEntity<List<RacingResultDTO>> showGameResult() {
         final List<RacingResultDTO> gameResults = racingCarService.showGameResults();
         return ResponseEntity.ok(gameResults);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        Map<String, String> errorMessageByFields = new HashMap<>();
+
+        final List<ObjectError> allErrors = exception.getBindingResult().getAllErrors();
+        allErrors.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errorMessageByFields.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessageByFields);
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<String> handleInvalidFormat(InvalidFormatException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("[ERROR] 시도할 횟수에 숫자를 입력할 수 없습니다. 입력 값 : " + exception.getValue());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 }
