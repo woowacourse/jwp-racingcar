6b89b9af662756b08375d8d29d70b1ed5bfda593
echo <echo.backend@gmail.com>
2023-04-20 09:15:10 +0900

    refactor: Game play static 메서드로 분리

--- src/main/java/racingcar/game/interfaces/Game.java
+++ src/main/java/racingcar/game/interfaces/Game.java
@@ -1,9 +1,22 @@
 package racingcar.game.interfaces;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.car.interfaces.Car;
+import racingcar.car.interfaces.NumberGenerator;
+import racingcar.car.model.RacingCar;
+import racingcar.game.model.RacingCarGame;
 
 public interface Game {
+    
+    static GameResult run(final NumberGenerator numberGenerator, final int count, final String namesLiteral) {
+        final List<Car> cars = Arrays.stream(namesLiteral.split(",")).map(name -> RacingCar.create(name, 0))
+                .collect(Collectors.toList());
+        final Game racingCarGame = RacingCarGame.create(numberGenerator, cars);
+        final Game updatedGame = racingCarGame.race(count);
+        return updatedGame.calculateResult();
+    }
     
     Game moveCars();
     
--- src/main/java/racingcar/game/service/RacingCarGameService.java
+++ src/main/java/racingcar/game/service/RacingCarGameService.java
@@ -1,20 +1,14 @@
 package racingcar.game.service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Repository;
-import racingcar.car.interfaces.Car;
 import racingcar.car.interfaces.CarDAO;
 import racingcar.car.interfaces.NumberGenerator;
-import racingcar.car.model.RacingCar;
 import racingcar.game.dto.GameRequestDTO;
 import racingcar.game.dto.GameResponseDTO;
 import racingcar.game.interfaces.Game;
 import racingcar.game.interfaces.GameDAO;
 import racingcar.game.interfaces.GameResult;
 import racingcar.game.interfaces.GameService;
-import racingcar.game.model.RacingCarGame;
 
 @Repository
 public class RacingCarGameService implements GameService {
@@ -30,23 +24,11 @@ public class RacingCarGameService implements GameService {
         this.racingCarDAO = racingCarDAO;
     }
     
-    private List<Car> generateCars(final String namesLiteral) {
-        return Arrays.stream(namesLiteral.split(",")).map(name -> RacingCar.create(name, 0))
-                .collect(Collectors.toList());
-    }
-    
-    private GameResult playGame(final int count, final String namesLiteral) {
-        final List<Car> cars = this.generateCars(namesLiteral);
-        final Game racingCarGame = RacingCarGame.create(this.numberGenerator, cars);
-        final Game updatedGame = racingCarGame.race(count);
-        return updatedGame.calculateResult();
-    }
-    
     @Override
     public GameResponseDTO createGame(final GameRequestDTO gameRequestDTO) {
         final int count = gameRequestDTO.getCount();
         final String namesLiteral = gameRequestDTO.getNames();
-        final GameResult gameResult = this.playGame(count, namesLiteral);
+        final GameResult gameResult = Game.run(this.numberGenerator, count, namesLiteral);
         final int gameId = this.racingGameDao.insert(count, gameResult);
         this.racingCarDAO.insertAll(gameResult.getCars(), gameId);
         return GameResponseDTO.create(gameResult);
