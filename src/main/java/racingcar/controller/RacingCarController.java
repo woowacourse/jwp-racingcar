1ec844dcc2e7ddc79e97528320a9febeaca2c8f2
xxeol2 <jhj01177@naver.com>
2023-04-20 15:45:38 +0900

    feat: 게임 플레이 이력 조회 API 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,7 +1,9 @@
 package racingcar.controller;
 
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,12 @@ public class RacingCarController {
     @PostMapping("/plays")
     public ResponseEntity<PlayResponse> play(@RequestBody @Valid PlayRequest playRequest) {
         PlayResponse response = racingCarService.play(playRequest);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/plays")
+    public ResponseEntity<List<PlayResponse>> play() {
+        List<PlayResponse> response = racingCarService.findHistory();
         return ResponseEntity.ok().body(response);
     }
 }
--- src/test/java/racingcar/controller/RacingCarControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,7 +35,7 @@ class RacingCarControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void 컨트롤러_테스트() throws Exception {
+    void 게임을_진행한다() throws Exception {
         PlayResponse response = PlayResponse.of(
                 "car2",
                 List.of(new Player(1, "car1", 10, 1), new Player(2, "car2", 10, 1)));
@@ -52,5 +53,33 @@ class RacingCarControllerTest {
                 .andExpect(jsonPath("winners").value("car2"))
                 .andExpect(jsonPath("racingCars", hasSize(2)))
                 .andDo(print());
+    }
+
+    @Test
+    void 이력을_조회한다() throws Exception {
+        List<PlayResponse> response = List.of(
+                PlayResponse.of(
+                        "car2",
+                        List.of(new Player(1, "car1", 8, 1), new Player(2, "car2", 10, 1))),
+                PlayResponse.of(
+                        "car3",
+                        List.of(new Player(1, "car3", 10, 2)))
+        );
+
+        given(racingCarService.findHistory()).willReturn(response);
+
+        mockMvc.perform(get("/plays"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].winners").value("car2"))
+                .andExpect(jsonPath("$[0].racingCars", hasSize(2)))
+                .andExpect(jsonPath("$[0].racingCars[0].name").value("car1"))
+                .andExpect(jsonPath("$[0].racingCars[0].position").value(8))
+                .andExpect(jsonPath("$[0].racingCars[1].name").value("car2"))
+                .andExpect(jsonPath("$[0].racingCars[1].position").value(10))
+                .andExpect(jsonPath("$[1].winners").value("car3"))
+                .andExpect(jsonPath("$[1].racingCars", hasSize(1)))
+                .andExpect(jsonPath("$[1].racingCars[0].name").value("car3"));
+
     }
 }
