ba99e7b98c138955a03588dfc2528713f1edb825
splitube <voicesplit@daum.net>
2023-04-20 16:30:36 +0900

    refactor: RestController로 변경

--- src/main/java/racing/controller/WebRacingGameController.java
+++ src/main/java/racing/controller/WebRacingGameController.java
@@ -2,17 +2,16 @@ package racing.controller;
 
 import java.util.List;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import racing.dto.GameInputDto;
 import racing.dto.GameResultDto;
 import racing.service.RacingGameService;
 import racing.util.RandomNumberGenerator;
 
-@Controller
+@RestController
 public class WebRacingGameController implements RacingGameController {
 
     private final RacingGameService racingGameService;
@@ -21,14 +20,12 @@ public class WebRacingGameController implements RacingGameController {
         this.racingGameService = racingGameService;
     }
 
-    @ResponseBody
     @PostMapping(path = "/plays", produces = MediaType.APPLICATION_JSON_VALUE)
     public GameResultDto play(@RequestBody GameInputDto gameInputDto) {
         return racingGameService.playGame(gameInputDto,
             new RandomNumberGenerator());
     }
 
-    @ResponseBody
     @GetMapping(path = "/plays", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<GameResultDto> showAllGames() {
         return racingGameService.showGames();
