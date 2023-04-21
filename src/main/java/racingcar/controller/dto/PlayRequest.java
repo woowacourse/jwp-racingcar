3782ae5a2c87be419c711c31c4db003132a092ce
0chil <0@chll.it>
2023-04-21 13:53:54 +0900

    feat(PlayRequest): 리플렉션을 위한 생성자 지정 어노테이션

--- src/main/java/racingcar/controller/dto/PlayRequest.java
+++ src/main/java/racingcar/controller/dto/PlayRequest.java
@@ -1,10 +1,13 @@
 package racingcar.controller.dto;
 
+import java.beans.ConstructorProperties;
+
 public class PlayRequest {
 
     private final String names;
     private final int count;
 
+    @ConstructorProperties({"names", "count"})
     public PlayRequest(String names, int count) {
         this.names = names;
         this.count = count;
