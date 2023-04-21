56a3807e7bc01817a6f8ddab014ced5b03d57d18
mcodnjs <codnjs8989@gmail.com>
2023-04-21 22:21:28 +0900

    test: json을 dto로 파싱하여 response 검증

--- src/test/java/racingcar/controller/WebRacingCarControllerTest.java
+++ src/test/java/racingcar/controller/WebRacingCarControllerTest.java
@@ -1,6 +1,9 @@
 package racingcar.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,20 +14,21 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import racingcar.dto.RacingCarDto;
 import racingcar.dto.RacingCarRequest;
 import racingcar.dto.RacingResultResponse;
 import racingcar.service.RacingCarService;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WebRacingCarController.class)
@@ -39,30 +43,48 @@ class WebRacingCarControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    private List<RacingCarDto> racingCars;
+    private List<String> winners;
+
+    @BeforeEach
+    void beforeEach() {
+        winners = List.of("raon");
+        racingCars = List.of(
+                new RacingCarDto("glen", 4),
+                new RacingCarDto("raon", 6)
+        );
+    }
+
     @Test
     @DisplayName("/plays로 POST 요청 시 HTTP 200 코드와 게임의 결과가 반환되어야 한다.")
     void racingGamePlay_success() throws Exception {
         // given
         RacingCarRequest request = new RacingCarRequest("glen,raon", 10);
-        List<String> winners = List.of("raon");
-        List<RacingCarDto> racingCars = List.of(new RacingCarDto("glen", 4), new RacingCarDto("raon", 6));
 
         doReturn(new RacingResultResponse(winners, racingCars))
                 .when(racingCarService)
                 .playRacingGame(anyList(), anyInt());
 
         // expect
-        mockMvc.perform(post("/plays")
+        MvcResult mvcResult = mockMvc.perform(post("/plays")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
+                        .content(toJson(request)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.winners[0]").value("raon"))
-                .andExpect(jsonPath("$.racingCars[*].name", containsInAnyOrder("glen", "raon")))
-                .andExpect(jsonPath("$.racingCars[*].position", containsInAnyOrder(4, 6)));
+                .andReturn();
+
+        RacingResultResponse racingResultResponse =
+                objectMapper.readValue(contentToString(mvcResult), RacingResultResponse.class);
+
+        assertThat(racingResultResponse.getRacingCars())
+                .usingRecursiveComparison()
+                .isEqualTo(racingCars);
+        assertThat(racingResultResponse.getWinners())
+                .usingRecursiveComparison()
+                .isEqualTo(winners);
     }
 
     @ParameterizedTest
-    @DisplayName("유효하지 않는 input이 들어오면 400이 반한되어야 한다.")
+    @DisplayName("유효하지 않는 input이 들어오면 400이 반환되어야 한다.")
     @MethodSource("provideRacingCarRequest")
     void racingCarRequestValidation_fail(String names, int count) throws Exception {
         // given
@@ -71,7 +93,7 @@ class WebRacingCarControllerTest {
         // when
         mockMvc.perform(post("/plays")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
+                        .content(toJson(request)))
                 .andExpect(status().isBadRequest());
     }
 
@@ -88,21 +110,29 @@ class WebRacingCarControllerTest {
     @DisplayName("/plays로 GET 요청 시 HTTP 200 코드와 저장된 게임의 모든 결과가 반환되어야 한다.")
     void searchGameHistory_success() throws Exception {
         // given
-        RacingCarDto racingCarDto1 = new RacingCarDto("glen", 1);
-        RacingCarDto racingCarDto2 = new RacingCarDto("raon", 1);
-
         List<RacingResultResponse> racingResultResponses = new ArrayList<>();
-        racingResultResponses.add(new RacingResultResponse(List.of("raon"),  List.of(racingCarDto1, racingCarDto2)));
+        racingResultResponses.add(new RacingResultResponse(winners, racingCars));
 
         when(racingCarService.searchGameHistory())
                 .thenReturn(racingResultResponses);
 
         // expect
-        mockMvc.perform(get("/plays")
-                .contentType(MediaType.APPLICATION_JSON))
+        MvcResult mvcResult = mockMvc.perform(get("/plays")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].winners[0]").value("raon"))
-                .andExpect(jsonPath("$[0].racingCars[*].name", containsInAnyOrder("glen", "raon")))
-                .andExpect(jsonPath("$[0].racingCars[*].position", containsInAnyOrder(1, 1)));
+                .andReturn();
+
+        List<RacingResultResponse> responses =
+                objectMapper.readValue(contentToString(mvcResult), new TypeReference<List<RacingResultResponse>>() {});
+
+        assertThat(responses).usingRecursiveComparison().isEqualTo(racingResultResponses);
+    }
+
+    private String toJson(RacingCarRequest request) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(request);
+    }
+
+    private String contentToString(MvcResult mvcResult) throws UnsupportedEncodingException {
+        return mvcResult.getResponse().getContentAsString();
     }
 }
