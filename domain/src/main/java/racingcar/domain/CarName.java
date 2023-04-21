3e4f186b55e65610f6ba47b1954c35bb690ffb60
be-student <presentsong@naver.com>
2023-04-21 12:27:05 +0900

    feat: vo 에 equals and hashcode 재정의

--- domain/src/main/java/racingcar/domain/CarName.java
+++ domain/src/main/java/racingcar/domain/CarName.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 final class CarName {
 
     private static final int MAX_LENGTH = 5;
@@ -28,5 +30,22 @@ final class CarName {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
