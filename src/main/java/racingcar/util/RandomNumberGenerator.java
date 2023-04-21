4f7b056010e160432cd17b45ae955599cdb6e2b9
java-saeng <gusdn85554@naver.com>
2023-04-21 21:58:40 +0900

    refactor : Random 대신 ThreadLocalRandom 사용

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,16 +2,17 @@ package racingcar.util;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class RandomNumberGenerator implements NumberGenerator {
 
     private static final int MAX_RANDOM_INT = 10;
 
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+
     @Override
     public int generate() {
-        Random random = new Random();
         return random.nextInt(MAX_RANDOM_INT);
     }
 }
