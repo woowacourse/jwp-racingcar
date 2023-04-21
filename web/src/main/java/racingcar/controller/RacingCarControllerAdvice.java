ff72cdc8b43cf4b1b5d6152bb1c8b54f77d3a7af
be-student <presentsong@naver.com>
2023-04-21 12:33:53 +0900

    feat: 예외에 대한 logger 작성

--- web/src/main/java/racingcar/controller/RacingCarControllerAdvice.java
+++ web/src/main/java/racingcar/controller/RacingCarControllerAdvice.java
@@ -2,6 +2,8 @@ package racingcar.controller;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +18,12 @@ import racingcar.dto.ExceptionResponse;
 
 @RestControllerAdvice
 public class RacingCarControllerAdvice extends ResponseEntityExceptionHandler {
-    
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(Exception.class)
-    private ResponseEntity<ExceptionResponse<String>> handleException() {
+    private ResponseEntity<ExceptionResponse<String>> handleException(final Exception e) {
+        log.error("예상치 못한 예외가 발생했습니다.", e);
         return ResponseEntity.internalServerError().body(new ExceptionResponse<>("예상치 못한 예외가 발생했습니다."));
     }
 
@@ -26,6 +31,7 @@ public class RacingCarControllerAdvice extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status,
             final WebRequest request) {
+        log.info("유효성 검사에 실패했습니다.", ex);
         final Map<String, String> body = ex.getFieldErrors()
                 .stream()
                 .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
@@ -35,11 +41,13 @@ public class RacingCarControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     private ResponseEntity<ExceptionResponse<String>> handleIllegalArgumentException(
             final IllegalArgumentException e) {
+        log.info("잘못된 인자가 들어왔습니다", e);
         return ResponseEntity.badRequest().body(new ExceptionResponse<>(e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     private ResponseEntity<ExceptionResponse<String>> handleMethodArgumentTypeMismatchException() {
+        log.info("올바른 타입을 입력해주세요.");
         return ResponseEntity.badRequest().body(new ExceptionResponse<>("올바른 타입을 입력해주세요."));
     }
 }
