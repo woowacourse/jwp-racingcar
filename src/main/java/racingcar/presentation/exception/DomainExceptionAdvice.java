dab6b549dd3a6ea225a0152c3a4fef4e95549a77
woosung1223 <dntjd991223@naver.com>
2023-04-22 17:17:07 +0900

    refactor: 불필요한 ControllerAdvice Order 지정 삭제
    스프링에서는 여러 ExceptionHandler가 있을 때, 가장 구체적인 ExceptionHandler를 선택함

--- src/main/java/racingcar/presentation/exception/DomainExceptionAdvice.java
+++ src/main/java/racingcar/presentation/exception/DomainExceptionAdvice.java
@@ -2,14 +2,11 @@ package racingcar.presentation.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import racingcar.presentation.dto.ExceptionResponse;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class DomainExceptionAdvice {
 
@@ -18,7 +15,7 @@ public class DomainExceptionAdvice {
     @ExceptionHandler({
             IllegalArgumentException.class
     })
-    public ResponseEntity<ExceptionResponse> handledException(final RuntimeException e) {
+    public ResponseEntity<ExceptionResponse> handledException(final IllegalArgumentException e) {
         logger.debug(e.getMessage());
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(e.getMessage()));
