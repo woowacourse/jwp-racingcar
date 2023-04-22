ad709455ba7ebf8c3c0bc2fa395cf967ac0b8a76
junpakPark <junpak.park@gmail.com>
2023-04-22 16:54:15 +0900

    refacor : 사용자 정의 예외 객체 삭제

--- src/main/java/racingcar/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package racingcar.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class BadRequestException extends RacingCarException {
-    public BadRequestException(final String message) {
-        super(message, HttpStatus.BAD_REQUEST);
-    }
-}
