bee5652941662ef5ef1f77decf6aefe438a00e62
hgo641 <hgo641@gmail.com>
2023-04-22 15:27:38 +0900

    refactor: 콘솔 View에서 name을 파싱, 조인하게 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,19 @@
 package racingcar.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String NAME_DELIMITER = ",";
 
-    public static String inputCarNames() {
+    public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return SCANNER.nextLine();
+        String combinedNames = SCANNER.nextLine();
+        return Arrays.stream(combinedNames.split(NAME_DELIMITER))
+                .collect(Collectors.toList());
     }
 
     public static int inputTryCount() {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,27 +1,23 @@
 package racingcar.view;
 
-import racingcar.dto.response.CarResponse;
-import racingcar.dto.response.GameResponse;
+import racingcar.domain.Car;
 
 import java.util.List;
 
 public class OutputView {
+    private static final String NAME_DELIMITER = ",";
+
     public static void printErrorMessage(final String errorMessage) {
         System.out.println(errorMessage);
     }
 
-    public static void printCarGameResult(final GameResponse gameResponse) {
-        printWinners(gameResponse.getWinners());
-        printCarResults(gameResponse.getRacingCars());
+    public static void printWinners(final List<String> winnersNames) {
+        System.out.println(String.format("%s가 최종 우승했습니다.", String.join(NAME_DELIMITER, winnersNames)));
     }
 
-    public static void printWinners(final String winnersName) {
-        System.out.println(String.format("%s가 최종 우승했습니다.", winnersName));
-    }
-
-    private static void printCarResults(final List<CarResponse> cars) {
-        for (CarResponse carResult : cars) {
-            System.out.println(String.format("%s의 최종 포지션 : %d", carResult.getName(), carResult.getPosition()));
+    public static void printCarResults(final List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(String.format("%s의 최종 포지션 : %d", car.getName(), car.getPosition()));
         }
     }
 }
