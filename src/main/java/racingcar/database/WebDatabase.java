322f4e93e7aa80aff40287f1f2ad1c467c0e807a
aiaiaiai1 <cvc100326@naver.com>
2023-04-22 14:53:28 +0900

    refactor: DB 접근 한번만 하도록 수정 및 메서드 분리

--- src/main/java/racingcar/database/WebDatabase.java
+++ src/main/java/racingcar/database/WebDatabase.java
@@ -5,11 +5,13 @@ import racingcar.dao.RacingCarDao;
 import racingcar.dao.RacingGameDao;
 import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
+import racingcar.dto.response.RacingCarResponseDto;
 import racingcar.dto.response.RacingGameResponseDto;
 import racingcar.dto.response.RacingGameWinnersDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class WebDatabase implements Database {
@@ -24,11 +26,10 @@ public class WebDatabase implements Database {
     }
 
     @Override
-    public void save(RacingGame racingGame, int trialCount) {
+    public void save(final RacingGame racingGame, final int trialCount) {
         final Long gameId = saveGame(racingGame, trialCount);
         saveCars(racingGame, gameId);
     }
-
 
     private Long saveGame(final RacingGame racingGame, final int trialCount) {
         final String names = String.join(",", racingGame.findWinners());
@@ -41,15 +42,31 @@ public class WebDatabase implements Database {
         }
     }
 
+    @Override
     public List<RacingGameResponseDto> findAllHistories() {
         List<RacingGameResponseDto> racingGameResponseDtos = new ArrayList<>();
+        List<RacingCarResponseDto> allRacingCarResponseDtos = racingCarDao.findAllCars();
+        addRacingGameResponseDto(racingGameResponseDtos, allRacingCarResponseDtos);
+        return racingGameResponseDtos;
+    }
+
+    private void addRacingGameResponseDto(
+            final List<RacingGameResponseDto> racingGameResponseDtos,
+            final List<RacingCarResponseDto> allRacingCarResponseDtos) {
         for (RacingGameWinnersDto racingGameWinnersDto : racingGameDao.findAllWinners()) {
+            List<RacingCarResponseDto> racingCarResponseDtos = filterSameGameId(racingGameWinnersDto, allRacingCarResponseDtos);
             racingGameResponseDtos.add(
-                    new RacingGameResponseDto(
-                            racingGameWinnersDto.getWinners(), racingCarDao.findCars(racingGameWinnersDto.getId())
-                    )
+                    new RacingGameResponseDto(racingGameWinnersDto.getWinners(), racingCarResponseDtos)
             );
         }
-        return racingGameResponseDtos;
+    }
+
+    private List<RacingCarResponseDto> filterSameGameId(
+            final RacingGameWinnersDto racingGameWinnersDto,
+            final List<RacingCarResponseDto> allRacingCarResponseDtos) {
+        List<RacingCarResponseDto> racingCarResponseDtos = allRacingCarResponseDtos.stream()
+                .filter(s -> s.getGameId().equals(racingGameWinnersDto.getId()))
+                .collect(Collectors.toList());
+        return racingCarResponseDtos;
     }
 }
