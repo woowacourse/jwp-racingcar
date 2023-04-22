eea8a2b7e37b153129d35d5da2be5ae22a093384
SproutMj <mjk54565@gmail.com>
2023-04-22 19:22:04 +0900

    refactor: 안쓰는 상수 제거

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -2,9 +2,6 @@ package racingcar.domain;
 
 public class TryCount {
 
-    private static final int MINIMUM_TRY_COUNT = 2;
-    private static final int MAXIMUM_TRY_COUNT = 10;
-
     private final int tryCount;
 
     public TryCount(final int tryCount) {
