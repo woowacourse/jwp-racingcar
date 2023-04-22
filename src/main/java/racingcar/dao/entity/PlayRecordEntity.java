b5d6c026b3222af1beea2dc3df166e33e0b01eb2
yoondgu <doyou2ing@naver.com>
2023-04-22 16:45:19 +0900

    refactor: PlayRecords 테이블에 대한 Entity 누락 컬럼 추가

--- src/main/java/racingcar/dao/entity/PlayRecordEntity.java
+++ src/main/java/racingcar/dao/entity/PlayRecordEntity.java
@@ -1,5 +1,6 @@
 package racingcar.dao.entity;
 
+import java.time.LocalTime;
 import lombok.Getter;
 
 @Getter
@@ -7,9 +8,11 @@ public class PlayRecordEntity {
 
     private final Long id;
     private final Integer count;
+    private final LocalTime createdAt;
 
     public PlayRecordEntity(final Integer count) {
         this.id = null;
         this.count = count;
+        this.createdAt = null;
     }
 }
