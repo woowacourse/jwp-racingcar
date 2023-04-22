a633380a808c425bc9f9c751ace48d33a6fbc328
Combi153 <jcm04@naver.com>
2023-04-22 16:03:54 +0900

    feat: 로그 기능 추가

--- src/main/java/racingcar/controller/CarRacingWebController.java
+++ src/main/java/racingcar/controller/CarRacingWebController.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -18,6 +20,7 @@ import java.util.Map;
 @RestController
 public class CarRacingWebController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
     private final CarRacingService carRacingService;
 
     public CarRacingWebController(final CarRacingService carRacingService) {
@@ -45,6 +48,7 @@ public class CarRacingWebController {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errorMessageByFields.put(fieldName, errorMessage);
+            log.error("Unexpected error occurred : {}", errorMessage, exception);
         });
 
         return ResponseEntity.badRequest().body(errorMessageByFields);
@@ -52,11 +56,14 @@ public class CarRacingWebController {
 
     @ExceptionHandler(InvalidFormatException.class)
     public ResponseEntity<String> handleInvalidFormat(InvalidFormatException exception) {
-        return ResponseEntity.badRequest().body("[ERROR] 시도할 횟수에 숫자를 입력할 수 없습니다. 입력 값 : " + exception.getValue());
+        final String exceptionMessage = "[ERROR] 시도할 횟수에 숫자를 입력할 수 없습니다. 입력 값 : " + exception.getValue();
+        log.error("Unexpected error occurred : {}", exceptionMessage, exception);
+        return ResponseEntity.badRequest().body(exceptionMessage);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException exception) {
+        log.error("Unexpected error occurred : {}", exception.getMessage(), exception);
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
