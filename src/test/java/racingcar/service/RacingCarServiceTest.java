62a3f87b899b5fc682f232185bd5d54c1b0f6e74
aiaiaiai1 <cvc100326@naver.com>
2023-04-22 14:55:41 +0900

    test: 저장된 게임 이력 불러오기 테스트 추가

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,17 +26,23 @@ class RacingCarServiceTest {
 
     @Autowired
     private RacingCarService racingCarService;
+    private RacingGame racingGame;
 
-    @DisplayName("게임결과를 반환한다.")
-    @Test
-    void play() {
+
+    @BeforeEach
+    void setup() {
         final NumberGenerator numberGenerator = new FixNumberGenerator(
                 new ArrayList<>(Arrays.asList(6, 3, 6, 5, 6, 8))
         );
         final Cars cars = new Cars(List.of("다즐", "루쿠"));
         final Count count = new Count(3);
-        final RacingGame racingGame = new RacingGame(numberGenerator, cars, count);
+        racingGame = new RacingGame(numberGenerator, cars, count);
+    }
 
+
+    @DisplayName("게임결과를 반환한다.")
+    @Test
+    void play() {
         final RacingGameResponseDto racingGameResponseDto = racingCarService.play(racingGame);
 
         assertAll(
@@ -47,4 +54,21 @@ class RacingCarServiceTest {
                 () -> assertThat(racingGameResponseDto.getRacingCars().get(1).getPosition()).isEqualTo(2)
         );
     }
+
+    @DisplayName("저장된 게임이력을 반환한다.")
+    @Test
+    void findAllHistories() {
+        racingCarService.play(racingGame);
+        List<RacingGameResponseDto> result = racingCarService.findAllHistories();
+
+        assertAll(
+                () -> assertThat(result.get(0).getWinners()).isEqualTo("다즐"),
+                () -> assertThat(result.get(0).getRacingCars().size()).isEqualTo(2),
+                () -> assertThat(result.get(0).getRacingCars().get(0).getName()).isEqualTo("다즐"),
+                () -> assertThat(result.get(0).getRacingCars().get(0).getPosition()).isEqualTo(3),
+                () -> assertThat(result.get(0).getRacingCars().get(1).getName()).isEqualTo("루쿠"),
+                () -> assertThat(result.get(0).getRacingCars().get(1).getPosition()).isEqualTo(2));
+    }
+
+
 }
