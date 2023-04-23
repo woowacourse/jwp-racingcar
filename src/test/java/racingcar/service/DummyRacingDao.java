0df5095fc2967a4e5a6662e5e65da6d0d444826b
Woojin Lee <dhkep03@gmail.com>
2023-04-23 13:40:37 +0900

    refactor: DAO 규격 수정, 테스트 리팩터링

--- src/test/java/racingcar/service/DummyRacingDao.java
+++ src/test/java/racingcar/service/DummyRacingDao.java
@@ -1,17 +1,15 @@
 package racingcar.service;
 
-import java.util.List;
 import racingcar.dao.RacingDao;
-import racingcar.dto.CarDto;
-import racingcar.dto.RacingResultDto;
+import racingcar.domain.RacingGame;
 
 public class DummyRacingDao implements RacingDao {
     @Override
-    public int saveGameResult(final RacingResultDto racingResultDto, final int trialCount) {
+    public int saveGameResult(final RacingGame racingGame, final int trialCount) {
         return 0;
     }
 
     @Override
-    public void savePlayerResults(final List<CarDto> racingCars, final int gameResultId) {
+    public void savePlayerResults(final RacingGame racingGame, final int gameResultId) {
     }
 }
