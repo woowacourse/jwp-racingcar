8c3c7d77f14bbc87e9f1433fb3ec53a61c5d618a
70825 <hello70825@gmail.com>
2023-04-23 15:02:07 +0900

    refactor: 서비스 테스트를 Mockito로 변경

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -3,27 +3,45 @@ package racingcar.service;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import racingcar.controller.dto.CarResponse;
 import racingcar.controller.dto.TrackRequest;
 import racingcar.controller.dto.TrackResponse;
+import racingcar.dao.RacingDao;
+import racingcar.dao.dto.TrackDto;
 import racingcar.mapper.TrackRequestMapper;
+import racingcar.model.car.strategy.MovingStrategy;
 
 import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class RacingServiceTest {
 
-    @Autowired
+    @InjectMocks
     private RacingService racingService;
+
+    @Mock
+    private RacingDao racingDao;
+
+    @Mock
+    private MovingStrategy movingStrategy;
 
     @Test
     @DisplayName("자동차 게임이 정상적으로 작동한다.")
     void playSuccess() {
         // given
-        final TrackRequest trackRequest = TrackRequestMapper.of("gray,hoy,logan", "10");
-        final int expected = trackRequest.getNames().split(",").length;
+        final String names = "gray,hoy,logan";
+        final TrackRequest trackRequest = TrackRequestMapper.of(names, "10");
+        final int expected = names.split(",").length;
+
+        when(racingDao.save((TrackDto) any())).thenReturn(1);
+        when(movingStrategy.movable()).thenReturn(true);
 
         // when
         final TrackResponse trackResponse = racingService.play(trackRequest);
