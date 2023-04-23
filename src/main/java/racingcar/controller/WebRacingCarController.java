ec6de3dca7070a6696a5a8f47c880f7e9a73cd7c
MoonJeWoong <munjin0201@naver.com>
2023-04-23 21:45:40 +0900

    feat: request, domain 예외처리 핸들러 구현

--- src/main/java/racingcar/controller/WebRacingCarController.java
+++ src/main/java/racingcar/controller/WebRacingCarController.java
@@ -1,6 +1,9 @@
 package racingcar.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +12,10 @@ import racingcar.dto.request.RacingStartRequest;
 import racingcar.dto.response.RacingResultResponse;
 import racingcar.service.RacingCarService;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class WebRacingCarController {
@@ -21,8 +27,25 @@ public class WebRacingCarController {
     }
 
     @PostMapping("/plays")
-    public ResponseEntity<RacingResultResponse> play(@RequestBody RacingStartRequest racingStartRequest) {
+    public ResponseEntity<RacingResultResponse> play(@RequestBody @Valid RacingStartRequest racingStartRequest) {
         return ResponseEntity.ok(webRacingCarService.play(racingStartRequest));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidExceptionHandler(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @GetMapping("/plays")
--- src/main/java/racingcar/dto/request/RacingStartRequest.java
+++ src/main/java/racingcar/dto/request/RacingStartRequest.java
@@ -1,11 +1,15 @@
 package racingcar.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Arrays;
 import java.util.List;
 
 public class RacingStartRequest {
 
+    @NotEmpty(message = "[ERROR] Request message`s names must not be Empty.")
     private List<String> names;
+    @NotNull(message = "[ERROR] Request message`s count must not be null.")
     private Integer count;
 
     public RacingStartRequest(String names, Integer count) {
