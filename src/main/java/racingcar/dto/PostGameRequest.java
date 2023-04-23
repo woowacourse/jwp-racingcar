6917858b96a20546ce8b65d3183e6896971ceec2
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-04-23 15:58:39 +0900

    chore: 불필요한 @JsonProperty 어노테이션 제거

--- src/main/java/racingcar/dto/PostGameRequest.java
+++ src/main/java/racingcar/dto/PostGameRequest.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PostGameRequest {
 
-    @JsonProperty("names")
     private final String names;
-    @JsonProperty("count")
     private final int count;
 
     @JsonCreator
-    public PostGameRequest(final @JsonProperty("names") String names, final @JsonProperty("count") int count) {
+    public PostGameRequest(final String names, final int count) {
         this.names = names;
         this.count = count;
     }
