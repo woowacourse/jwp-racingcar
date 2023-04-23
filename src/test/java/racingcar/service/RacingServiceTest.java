7040523520f7f966ab96750832bc08cf96a9424e
70825 <hello70825@gmail.com>
2023-04-23 14:05:55 +0900

    refactor: 서비스 테스트 가독성 있게 수정

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,24 +12,25 @@ import racingcar.mapper.TrackRequestMapper;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingServiceTest {
 
     @Autowired
-    RacingService racingService;
+    private RacingService racingService;
 
     @Test
     @DisplayName("자동차 게임이 정상적으로 작동한다.")
     void playSuccess() {
-        String names = "그레이,호이,로건";
-        String trialTimes = "10";
+        // given
+        final TrackRequest trackRequest = TrackRequestMapper.of("gray,hoy,logan", "10");
+        final int expected = trackRequest.getNames().split(",").length;
 
-        final TrackRequest trackRequest = TrackRequestMapper.of(names, trialTimes);
+        // when
         final TrackResponse trackResponse = racingService.play(trackRequest);
-        final List<CarResponse> cars = trackResponse.getRacingCars();
+        final List<CarResponse> carResponses = trackResponse.getRacingCars();
+        final int actual = carResponses.size();
 
-        assertThat(cars.size()).isEqualTo(names.split(",").length);
+        // then
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
