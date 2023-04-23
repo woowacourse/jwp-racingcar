4f1986e7a89a1c0a4eebe6175b0ab83f67cb797d
70825 <hello70825@gmail.com>
2023-04-23 11:27:02 +0900

    refactor: 테스트 어노테이션 변경으로 속도 최적화 @SpringBootTest -> @Import(RacingDao.class), @JdbcTest

--- src/test/java/racingcar/dao/RacingDaoTest.java
+++ src/test/java/racingcar/dao/RacingDaoTest.java
@@ -2,7 +2,8 @@ package racingcar.dao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
 import racingcar.dao.dto.CarDto;
 import racingcar.dao.dto.TrackDto;
 import racingcar.mapper.CarDtoMapper;
@@ -11,11 +12,12 @@ import racingcar.mapper.TrackDtoMapper;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(RacingDao.class)
+@JdbcTest
 class RacingDaoTest {
 
     @Autowired
-    RacingDao racingDao;
+    private RacingDao racingDao;
 
     @Test
     void createTrack() {
