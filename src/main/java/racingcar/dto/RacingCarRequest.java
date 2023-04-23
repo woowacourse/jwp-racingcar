4f294fc28e8d3cd422e9c919aa0203b7dfd770ee
songusika <sws981202@gmail.com>
2023-04-23 17:20:49 +0900

    refactor: 자동차 이름 입력 검증 로직 추가

--- src/main/java/racingcar/dto/RacingCarRequest.java
+++ src/main/java/racingcar/dto/RacingCarRequest.java
@@ -1,16 +1,44 @@
 package racingcar.dto;
 
+import static java.util.Collections.addAll;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public final class RacingCarRequest {
 
-    private final String names;
+    private static final String ENTER_NAME_WITH_COMMA = "쉼표로 이름을 구분해주세요.";
+    private static final String COMMA = ",";
+
+    private final List<String> names;
     private final int tryCount;
 
     public RacingCarRequest(final String names, final int count) {
-        this.names = names;
+        this.names = sliceNameByComma(names);
         this.tryCount = count;
     }
 
-    public String getNames() {
+    private List<String> sliceNameByComma(final String names) {
+        validateComma(names);
+
+        return getSplitName(names);
+    }
+
+    private static List<String> getSplitName(final String names) {
+        List<String> splitNames = new ArrayList<>();
+
+        addAll(splitNames, names.replace(" ", "").split(COMMA));
+
+        return splitNames;
+    }
+
+    private void validateComma(final String names) {
+        if (!names.contains(COMMA)) {
+            throw new IllegalArgumentException(ENTER_NAME_WITH_COMMA);
+        }
+    }
+
+    public List<String> getNames() {
         return names;
     }
 
