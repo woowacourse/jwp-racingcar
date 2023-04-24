d722d3ffad2ef74abc6cba8e00458b8f65804f8d
songusika <sws981202@gmail.com>
2023-04-24 13:59:54 +0900

    refactor: RacingCarRequest 사용하지 않는 메서드 리팩터링

--- src/main/java/racingcar/dto/RacingCarRequest.java
+++ src/main/java/racingcar/dto/RacingCarRequest.java
@@ -1,44 +1,25 @@
 package racingcar.dto;
 
-import static java.util.Collections.addAll;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
+import racingcar.controller.annotation.ContainDelimiter;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@Validated
 public final class RacingCarRequest {
 
-    private static final String ENTER_NAME_WITH_COMMA = "쉼표로 이름을 구분해주세요.";
-    private static final String COMMA = ",";
-
-    private final List<String> names;
+    @NotBlank
+    @ContainDelimiter
+    private final String names;
+    @NotNull
     private final int tryCount;
 
     public RacingCarRequest(final String names, final int count) {
-        this.names = sliceNameByComma(names);
+        this.names = names;
         this.tryCount = count;
     }
 
-    private List<String> sliceNameByComma(final String names) {
-        validateComma(names);
-
-        return getSplitName(names);
-    }
-
-    private static List<String> getSplitName(final String names) {
-        List<String> splitNames = new ArrayList<>();
-
-        addAll(splitNames, names.replace(" ", "").split(COMMA));
-
-        return splitNames;
-    }
-
-    private void validateComma(final String names) {
-        if (!names.contains(COMMA)) {
-            throw new IllegalArgumentException(ENTER_NAME_WITH_COMMA);
-        }
-    }
-
-    public List<String> getNames() {
+    public String getNames() {
         return names;
     }
 
