70a74c19af44fb62a681dcd679e48bb0aaf39e92
yeonkkk <tjddus0302@gmail.com>
2023-04-24 13:16:21 +0900

    test: 게임 조회 테스트

--- src/test/java/racingcar/dao/GameDaoTest.java
+++ src/test/java/racingcar/dao/GameDaoTest.java
@@ -1,11 +1,12 @@
 package racingcar.dao;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static racingcar.constant.RacingCarDDL.CREATE_TABLE_CAR;
 import static racingcar.constant.RacingCarDDL.CREATE_TABLE_GAME;
 import static racingcar.constant.RacingCarDDL.DROP_TABLE_CAR;
 import static racingcar.constant.RacingCarDDL.DROP_TABLE_GAME;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,5 +36,14 @@ class GameDaoTest {
     void insert() {
         int gameId = gameDao.insertGame(5);
         assertThat(gameId).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("모든 게임 id를 조회한다.")
+    public void findAllGamesId() {
+        int gameId = gameDao.insertGame(5);
+        List<Integer> allGamesId = gameDao.findAllGamesId();
+
+        assertThat(allGamesId).containsExactly(gameId);
     }
 }
