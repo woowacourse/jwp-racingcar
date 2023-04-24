36aaacb349899d3654da6b2fdbf191ff96d9e9cd
kokodak <kokodakadokok@gmail.com>
2023-04-24 14:27:40 +0900

    refactor: 사용하지 않은 import 삭제

--- src/main/java/racingcar/service/RacingCarGameService.java
+++ src/main/java/racingcar/service/RacingCarGameService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import racingcar.domain.AttemptNumber;
