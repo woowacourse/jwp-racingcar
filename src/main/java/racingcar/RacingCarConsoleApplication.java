7b4652011a0fadc84f448e9f5c7d31e8496f5b09
kiarakim <kiarakim413@gmail.com>
2023-04-24 12:56:36 +0900

    feat: controller에 service 의존성 주입

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -1,10 +1,14 @@
 package racingcar;
 
 import racingcar.controller.RacingCarConsoleController;
+import racingcar.repository.console.ConsoleRacingCarRepository;
+import racingcar.service.RacingGameService;
 
 public class RacingCarConsoleApplication {
 	public static void main(String[] args) {
-		RacingCarConsoleController controller = new RacingCarConsoleController();
+		RacingCarConsoleController controller = new RacingCarConsoleController(
+			new RacingGameService(new ConsoleRacingCarRepository())
+		);
 		controller.run();
 	}
 }
