eedf45bb392895d0f5426b09cde77f1036d057cd
yeonkkk <tjddus0302@gmail.com>
2023-04-24 13:15:39 +0900

    refactor: @Transactional 애너테이션 추가

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -3,6 +3,7 @@ package racingcar.service;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.dao.CarDao;
 import racingcar.dao.GameDao;
 import racingcar.domain.Car;
@@ -12,6 +13,7 @@ import racingcar.domain.Result;
 import racingcar.dto.ResultDto;
 import racingcar.strategy.RandomMovingStrategy;
 
+@Transactional
 @Service
 public class RacingCarService {
 
