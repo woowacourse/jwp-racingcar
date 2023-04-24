ea8d0e40b937d42fb7c4a9cc4bacb41f25f6253f
kiarakim <kiarakim413@gmail.com>
2023-04-24 12:56:55 +0900

    feat: controller에 service 의존성 주입

--- src/main/java/racingcar/controller/RacingCarWebController.java
+++ src/main/java/racingcar/controller/RacingCarWebController.java
@@ -10,31 +10,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import racingcar.domain.Cars;
-import racingcar.domain.RacingGame;
 import racingcar.dto.RacingGameDto;
 import racingcar.dto.ResultDto;
-import racingcar.jdbc.RacingCarDao;
+import racingcar.service.RacingGameService;
 
 @RestController
 public class RacingCarWebController {
-	private final RacingCarDao racingCarDao = new RacingCarDao();
+	private final RacingGameService racingGameService;
+
+	public RacingCarWebController(RacingGameService racingGameService) {
+		this.racingGameService = racingGameService;
+	}
 
 	@GetMapping("/plays")
 	public List<ResultDto> findResult() {
-		return racingCarDao.find();
+		return racingGameService.find();
 	}
 
 	@PostMapping("/plays")
 	public ResultDto createData(@RequestBody RacingGameDto racingGameDto) {
-		String carNames = racingGameDto.getNames();
-		int count = Integer.parseInt(racingGameDto.getCount());
 
-		RacingGame racingGame = new RacingGame(carNames);
-		racingGame.startRacing(count);
-
-		Cars cars = racingGame.getCars();
-		racingCarDao.save(cars, count);
-
+		Cars cars = racingGameService.save(racingGameDto);
 		return toResultDto(cars);
 	}
 }
