55abc4181a064527229dd7cb394c3d69cf453f9b
aiaiaiai1 <cvc100326@naver.com>
2023-04-24 17:09:17 +0900

    feat: 인메모리데이터베이스 구현체 구현

--- src/main/java/racingcar/database/InMemoryDatabase.java
+++ src/main/java/racingcar/database/InMemoryDatabase.java
@@ -1,19 +1,41 @@
 package racingcar.database;
 
+import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
+import racingcar.dto.response.RacingCarResponseDto;
 import racingcar.dto.response.RacingGameResponseDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryDatabase implements Database {
 
+    private final List<RacingGame> racingGames;
+    private final List<Integer> trialCounts;
+
+    public InMemoryDatabase() {
+        this.racingGames = new ArrayList<>();
+        this.trialCounts = new ArrayList<>();
+    }
+
     @Override
     public void save(RacingGame racingGame, int trialCount) {
-
+        racingGames.add(racingGame);
+        trialCounts.add(trialCount);
     }
 
     @Override
     public List<RacingGameResponseDto> findAllHistories() {
-        return null;
+        List<RacingGameResponseDto> racingGameResponseDtos = new ArrayList<>();
+        for (RacingGame racingGame : racingGames) {
+            List<RacingCarResponseDto> racingCarResponseDtos = new ArrayList<>();
+            for (Car car : racingGame.getCurrentResult()) {
+                racingCarResponseDtos.add(new RacingCarResponseDto(car.getName(), car.getPosition()));
+            }
+            racingGameResponseDtos.add(new RacingGameResponseDto(
+                    String.join(",", racingGame.findWinners()), racingCarResponseDtos));
+        }
+
+        return racingGameResponseDtos;
     }
 }
