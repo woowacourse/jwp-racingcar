f1a5ac9c0f404b6f05b27284d283c22ca4e3ef83
kevstevie <jianna442@gmail.com>
2023-04-25 20:13:33 +0900

    refactor: jsonProperty 제거

--- src/main/java/racingcar/dto/PlayRequestDto.java
+++ src/main/java/racingcar/dto/PlayRequestDto.java
@@ -1,16 +1,10 @@
 package racingcar.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public final class PlayRequestDto {
-    @JsonProperty("names")
     private String names;
-    @JsonProperty("count")
     private int count;
 
-    @JsonCreator
-    public PlayRequestDto(@JsonProperty("names") final String names, @JsonProperty("count") final int count) {
+    public PlayRequestDto(final String names, final int count) {
         this.names = names;
         this.count = count;
     }
