898558415f994389a5faaa8169791629c4b64538
rawfishthelgh <rg970604@naver.com>
2023-04-26 21:09:30 +0900

    refactor : 오타 수정

--- src/main/java/racingcar/service/RacingCarPlayRule.java
+++ src/main/java/racingcar/service/RacingCarPlayRule.java
@@ -7,7 +7,7 @@ import racingcar.genertor.NumberGenerator;
 @Component
 public class RacingCarPlayRule {
 
-    public void moverCarsUntilCountIsOver(Cars cars, int count, NumberGenerator numberGenerator) {
+    public void moveCarsUntilCountIsOver(Cars cars, int count, NumberGenerator numberGenerator) {
         while (count-- > 0) {
             cars.moveCars(numberGenerator);
         }
