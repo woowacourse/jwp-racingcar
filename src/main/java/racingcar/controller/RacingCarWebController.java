9987dfdfb53b991dd2bb33cd3eb1bda3d4c467bd
ezzanzzan <cjsdmswjd821@naver.com>
2023-04-26 21:38:19 +0900

    refactor: 중복 코드 제거
    - Controller -> RestController

--- src/main/java/racingcar/controller/RacingCarWebController.java
+++ src/main/java/racingcar/controller/RacingCarWebController.java
@@ -2,18 +2,19 @@ package racingcar.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import racingcar.domain.Cars;
 import racingcar.domain.TryCount;
 import racingcar.dto.GameInitializeDto;
 import racingcar.dto.RacingCarGameResultDto;
 import racingcar.service.RacingCarService;
 
-@Controller
+@RestController
+@RequestMapping("/plays")
 public class RacingCarWebController {
     private final RacingCarService racingCarService;
 
@@ -21,16 +22,15 @@ public class RacingCarWebController {
         this.racingCarService = racingCarService;
     }
 
-    @PostMapping("/plays")
-    @ResponseBody
-    public RacingCarGameResultDto run(@RequestBody GameInitializeDto gameInitializeDto) {
+    @PostMapping()
+    public ResponseEntity<RacingCarGameResultDto> run(@RequestBody GameInitializeDto gameInitializeDto) {
         Cars cars = Cars.from(gameInitializeDto.getNames());
         TryCount tryCount = new TryCount(gameInitializeDto.getCount());
-        return racingCarService.play(cars, tryCount);
+        RacingCarGameResultDto racingCarGameResultDto = racingCarService.play(cars, tryCount);
+        return ResponseEntity.ok().body(racingCarGameResultDto);
     }
 
-    @GetMapping("/plays")
-    @ResponseBody
+    @GetMapping()
     public ResponseEntity<List<RacingCarGameResultDto>> showGameResult() {
         List<RacingCarGameResultDto> gameResult = racingCarService.getAllGameResult();
         return ResponseEntity.ok().body(gameResult);
